c5bfe977cda1c41875f799a7abc25b5b00aa0e23
Anthony Fu <anthonyfu117@hotmail.com>
2021-10-08 14:49:18 +0800

    feat: new @unocss/reset package

---
+++ packages/reset/scripts/copy.ts
@@ -0,0 +1,6 @@
+import fs from 'fs'
+
+fs.copyFileSync(
+  'node_modules/@csstools/normalize.css/normalize.css',
+  'normalize.css',
+)
--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@unocss/reset/normalize.css'
 import './style.css'
 
 createApp(App).mount('#app')
