5b9c39f04698bd792a2097c1c14801686b67b30a
Anthony Fu <anthonyfu117@hotmail.com>
2021-10-14 21:54:40 +0800

    feat: transform origin

--- packages/preset-uno/src/rules/transform.ts
+++ packages/preset-uno/src/rules/transform.ts
@@ -21,6 +21,15 @@ export const transforms: Rule[] = [
   [/^scale()-([^-]+)$/, handleScale],
   [/^scale-([xyz])-([^-]+)$/, handleScale],
   [/^rotate-([^-]+)(?:deg)?$/, handleRotate],
+  ['origin-center', { 'transform-origin': 'center' }],
+  ['origin-top', { 'transform-origin': 'top' }],
+  ['origin-top-right', { 'transform-origin': 'top right' }],
+  ['origin-right', { 'transform-origin': 'right' }],
+  ['origin-bottom-right', { 'transform-origin': 'bottom right' }],
+  ['origin-bottom', { 'transform-origin': 'bottom' }],
+  ['origin-bottom-left', { 'transform-origin': 'bottom left' }],
+  ['origin-left', { 'transform-origin': 'left' }],
+  ['origin-top-left', { 'transform-origin': 'top left' }],
 ]
 
 function handleTranslate([, d, b]: string[]): CSSEntries | undefined {
