b617f5737eee42e3dcb5f78f7f235040cfd4a887
Anthony Fu <anthonyfu117@hotmail.com>
2021-10-31 15:54:49 +0800

    fix: add `.astro` as scanning target, close #24

--- packages/vite/src/utils.ts
+++ packages/vite/src/utils.ts
@@ -1,7 +1,7 @@
 import { createHash } from 'crypto'
 
 export const defaultExclude = [/[\/\\]node_modules[\/\\]/, /[\/\\]dist[\/\\]/, /\.(css|postcss|sass|scss|less|stylus|styl)$/]
-export const defaultInclude = [/\.vue$/, /\.vue\?vue/, /\.svelte$/, /\.[jt]sx$/, /\.mdx?$/]
+export const defaultInclude = [/\.vue$/, /\.vue\?vue/, /\.svelte$/, /\.[jt]sx$/, /\.mdx?$/, /\.astro$/]
 
 export function getHash(input: string, length = 8) {
   return createHash('sha256')
