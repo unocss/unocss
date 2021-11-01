9b3c1aa7eba9b0098a61b2a10681389fe40cdc10
neiyichao03 <nieyichao03@kuaishou.com>
2021-11-01 13:19:04 +0800

    chore: update shortcut test

--- test/shortcuts.test.ts
+++ test/shortcuts.test.ts
@@ -4,9 +4,9 @@ describe('shortcuts', () => {
   const uno = createGenerator({
     shortcuts: [
       {
-        sh1: 'px-2 py-3',
+        sh1: 'px-2 py-3 sh3',
         sh2: 'hover:text-lg text-sm text-lg',
-        sh3: 'sm:m-2 m-3',
+        sh3: 'sm:m-2 m-3 sh1',
       },
       [/^button-(\d)$/, ([, d]) => [`px${(+d) * 3}`, `py${(+d) * 2}`]],
     ],
