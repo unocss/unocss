25a8fb681674a4df68e14299370bb09552d92d89
neiyichao03 <nieyichao03@kuaishou.com>
2021-11-02 19:01:04 +0800

    fix: export colorResolver from color.ts

--- packages/preset-uno/src/rules/behaviors.ts
+++ packages/preset-uno/src/rules/behaviors.ts
@@ -1,7 +1,6 @@
-import { hex2rgba, Rule, RuleContext } from '@unocss/core'
-import { Theme } from '../theme'
+import { Rule } from '@unocss/core'
 import { handler as h } from '../utils'
-import { extractColor } from './color'
+import { colorResolver } from './color'
 
 const listStyleProps = [
   'none', 'disc', 'circle', 'square',
@@ -30,59 +29,6 @@ export const listStyle: Rule[] = [
     }
   }],
 ]
-
-const colorResolver
-  = (attribute: string, varName: string) =>
-    ([, body]: string[], { theme }: RuleContext<Theme>) => {
-      const data = extractColor(body)
-
-      if (!data)
-        return
-
-      const { opacity, name, no, color } = data
-
-      if (!name)
-        return
-
-      let useColor = color
-
-      if (!color) {
-        if (name === 'transparent') {
-          return {
-            [attribute]: 'transparent',
-          }
-        }
-        else if (name === 'current') {
-          return {
-            [attribute]: 'currentColor',
-          }
-        }
-        useColor = theme.colors?.[name]
-        if (no && useColor && typeof useColor !== 'string')
-          useColor = useColor[no]
-      }
-
-      if (typeof useColor !== 'string')
-        return
-
-      const rgba = hex2rgba(useColor)
-      if (rgba) {
-        const a = opacity ? opacity[0] === '[' ? h.bracket.percent(opacity)! : (parseFloat(opacity) / 100) : rgba[3]
-        if (a != null && !Number.isNaN(a)) {
-          // @ts-expect-error
-          rgba[3] = typeof a === 'string' && !a.includes('%') ? parseFloat(a) : a
-          return {
-            [attribute]: `rgba(${rgba.join(',')})`,
-          }
-        }
-        else {
-          return {
-            [`--un-${varName}-opacity`]: 1,
-            [attribute]: `rgba(${rgba.slice(0, 3).join(',')},var(--un-${varName}-opacity))`,
-          }
-        }
-      }
-    }
 
 export const boxDecorationBreaks: Rule[] = [
   ['decoration-slice', { 'box-decoration-break': 'slice' }],
