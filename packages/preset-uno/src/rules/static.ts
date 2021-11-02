9cc7cb55ce1cafc1aeda36934f596353051a87c9
hanlee <me@hanlee.co>
2021-11-02 20:14:29 +0900

    feat: add unprefixed css for future usage

--- packages/preset-uno/src/rules/static.ts
+++ packages/preset-uno/src/rules/static.ts
@@ -107,10 +107,12 @@ export const fontSmoothings: Rule[] = [
   ['antialiased', {
     '-webkit-font-smoothing': 'antialiased',
     '-moz-osx-font-smoothing': 'grayscale',
+    'font-smoothing': 'grayscale',
   }],
   ['subpixel-antialiased', {
     '-webkit-font-smoothing': 'auto',
     '-moz-osx-font-smoothing': 'auto',
+    'font-smoothing': 'auto',
   }],
 ]
 
