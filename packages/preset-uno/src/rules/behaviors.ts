cfdc8b2c9378141334777e914ecc0bacaecb9e05
edwardnie <31278178+edwardnyc@users.noreply.github.com>
2021-11-05 08:20:02 +0800

    feat(preset-uno): outline utilities (#49)
    Co-authored-by: neiyichao03 <nieyichao03@kuaishou.com>
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/preset-uno/src/rules/behaviors.ts
+++ packages/preset-uno/src/rules/behaviors.ts
@@ -2,6 +2,66 @@ import { Rule } from '@unocss/core'
 import { handler as h } from '../utils'
 import { colorResolver } from './color'
 
+const outlineStyle = [
+  'none',
+  'auto',
+  'dotted',
+  'dashed',
+  'solid',
+  'double',
+  'groove',
+  'ridge',
+  'inset',
+  'outset',
+  'inherit',
+  'initial',
+  'revert',
+  'unset',
+]
+
+const parseOutlineSize = (s: string) => {
+  const propName = ['width', 'offset'].find(item => s.startsWith(item)) || 'width'
+  const size = h.bracket.fraction.rem((s.replace(/^(offset\-|width\-)/, '')))
+  if (size) {
+    return {
+      [`outline-${propName}`]: size,
+    }
+  }
+}
+
+export const outline: Rule[] = [
+  ['outline', { 'outline-style': 'solid' }],
+  [
+    /^outline-(.+)$/, (match, config) => {
+      const [, d] = match
+
+      if (d === 'none') {
+        return {
+          'outline': '2px solid transparent',
+          'outline-offset': '2px',
+        }
+      }
+
+      if (outlineStyle.includes(d)) {
+        return {
+          'outline-style': d,
+        }
+      }
+
+      const sizeSheet = parseOutlineSize(d)
+      if (sizeSheet)
+        return sizeSheet
+
+      const colorSheet = colorResolver('outline-color', 'outline-color')([
+        match[0],
+        match[1].replace(/^color-/, ''),
+      ], config)
+      if (colorSheet)
+        return colorSheet
+    },
+  ],
+]
+
 const listStyleProps = [
   'none', 'disc', 'circle', 'square',
   'decimal', 'zero-decimal', 'greek', 'roman',
