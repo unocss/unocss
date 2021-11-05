b6a15ba3b9fd6bdb939c82708f452fd63624dd08
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-05 08:11:00 +0800

    chore: update playground

--- playground/unocss.config.ts
+++ playground/unocss.config.ts
@@ -1,26 +1,30 @@
 import { defineConfig, presetAttributify, presetUno } from 'unocss'
 import presetIcons from '@unocss/preset-icons'
 
-export default defineConfig({
-  theme: {
-    fontFamily: {
-      sans: '\'Inter\', sans-serif',
-      mono: '\'Fira Code\', monospace',
-    },
-  },
-  presets: [
-    presetAttributify({ strict: true }),
-    presetIcons({
-      collections: {
-        carbon: () => import('@iconify-json/carbon/icons.json').then(i => i.default as any),
-        mdi: () => import('@iconify-json/mdi/icons.json').then(i => i.default as any),
-        logos: () => import('@iconify-json/logos/icons.json').then(i => i.default as any),
-        twemoji: () => import('@iconify-json/twemoji/icons.json').then(i => i.default as any),
-        ri: () => import('@iconify-json/ri/icons.json').then(i => i.default as any),
-        tabler: () => import('@iconify-json/tabler/icons.json').then(i => i.default as any),
-        uim: () => import('@iconify-json/uim/icons.json').then(i => i.default as any),
+export function createConfig({ strict = true, dev = true } = {}) {
+  return defineConfig({
+    theme: {
+      fontFamily: {
+        sans: '\'Inter\', sans-serif',
+        mono: '\'Fira Code\', monospace',
       },
-    }),
-    presetUno(),
-  ],
-})
+    },
+    presets: [
+      presetAttributify({ strict }),
+      presetIcons({
+        collections: {
+          carbon: () => import('@iconify-json/carbon/icons.json').then(i => i.default as any),
+          mdi: () => import('@iconify-json/mdi/icons.json').then(i => i.default as any),
+          logos: () => import('@iconify-json/logos/icons.json').then(i => i.default as any),
+          twemoji: () => import('@iconify-json/twemoji/icons.json').then(i => i.default as any),
+          ri: () => import('@iconify-json/ri/icons.json').then(i => i.default as any),
+          tabler: () => import('@iconify-json/tabler/icons.json').then(i => i.default as any),
+          uim: () => import('@iconify-json/uim/icons.json').then(i => i.default as any),
+        },
+      }),
+      presetUno({ dev }),
+    ],
+  })
+}
+
+export default createConfig()
