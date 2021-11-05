8574334c50984a6a55bb4892272ab212fb129c58
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-05 12:31:41 +0800

    feat: pug extractor, close #58

---
+++ packages/extractor-pug/README.md
@@ -0,0 +1,25 @@
+# @unocss/extractor-pug
+
+Pug extractor for UnoCSS.
+
+## Install
+
+```bash
+npm i -D @unocss/extractor-pug
+```
+
+```ts
+import extractorPug from '@unocss/extractor-pug'
+import { extractorSplit } from '@unocss/core'
+
+Unocss({
+  extractors: [
+    extractorPug(),
+    extractorSplit(),
+  ]
+})
+```
+
+## License
+
+MIT License © 2021-PRESENT [Anthony Fu](https://github.com/antfu)
---
+++ packages/extractor-pug/src/index.ts
@@ -0,0 +1,35 @@
+import type { Extractor } from '@unocss/core'
+
+const regexVueTemplate = /<template.*?lang=['"]pug['"][^>]*?>\n([\s\S]*?\n)<\/template>/gm
+
+export default function extractorPug(): Extractor {
+  async function compile(code: string, id: string) {
+    const Pug = await import('pug')
+    try {
+      return Pug.compile(code, { filename: id })()
+      // other build processes will catch pug errors
+    }
+    catch { }
+  }
+
+  return {
+    name: 'pug',
+    order: -1,
+    async extract(ctx) {
+      if (ctx.id?.match(/\.vue$/)) {
+        const matches = Array.from(ctx.code.matchAll(regexVueTemplate))
+        let tail = ''
+        for (const match of matches) {
+          if (match && match[1])
+            tail += `\n${await compile(match[1], ctx.id)}`
+        }
+        if (tail)
+          ctx.code = `${ctx.code}\n\n${tail}`
+      }
+      else if (ctx.id?.endsWith('.pug')) {
+        ctx.code = await compile(ctx.code, ctx.id) || ctx.code
+      }
+      return undefined
+    },
+  }
+}
