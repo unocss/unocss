fea9bb0fe0bca54c698c5d46f0ba9b8fac2f12e5
hanlee <me@hanlee.co>
2021-11-08 12:42:51 +0900

    chore(preset-uno): cleanup filter

--- packages/preset-uno/src/rules/filters.ts
+++ packages/preset-uno/src/rules/filters.ts
@@ -42,7 +42,6 @@ const backdropFilter = {
   '--un-backdrop-invert': varEmpty,
   '--un-backdrop-saturate': varEmpty,
   '--un-backdrop-sepia': varEmpty,
-  '--un-backdrop-drop-shadow': varEmpty,
   '-webkit-backdrop-filter': backdropFilterContent,
   'filter': backdropFilterContent,
 }
