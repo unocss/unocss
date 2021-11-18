9f08b48d6ed52831ebe090250f9ba182768cc5c8
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-19 02:48:47 +0800

    fix(pug): hmr, close #66

--- packages/extractor-pug/src/index.ts
+++ packages/extractor-pug/src/index.ts
@@ -16,18 +16,13 @@ export default function extractorPug(): Extractor {
     name: 'pug',
     order: -1,
     async extract(ctx) {
-      if (ctx.id?.match(/\.vue$/) || ctx.id?.match(/\.vue\?vue/)) {
-        const matches = Array.from(ctx.code.matchAll(regexVueTemplate))
-        let tail = ''
-        for (const match of matches) {
-          if (match && match[1])
-            tail += `\n${await compile(match[1], ctx.id)}`
+      if (!ctx.id)
+        return
+      if (ctx.id.endsWith('.pug') || ctx.id.match(/\.vue\?vue&type=template/)) {
+        try {
+          ctx.code = await compile(ctx.code, ctx.id) || ctx.code
         }
-        if (tail)
-          ctx.code = `${ctx.code}\n\n${tail}`
-      }
-      else if (ctx.id?.endsWith('.pug')) {
-        ctx.code = await compile(ctx.code, ctx.id) || ctx.code
+        catch {}
       }
       return undefined
     },
---
+++ test/fixtures/vite-pug/src/App.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+.font-mono.text-4xl.text-red4 Hello
+</template>
