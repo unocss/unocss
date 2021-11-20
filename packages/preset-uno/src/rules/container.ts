e2208fe7029c4bc35b24468004f3138fdb5945fb
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-11-20 12:38:41 +0100

    chore: rewrite container logic

--- packages/preset-uno/src/rules/container.ts
+++ packages/preset-uno/src/rules/container.ts
@@ -6,22 +6,26 @@ export const container: Rule<Theme>[] = [
     const bps = theme.breakpoints
 
     if (!bps)
-      return { width: '100%' }
+      return undefined
 
-    if (m.length > 1) {
-      const [,, useBp] = m
-      if (bps[useBp]) {
-        const keys = Array.from(Object.keys(bps))
-        const idx = keys.findIndex(cbp => cbp === useBp)
-        if (idx !== -1) {
-          return keys.slice(idx).map((bp, idx) => {
-            return `@media (min-width:${bps[bp]}) {.${useBp}\\:container,[${useBp}\\:container=""]{${idx === 0 ? 'width:100%;' : ''}max-width:${bps[bp]}}}`
-          }).join('\n')
-        }
+    const [,, useBp] = m
+
+    if (useBp && useBp.length > 0 && !bps[useBp])
+      return undefined
+
+    const keys = Array.from(Object.keys(bps))
+
+    if (bps[useBp]) {
+      const idx = keys.findIndex(cbp => cbp === useBp)
+      if (idx !== -1) {
+        return keys.slice(idx).map((bp, idx) => {
+          return `@media (min-width:${bps[bp]}) {.${useBp}\\:container,[${useBp}\\:container=""]{${idx === 0 ? 'width:100%;' : ''}max-width:${bps[bp]}}}`
+        }).join('\n')
       }
+      return undefined
     }
 
-    const entries = Object.keys(bps).map(bp => `@media (min-width:${bps[bp]}) {.container,[container=""]{max-width:${bps[bp]}}}`)
+    const entries = keys.map(bp => `@media (min-width:${bps[bp]}) {.container,[container=""]{max-width:${bps[bp]}}}`)
     return `.container,[container=""]{width:100%}\n${entries.join('\n')}`
   }],
 ]
