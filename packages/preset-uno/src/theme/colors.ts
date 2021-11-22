53f888ba0e97d88806638796b56c58425ab2ce08
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-22 16:23:14 +0800

    chore: update

--- packages/preset-uno/src/theme/colors.ts
+++ packages/preset-uno/src/theme/colors.ts
@@ -228,30 +228,7 @@ export const colors: Theme['colors'] = {
     800: '#1f2937',
     900: '#111827',
   },
-  light: {
-    50: '#fdfdfd',
-    100: '#fcfcfc',
-    200: '#fafafa',
-    300: '#f8f9fa',
-    400: '#f6f6f6',
-    500: '#f2f2f2',
-    600: '#f1f3f5',
-    700: '#e9ecef',
-    800: '#dee2e6',
-    900: '#dde1e3',
-  },
-  dark: {
-    50: '#4a4a4a',
-    100: '#3c3c3c',
-    200: '#323232',
-    300: '#2d2d2d',
-    400: '#222222',
-    500: '#1f1f1f',
-    600: '#1c1c1e',
-    700: '#1b1b1b',
-    800: '#181818',
-    900: '#0f0f0f',
-  },
+
   slate: {
     50: '#f8fafc',
     100: '#f1f5f9',
@@ -300,17 +277,56 @@ export const colors: Theme['colors'] = {
     800: '#292524',
     900: '#1c1917',
   },
+  light: {
+    50: '#fdfdfd',
+    100: '#fcfcfc',
+    200: '#fafafa',
+    300: '#f8f9fa',
+    400: '#f6f6f6',
+    500: '#f2f2f2',
+    600: '#f1f3f5',
+    700: '#e9ecef',
+    800: '#dee2e6',
+    900: '#dde1e3',
+  },
+  dark: {
+    50: '#4a4a4a',
+    100: '#3c3c3c',
+    200: '#323232',
+    300: '#2d2d2d',
+    400: '#222222',
+    500: '#1f1f1f',
+    600: '#1c1c1e',
+    700: '#1b1b1b',
+    800: '#181818',
+    900: '#0f0f0f',
+  },
+  get lightblue() {
+    return this.sky
+  },
   get lightBlue() {
     return this.sky
+  },
+  get warmgray() {
+    return this.stone
   },
   get warmGray() {
     return this.stone
   },
+  get truegray() {
+    return this.neutral
+  },
   get trueGray() {
     return this.neutral
   },
+  get coolgray() {
+    return this.gray
+  },
   get coolGray() {
     return this.gray
+  },
+  get bluegray() {
+    return this.slate
   },
   get blueGray() {
     return this.slate
