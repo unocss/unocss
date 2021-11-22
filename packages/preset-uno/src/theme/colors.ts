eaa502ecfb2e6289c300a7b8b960f9e32bd9ea3d
chris-zhu <42139754+chris-zhu@users.noreply.github.com>
2021-11-22 16:23:36 +0800

    feat(preset-uno): theme colors align with tw v3 (#141)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/preset-uno/src/theme/colors.ts
+++ packages/preset-uno/src/theme/colors.ts
@@ -216,43 +216,7 @@ export const colors: Theme['colors'] = {
     800: '#991b1b',
     900: '#7f1d1d',
   },
-  warmgray: {
-    50: '#fafaf9',
-    100: '#f5f5f4',
-    200: '#e7e5e4',
-    300: '#d6d3d1',
-    400: '#a8a29e',
-    500: '#78716c',
-    600: '#57534e',
-    700: '#44403c',
-    800: '#292524',
-    900: '#1c1917',
-  },
-  truegray: {
-    50: '#fafafa',
-    100: '#f5f5f5',
-    200: '#e5e5e5',
-    300: '#d4d4d4',
-    400: '#a3a3a3',
-    500: '#737373',
-    600: '#525252',
-    700: '#404040',
-    800: '#262626',
-    900: '#171717',
-  },
   gray: {
-    50: '#fafafa',
-    100: '#f4f4f5',
-    200: '#e4e4e7',
-    300: '#d4d4d8',
-    400: '#a1a1aa',
-    500: '#71717a',
-    600: '#52525b',
-    700: '#3f3f46',
-    800: '#27272a',
-    900: '#18181b',
-  },
-  coolgray: {
     50: '#f9fafb',
     100: '#f3f4f6',
     200: '#e5e7eb',
@@ -264,7 +228,8 @@ export const colors: Theme['colors'] = {
     800: '#1f2937',
     900: '#111827',
   },
-  bluegray: {
+
+  slate: {
     50: '#f8fafc',
     100: '#f1f5f9',
     200: '#e2e8f0',
@@ -275,6 +240,42 @@ export const colors: Theme['colors'] = {
     700: '#334155',
     800: '#1e293b',
     900: '#0f172a',
+  },
+  zinc: {
+    50: '#fafafa',
+    100: '#f4f4f5',
+    200: '#e4e4e7',
+    300: '#d4d4d8',
+    400: '#a1a1aa',
+    500: '#71717a',
+    600: '#52525b',
+    700: '#3f3f46',
+    800: '#27272a',
+    900: '#18181b',
+  },
+  neutral: {
+    50: '#fafafa',
+    100: '#f5f5f5',
+    200: '#e5e5e5',
+    300: '#d4d4d4',
+    400: '#a3a3a3',
+    500: '#737373',
+    600: '#525252',
+    700: '#404040',
+    800: '#262626',
+    900: '#171717',
+  },
+  stone: {
+    50: '#fafaf9',
+    100: '#f5f5f4',
+    200: '#e7e5e4',
+    300: '#d6d3d1',
+    400: '#a8a29e',
+    500: '#78716c',
+    600: '#57534e',
+    700: '#44403c',
+    800: '#292524',
+    900: '#1c1917',
   },
   light: {
     50: '#fdfdfd',
@@ -299,6 +300,36 @@ export const colors: Theme['colors'] = {
     700: '#1b1b1b',
     800: '#181818',
     900: '#0f0f0f',
+  },
+  get lightblue() {
+    return this.sky
+  },
+  get lightBlue() {
+    return this.sky
+  },
+  get warmgray() {
+    return this.stone
+  },
+  get warmGray() {
+    return this.stone
+  },
+  get truegray() {
+    return this.neutral
+  },
+  get trueGray() {
+    return this.neutral
+  },
+  get coolgray() {
+    return this.gray
+  },
+  get coolGray() {
+    return this.gray
+  },
+  get bluegray() {
+    return this.slate
+  },
+  get blueGray() {
+    return this.slate
   },
 }
 
