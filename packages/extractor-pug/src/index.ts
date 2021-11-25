b4a248a6d2ee655477b2c50613804028977a7fe4
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-25 09:58:18 +0800

    fix(pug): vue <script setup> build, close #168

--- packages/extractor-pug/src/index.ts
+++ packages/extractor-pug/src/index.ts
@@ -1,5 +1,7 @@
 import type { Extractor } from '@unocss/core'
 
+const regexVueTemplate = /<template.*?lang=['"]pug['"][^>]*?>\n([\s\S]*?\n)<\/template>/gm
+
 export default function extractorPug(): Extractor {
   async function compile(code: string, id: string) {
     const Pug = await import('pug')
@@ -16,11 +18,21 @@ export default function extractorPug(): Extractor {
     async extract(ctx) {
       if (!ctx.id)
         return
-      if (ctx.id.endsWith('.pug') || ctx.id.match(/\.vue\?vue&type=template/)) {
+      if (ctx.id.match(/\.pug$/) || ctx.id.match(/\?vue&type=template/)) {
         try {
           ctx.code = await compile(ctx.code, ctx.id) || ctx.code
         }
         catch {}
+      }
+      else if (ctx.id.match(/\.vue$/)) {
+        const matches = Array.from(ctx.code.matchAll(regexVueTemplate))
+        let tail = ''
+        for (const match of matches) {
+          if (match && match[1])
+            tail += `\n${await compile(match[1], ctx.id)}`
+        }
+        if (tail)
+          ctx.code = `${ctx.code}\n\n${tail}`
       }
       return undefined
     },
