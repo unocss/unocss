6d7e5084c916993c5e7a3ece77717c246c81d9f2
Saya <chu121su12@gmail.com>
2021-12-01 07:16:42 +0800

    revert encodeSvg

--- packages/preset-icons/src/utils.ts
+++ packages/preset-icons/src/utils.ts
@@ -1,19 +1,13 @@
 export const isNode = typeof process < 'u' && typeof process.stdout < 'u'
 
-const svgCharRe: [RegExp, string][] = [
-    [/"/g, '\''],
-    [/%/g, '%25'],
-    [/#/g, '%23'],
-    [/{/g, '%7B'],
-    [/}/g, '%7D'],
-    [/</g, '%3C'],
-    [/>/g, '%3E'],
-]
-
 // https://bl.ocks.org/jennyknuth/222825e315d45a738ed9d6e04c7a88d0
 export function encodeSvg(svg: string) {
-  return svgCharRe.reduce(
-    (str, [regexp, replacement]) => str.replace(regexp, replacement),
-    svg.replace('<svg', (~svg.indexOf('xmlns') ? '<svg' : '<svg xmlns="http://www.w3.org/2000/svg"'))
-  )
+  return svg.replace('<svg', (~svg.indexOf('xmlns') ? '<svg' : '<svg xmlns="http://www.w3.org/2000/svg"'))
+    .replace(/"/g, '\'')
+    .replace(/%/g, '%25')
+    .replace(/#/g, '%23')
+    .replace(/{/g, '%7B')
+    .replace(/}/g, '%7D')
+    .replace(/</g, '%3C')
+    .replace(/>/g, '%3E')
 }
