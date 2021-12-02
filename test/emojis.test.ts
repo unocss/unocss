ab118f3decea2790e38488d54ccb35c12d8f029e
Unai Mengual <owlnai@outlook.com>
2021-12-02 13:48:06 +0100

    fix: improve selector regex (#196)

---
+++ test/emojis.test.ts
@@ -0,0 +1,31 @@
+import { createGenerator } from '@unocss/core'
+import presetAttributify from '@unocss/preset-attributify'
+
+describe('emojis', () => {
+  const fixture1 = `
+    <button 
+    🦉 class="🦉" 🦉="1" 🥝-2 type="button"
+    >
+    Button
+    </button>
+  `
+  const uno = createGenerator({
+    presets: [
+      presetAttributify({ strict: true }),
+    ],
+    rules: [
+      ['🦉', {
+        color: 'red',
+      }],
+    ],
+  })
+
+  test('extractor1', async() => {
+    expect(await uno.applyExtractors(fixture1)).toMatchSnapshot()
+  })
+
+  test('fixture1', async() => {
+    const { css } = await uno.generate(fixture1)
+    expect(css).toMatchSnapshot()
+  })
+})
