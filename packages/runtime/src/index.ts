92a0b5c8e4a9002faefe592f9bfa9b38e68d8190
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-03 17:27:17 +0800

    fix(runtime): improve extracting triggering

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -2,13 +2,6 @@ import { UserConfigDefaults, createGenerator, UserConfig } from '@unocss/core'
 
 export interface RuntimeOptions {
   /**
-   * Timing for initiation
-   *
-   * @default 'pageload'
-   */
-  initializeAt?: 'pageload' | 'immediate'
-
-  /**
    * Default config of UnoCSS
    */
   defaults?: UserConfigDefaults
@@ -21,62 +14,64 @@ declare global {
 }
 
 export default function init(options: RuntimeOptions = {}) {
-  if (typeof window == 'undefined')
+  if (typeof window == 'undefined') {
+    console.warn('@unocss/runtime been used in non-browser environment, skipped.')
     return
+  }
 
   Object.assign(options, window.__unocss?.runtime)
 
-  if (options.initializeAt === 'immediate')
-    activate()
-  else
-    window.addEventListener('load', activate)
+  let el: HTMLStyleElement | undefined
 
-  function activate() {
-    const el = document.createElement('style')
-    document.head.appendChild(el)
+  const uno = createGenerator(window.__unocss || {}, options.defaults)
+  const tokens = new Set<string>()
 
-    const uno = createGenerator(window.__unocss || {}, options.defaults)
-    const tokens = new Set<string>()
-
-    let _timer: number | undefined
-    function scheduleUpdate() {
-      if (_timer != null)
-        clearTimeout(_timer)
-      _timer = setTimeout(updateStyle, 0) as any
-    }
-
-    async function updateStyle() {
-      const result = await uno.generate(tokens)
-      el.innerHTML = result.css
-    }
-
-    async function extract(str: string) {
-      await uno.applyExtractors(str, undefined, tokens)
-      scheduleUpdate()
-    }
-
-    function extractAll() {
-      extract(document.body.outerHTML)
-    }
-
-    const mutationObserver = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        const target = mutation.target as Element
-        if (target === el) return
-        const attrs = Array.from(target.attributes)
-          .map(i => i.value ? `${i.name}="${i.value}"` : i.name)
-          .join(' ')
-        const tag = `<${target.tagName.toLowerCase()} ${attrs}>`
-        extract(tag)
-      })
-    })
-
-    mutationObserver.observe(document.documentElement || document.body, {
-      childList: true,
-      subtree: true,
-      attributes: true,
-    })
-
-    extractAll()
+  let _timer: number | undefined
+  function scheduleUpdate() {
+    if (_timer != null)
+      clearTimeout(_timer)
+    _timer = setTimeout(updateStyle, 0) as any
   }
+
+  async function updateStyle() {
+    const result = await uno.generate(tokens)
+    if (!el) {
+      el = document.createElement('style')
+      document.head.appendChild(el)
+    }
+    el.innerHTML = result.css
+  }
+
+  async function extract(str: string) {
+    await uno.applyExtractors(str, undefined, tokens)
+    scheduleUpdate()
+  }
+
+  function extractAll() {
+    if (document.body && document.body.outerHTML)
+      extract(document.body.outerHTML)
+  }
+
+  const mutationObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      const target = mutation.target as Element
+      if (target === el)
+        return
+      const attrs = Array.from(target.attributes)
+        .map(i => i.value ? `${i.name}="${i.value}"` : i.name)
+        .join(' ')
+      const tag = `<${target.tagName.toLowerCase()} ${attrs}>`
+      extract(tag)
+    })
+  })
+
+  mutationObserver.observe(document.documentElement || document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+  })
+
+  extractAll()
+  window.addEventListener('load', extractAll)
+  window.addEventListener('DOMContentLoaded', extractAll)
 }
