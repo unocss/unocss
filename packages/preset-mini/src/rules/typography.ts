6a67291971cce152ab5d65a3c34835906372bd38
Saya <chu121su12@gmail.com>
2021-12-06 08:35:13 +0800

    rule

--- packages/preset-mini/src/rules/typography.ts
+++ packages/preset-mini/src/rules/typography.ts
@@ -41,6 +41,11 @@ export const fontSizes: Rule<Theme>[] = [
       }
     }
   }],
+  [/^font-size-(.+)$/, ([, s]) => {
+    const raw = h.bracket.rem(s)
+    if (raw)
+      return { 'font-size': raw }
+  }],
 ]
 
 export const fontWeights: Rule[] = [
