209f94953e54c9a4830a5a14afd69a90c39b5e84
Saya <chu121su12@gmail.com>
2021-12-08 15:47:24 +0800

    feat: handler helper (#234)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/preset-mini/src/utils/handlers/index.ts
+++ packages/preset-mini/src/utils/handlers/index.ts
@@ -1,2 +1,7 @@
-export * from './handlers'
-export * from './shorthand'
+import { createValueHandler } from '@unocss/core'
+import * as valueHandlers from './handlers'
+
+export const handler = createValueHandler(valueHandlers)
+export const h = handler
+
+export { valueHandlers }
