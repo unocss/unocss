75b02409de93ff703ecc2ba293ad873c0495dc7c
Saya <chu121su12@gmail.com>
2021-12-10 14:08:18 +0800

    split rules for use in preset-wind

--- packages/preset-mini/src/rules/flex.ts
+++ packages/preset-mini/src/rules/flex.ts
@@ -1,23 +1,31 @@
 import { Rule } from '@unocss/core'
 
-export const flex: Rule[] = [
-  ['flex-col', { 'flex-direction': 'column' }],
-  ['flex-col-reverse', { 'flex-direction': 'column-reverse' }],
-  ['flex-row', { 'flex-direction': 'row' }],
-  ['flex-row-reverse', { 'flex-direction': 'row-reverse' }],
-  ['flex-wrap', { 'flex-wrap': 'wrap' }],
-  ['flex-wrap-reverse', { 'flex-wrap': 'wrap-reverse' }],
-  ['flex-nowrap', { 'flex-wrap': 'nowrap' }],
+export const flexBase: Rule[] = [
+  ['flex', { display: 'flex' }],
+  ['inline-flex', { display: 'inline-flex' }],
+  ['flex-inline', { display: 'inline-flex' }],
   ['flex-1', { flex: '1 1 0%' }],
   ['flex-auto', { flex: '1 1 auto' }],
   ['flex-initial', { flex: '0 1 auto' }],
   ['flex-none', { flex: 'none' }],
   [/^flex-\[(.+)\]$/, ([, d]) => ({ flex: d })],
-  ['flex-grow', { 'flex-grow': 1 }],
-  ['flex-grow-0', { 'flex-grow': 0 }],
   ['flex-shrink', { 'flex-shrink': 1 }],
   ['flex-shrink-0', { 'flex-shrink': 0 }],
-  ['flex', { display: 'flex' }],
-  ['inline-flex', { display: 'inline-flex' }],
-  ['flex-inline', { display: 'inline-flex' }],
+  ['flex-grow', { 'flex-grow': 1 }],
+  ['flex-grow-0', { 'flex-grow': 0 }],
 ]
+
+export const flexDirections: Rule[] = [
+  ['flex-row', { 'flex-direction': 'row' }],
+  ['flex-row-reverse', { 'flex-direction': 'row-reverse' }],
+  ['flex-col', { 'flex-direction': 'column' }],
+  ['flex-col-reverse', { 'flex-direction': 'column-reverse' }],
+  ['flex-wrap', { 'flex-wrap': 'wrap' }],
+  ['flex-wrap-reverse', { 'flex-wrap': 'wrap-reverse' }],
+  ['flex-nowrap', { 'flex-wrap': 'nowrap' }],
+]
+
+export const flex = [
+  flexBase,
+  flexDirections,
+].flat(1)
