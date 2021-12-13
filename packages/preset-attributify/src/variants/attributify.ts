7019f6ce8fd78089a31973d621f2d409787670e9
Saya <chu121su12@gmail.com>
2021-12-13 10:59:55 +0800

    refactor: attributify variant

---
+++ packages/preset-attributify/src/variants/attributify.ts
@@ -0,0 +1,28 @@
+import type { VariantFunction } from '@unocss/core'
+import { isAttributifySelector } from '@unocss/core'
+import type { AttributifyOptions } from '../types'
+
+const variantsRE = /^(.+\:\!?)?(.*?)$/
+
+export const variantAttributify = (options: AttributifyOptions = {}): VariantFunction => {
+  const prefix = options.prefix ?? 'un-'
+
+  return (input) => {
+    const match = isAttributifySelector(input)
+    if (!match)
+      return
+
+    let name = match[1]
+    if (name.startsWith(prefix))
+      name = name.slice(prefix.length)
+    else if (options.prefixedOnly)
+      return
+
+    const content = match[2]
+    const [, variants = '', body = content] = content.match(variantsRE) || []
+    if (body === '~' || !body)
+      return `${variants}${name}`
+    else
+      return `${variants}${name}-${body}`
+  }
+}
