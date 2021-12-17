f7e48cac971f5b649b21828b9447d6e258e6913b
qiheizhiya <953136447@qq.com>
2021-12-17 23:30:41 +0800

    Modify the order

--- packages/preset-mini/src/rules/transition.ts
+++ packages/preset-mini/src/rules/transition.ts
@@ -28,8 +28,8 @@ export const transitions: Rule[] = [
       'transition-duration': `${duration}ms`,
     }
   }],
-  ['ease-linear', { 'transition-timing-function': 'linear' }],
   ['ease', { 'transition-timing-function': 'cubic-bezier(0.4, 0, 0.2, 1)' }],
+  ['ease-linear', { 'transition-timing-function': 'linear' }],
   ['ease-in', { 'transition-timing-function': 'cubic-bezier(0.4, 0, 1, 1)' }],
   ['ease-out', { 'transition-timing-function': 'cubic-bezier(0, 0, 0.2, 1)' }],
   ['ease-in-out', { 'transition-timing-function': 'cubic-bezier(0.4, 0, 0.2, 1)' }],
