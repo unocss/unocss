97d04e002a27be9237b3c0607406481988e87d7d
Saya <chu121su12@gmail.com>
2021-12-22 02:48:02 +0800

    lint

--- packages/preset-mini/src/variants/default.ts
+++ packages/preset-mini/src/variants/default.ts
@@ -1,12 +1,11 @@
 import type { Variant } from '@unocss/core'
-import { variantTaggedPseudoClasses } from '.'
 import type { PresetMiniOptions } from '..'
 import type { Theme } from '../theme'
 import { variantBreakpoints } from './breakpoints'
 import { variantCombinators } from './combinators'
 import { variantColorsClass, variantColorsMedia } from './dark'
 import { variantImportant, variantNegative, variantSpace } from './misc'
-import { partClasses, variantPseudoClasses, variantPseudoElements } from './pseudo'
+import { partClasses, variantPseudoClasses, variantPseudoElements, variantTaggedPseudoClasses } from './pseudo'
 
 export const variants = (options: PresetMiniOptions): Variant<Theme>[] => [
   variantSpace,
