bbe60e844254e305c9374c38a6e8d9dd1d6966e2
Saya <chu121su12@gmail.com>
2021-12-28 12:04:39 +0800

    remove CSSEntries

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -11,7 +11,7 @@ export type PartialByKeys<T, K extends keyof T = keyof T> = FlatObjectTuple<Part
 export type RequiredByKey<T, K extends keyof T = keyof T> = FlatObjectTuple<Required<Pick<T, Extract<keyof T, K>>> & Omit<T, K>>
 
 export type CSSObject = Record<string, string | number | undefined>
-export type CSSEntries = [string, string | number | undefined][]
+export type CSSEntry = [string, string | number | undefined]
 
 export type RGBAColorValue = [number, number, number, number] | [number, number, number]
 export type ParsedColorValue = {
