ba1c25abbf7138effdde5889ecad8f8abb4374f5
Saya <chu121su12@gmail.com>
2021-12-30 12:29:48 +0800

    feat(preset-mini)!: add variantParentMatcher utility

--- packages/preset-mini/src/variants/dark.ts
+++ packages/preset-mini/src/variants/dark.ts
@@ -1,41 +1,21 @@
 import type { Variant } from '@unocss/core'
-import { variantMatcher } from '../utils'
+import { variantMatcher, variantParentMatcher } from '../utils'
 
 export const variantColorsMediaOrClass: Variant[] = [
   (v, { options: { dark } }) => {
-    if (dark === 'class') {
-      const match = variantMatcher('dark', input => `.dark $$ ${input}`)(v)
-      if (match)
-        return match
-    }
+    if (dark === 'class')
+      return variantMatcher('dark', input => `.dark $$ ${input}`)(v)
   },
   (v, { options: { dark } }) => {
-    if (dark === 'class') {
-      const match = variantMatcher('light', input => `.light $$ ${input}`)(v)
-      if (match)
-        return match
-    }
+    if (dark === 'class')
+      return variantMatcher('light', input => `.light $$ ${input}`)(v)
   },
   (v, { options: { dark } }) => {
-    if (dark === 'media') {
-      const match = variantMatcher('dark')(v)
-      if (match) {
-        return {
-          ...match,
-          parent: '@media (prefers-color-scheme: dark)',
-        }
-      }
-    }
+    if (dark === 'media')
+      return variantParentMatcher('dark', '@media (prefers-color-scheme: dark)')(v)
   },
   (v, { options: { dark } }) => {
-    if (dark === 'media') {
-      const match = variantMatcher('light')(v)
-      if (match) {
-        return {
-          ...match,
-          parent: '@media (prefers-color-scheme: light)',
-        }
-      }
-    }
+    if (dark === 'media')
+      return variantParentMatcher('light', '@media (prefers-color-scheme: light)')(v)
   },
 ]
--- packages/preset-wind/src/variants/dark.ts
+++ packages/preset-wind/src/variants/dark.ts
@@ -1,26 +1,9 @@
 import type { Variant } from '@unocss/core'
-import { variantMatcher } from '@unocss/preset-mini/utils'
+import { variantMatcher, variantParentMatcher } from '@unocss/preset-mini/utils'
 
 export const variantColorsScheme: Variant[] = [
-  variantMatcher('\\.dark', input => `.dark $$ ${input}`),
-  variantMatcher('\\.light', input => `.light $$ ${input}`),
-
-  (v) => {
-    const match = variantMatcher('@dark')(v)
-    if (match) {
-      return {
-        ...match,
-        parent: '@media (prefers-color-scheme: dark)',
-      }
-    }
-  },
-  (v) => {
-    const match = variantMatcher('@light')(v)
-    if (match) {
-      return {
-        ...match,
-        parent: '@media (prefers-color-scheme: light)',
-      }
-    }
-  },
+  variantMatcher('.dark', input => `.dark $$ ${input}`),
+  variantMatcher('.light', input => `.light $$ ${input}`),
+  variantParentMatcher('@dark', '@media (prefers-color-scheme: dark)'),
+  variantParentMatcher('@light', '@media (prefers-color-scheme: light)'),
 ]
