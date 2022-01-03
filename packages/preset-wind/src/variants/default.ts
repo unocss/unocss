ce928b7f76f56bad476bcbf99ba322e4a39992e3
Saya <chu121su12@gmail.com>
2022-01-03 17:10:38 +0800

    refactor(preset-wind): move space & divide variant into wind (#383)

--- packages/preset-wind/src/variants/default.ts
+++ packages/preset-wind/src/variants/default.ts
@@ -2,8 +2,10 @@ import type { Variant } from '@unocss/core'
 import type { Theme } from '@unocss/preset-mini'
 import { variants as miniVariants } from '@unocss/preset-mini/variants'
 import { variantColorsScheme } from './dark'
+import { variantSpaceAndDivide } from './misc'
 
 export const variants: Variant<Theme>[] = [
+  variantSpaceAndDivide,
   ...miniVariants,
   ...variantColorsScheme,
 ]
