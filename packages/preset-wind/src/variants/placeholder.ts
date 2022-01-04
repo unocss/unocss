9d3c09780ee432963c39b744e01341698180aa71
Saya <chu121su12@gmail.com>
2022-01-04 21:05:22 +0800

    fix opacity

--- packages/preset-wind/src/variants/placeholder.ts
+++ packages/preset-wind/src/variants/placeholder.ts
@@ -1,13 +1,25 @@
 import type { VariantFunction } from '@unocss/core'
+import type { Theme } from '@unocss/preset-mini'
 import { handler as h, parseColor } from '@unocss/preset-mini/utils'
 
 export const variantPseudoPlaceholder: VariantFunction = (input: string, { theme }) => {
   const match = input.match(/^placeholder-(.+?)$/)
   if (match) {
-    if (!!parseColor(match[1], theme)?.color || (match[1].match(/^op(?:acity)?-?(.+)$/) && h.bracket.percent.cssvar(match[1]) != null)) {
+    if (hasColorValue(match[1], theme) || hasOpacityValue(match[1])) {
       return {
         matcher: input.replace(/-/, '-$-placeholder-'),
       }
     }
   }
+}
+
+function hasColorValue(body: string, theme: Theme) {
+  return !!parseColor(body, theme)?.color
+}
+
+function hasOpacityValue(body: string) {
+  const match = body.match(/^op(?:acity)?-?(.+)$/)
+  if (match[1] != null)
+    return h.bracket.percent.cssvar(match[1]) != null
+  return false
 }
