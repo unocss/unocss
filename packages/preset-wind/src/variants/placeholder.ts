d65dd09116cb049b60d5febdfcc65943e0e11f14
Saya <chu121su12@gmail.com>
2022-01-04 20:30:57 +0800

    check for opacity value

--- packages/preset-wind/src/variants/placeholder.ts
+++ packages/preset-wind/src/variants/placeholder.ts
@@ -4,7 +4,7 @@ import { handler as h, parseColor } from '@unocss/preset-mini/utils'
 export const variantPseudoPlaceholder: VariantFunction = (input: string, { theme }) => {
   const match = input.match(/^placeholder-(.+?)$/)
   if (match) {
-    if (!!parseColor(match[1], theme)?.color || match[1].match(/^op(?:acity)?-?(.+)$/)) {
+    if (!!parseColor(match[1], theme)?.color || (match[1].match(/^op(?:acity)?-?(.+)$/) && h.bracket.percent.cssvar(match[1]) != null)) {
       return {
         matcher: input.replace(/-/, '-$-placeholder-'),
       }
