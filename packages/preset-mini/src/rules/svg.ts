38b75f987ba89758b3b9269deaf7e1b02025d19b
Saya <chu121su12@gmail.com>
2022-01-06 11:18:55 +0800

    feat(preset-mini): add svg stroke-x from windi

--- packages/preset-mini/src/rules/svg.ts
+++ packages/preset-mini/src/rules/svg.ts
@@ -10,8 +10,26 @@ export const svgUtilities: Rule[] = [
   // stroke size
   [/^stroke-(?:size-|width-)?(.+)$/, ([, s]) => ({ 'stroke-width': h.bracket.fraction.px.number(s) })],
 
+  // stroke dash
+  [/^stroke-dash-(.+)$/, ([, s]) => ({ 'stroke-dasharray': h.bracket.number(s) })],
+  [/^stroke-offset-(.+)$/, ([, s]) => ({ 'stroke-dashoffset': h.bracket.px.numberWithUnit(s) })],
+
   // stroke colors
   [/^stroke-(.+)$/, colorResolver('stroke', 'stroke')],
   [/^stroke-op(?:acity)?-?(.+)$/, ([, opacity]) => ({ '--un-stroke-opacity': h.bracket.percent(opacity) })],
+
+  // line cap
+  ['stroke-cap-square', { 'stroke-linecap': 'square' }],
+  ['stroke-cap-round', { 'stroke-linecap': 'round' }],
+  ['stroke-cap-auto', { 'stroke-linecap': 'butt' }],
+
+  // line join
+  ['stroke-join-arcs', { 'stroke-linejoin': 'arcs' }],
+  ['stroke-join-bevel', { 'stroke-linejoin': 'bevel' }],
+  ['stroke-join-clip', { 'stroke-linejoin': 'miter-clip' }],
+  ['stroke-join-round', { 'stroke-linejoin': 'round' }],
+  ['stroke-join-auto', { 'stroke-linejoin': 'miter' }],
+
+  // none
   ['stroke-none', { stroke: 'none' }],
 ]
