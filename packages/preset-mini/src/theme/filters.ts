876b1866776fd0e713625aff129376e53fc6d824
Saya <chu121su12@gmail.com>
2022-01-12 03:13:12 +0800

    feat(preset-wind): fix drop-shadow value and add color & opacity (#452)

--- packages/preset-mini/src/theme/filters.ts
+++ packages/preset-mini/src/theme/filters.ts
@@ -10,11 +10,11 @@ export const blur = {
 }
 
 export const dropShadow = {
-  'DEFAULT': ['0 1px 2px rgba(0, 0, 0, 0.1)', '0 1px 1px rgba(0, 0, 0, 0.06)'],
-  'sm': '0 1px 1px rgba(0, 0, 0, 0.05)',
-  'md': ['0 4px 3px rgba(0, 0, 0, 0.07)', '0 2px 2px rgba(0, 0, 0, 0.06)'],
-  'lg': ['0 10px 8px rgba(0, 0, 0, 0.04)', '0 4px 3px rgba(0, 0, 0, 0.1)'],
-  'xl': ['0 20px 13px rgba(0, 0, 0, 0.03)', '0 8px 5px rgba(0, 0, 0, 0.08)'],
-  '2xl': '0 25px 25px rgba(0, 0, 0, 0.15)',
+  'DEFAULT': ['0 1px 2px rgba(0,0,0,0.1)', '0 1px 1px rgba(0,0,0,0.06)'],
+  'sm': '0 1px 1px rgba(0,0,0,0.05)',
+  'md': ['0 4px 3px rgba(0,0,0,0.07)', '0 2px 2px rgba(0,0,0,0.06)'],
+  'lg': ['0 10px 8px rgba(0,0,0,0.04)', '0 4px 3px rgba(0,0,0,0.1)'],
+  'xl': ['0 20px 13px rgba(0,0,0,0.03)', '0 8px 5px rgba(0,0,0,0.08)'],
+  '2xl': '0 25px 25px rgba(0,0,0,0.15)',
   'none': '0 0 #0000',
 }
