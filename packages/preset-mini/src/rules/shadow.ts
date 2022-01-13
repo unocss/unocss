5923c78d0c819c8082fde4e62576746ab2acffe5
Saya <chu121su12@gmail.com>
2022-01-13 19:57:50 +0800

    refactor(preset-mini,preset-wind): simplify shadow (#456)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/preset-mini/src/rules/shadow.ts
+++ packages/preset-mini/src/rules/shadow.ts
@@ -1,4 +1,4 @@
-import type { CSSObject, Rule } from '@unocss/core'
+import type { Rule } from '@unocss/core'
 import { CONTROL_SHORTCUT_NO_MERGE, toArray } from '@unocss/core'
 import type { Theme } from '../theme'
 import { colorResolver, handler as h } from '../utils'
@@ -10,20 +10,28 @@ export const shadowBase = {
   '--un-ring-shadow': '0 0 #0000',
   '--un-shadow-inset': varEmpty,
   '--un-shadow': '0 0 #0000',
-  '--un-shadow-colored': '0 0 #0000',
+}
+
+export const colorableShadows = (shadows: string | string[], colorVar: string) => {
+  const colored = []
+  shadows = toArray(shadows)
+  for (let i = 0; i < shadows.length; i++) {
+    const [size, color] = shadows[i].split(/\s(\S+)$/)
+    if (color.split('(').length !== color.split(')').length)
+      return shadows
+    colored.push(`${size} var(${colorVar}, ${color})`)
+  }
+  return colored
 }
 
 export const boxShadows: Rule<Theme>[] = [
   [/^shadow(?:-(.+))?$/, ([, d], { theme }) => {
     const v = theme.boxShadow?.[d || 'DEFAULT']
     if (v) {
-      const shadow = toArray(v)
-      const colored = shadow.map(s => s.replace(/\s\S+$/, ' var(--un-shadow-color)'))
       return [
         shadowBase,
         {
-          '--un-shadow': shadow.join(','),
-          '--un-shadow-colored': colored.join(','),
+          '--un-shadow': colorableShadows(v, '--un-shadow-color').join(','),
           'box-shadow': 'var(--un-ring-offset-shadow, 0 0 #0000), var(--un-ring-shadow, 0 0 #0000), var(--un-shadow)',
         },
       ]
@@ -31,15 +39,7 @@ export const boxShadows: Rule<Theme>[] = [
   }],
 
   // color
-  [/^shadow-(.+)$/, (m, ctx) => {
-    const color = colorResolver('--un-shadow-color', 'shadow')(m, ctx) as CSSObject | undefined
-    if (color) {
-      return {
-        ...color,
-        '--un-shadow': 'var(--un-shadow-colored)',
-      }
-    }
-  }],
+  [/^shadow-(.+)$/, colorResolver('--un-shadow-color', 'shadow')],
   [/^shadow-op(?:acity)?-?(.+)$/, ([, opacity]) => ({ '--un-shadow-opacity': h.bracket.percent.cssvar(opacity) })],
 
   // inset
