33651c82239b5d108a3168d49bd5e54deff42556
Saya <chu121su12@gmail.com>
2022-01-15 18:51:42 +0800

    lint

--- packages/preset-mini/src/rules/variables.ts
+++ packages/preset-mini/src/rules/variables.ts
@@ -2,18 +2,18 @@ import type { Rule } from '@unocss/core'
 import { handler as h } from '../utils'
 
 const variablesAbbrMap: Record<string, string> = {
-  'backface': 'backface-visibility',
-  'break': 'word-break',
-  'case': 'text-transform',
-  'content': 'align-content',
-  'items': 'align-items',
-  'justify': 'justify-content',
-  'object': 'object-fit',
-  'select': 'user-select',
-  'self': 'align-self',
-  'vertical': 'vertical-align',
-  'visible': 'visibility',
-  'whitespace': 'white-space',
+  backface: 'backface-visibility',
+  break: 'word-break',
+  case: 'text-transform',
+  content: 'align-content',
+  items: 'align-items',
+  justify: 'justify-content',
+  object: 'object-fit',
+  select: 'user-select',
+  self: 'align-self',
+  vertical: 'vertical-align',
+  visible: 'visibility',
+  whitespace: 'white-space',
 }
 
 export const cssVariables: Rule[] = [
