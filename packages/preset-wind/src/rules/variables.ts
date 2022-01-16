f434dd5ff72d7e4021733b7d38b72d92556a9f8e
Saya <chu121su12@gmail.com>
2022-01-16 19:08:25 +0800

    sort

--- packages/preset-wind/src/rules/variables.ts
+++ packages/preset-wind/src/rules/variables.ts
@@ -4,16 +4,16 @@ const variablesAbbrMap: Record<string, string> = {
   'bg-blend': 'background-blend-mode',
   'bg-clip': '-webkit-background-clip',
   'bg-gradient': 'linear-gradient',
+  'bg-image': 'background-image',
   'bg-origin': 'background-origin',
   'bg-position': 'background-position',
   'bg-repeat': 'background-repeat',
   'bg-size': 'background-size',
   'mix-blend': 'mix-blend-mode',
   'object': 'object-fit',
+  'object-position': 'object-position',
   'write': 'writing-mode',
   'write-orient': 'text-orientation',
-  'bg-image': 'background-image',
-  'object-position': 'object-position',
 }
 
 export const cssVariables: Rule[] = [[
