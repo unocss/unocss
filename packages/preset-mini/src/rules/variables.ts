30885055362b5bec6500c01c0caa04a06e9ebed1
Saya <chu121su12@gmail.com>
2022-01-21 13:34:34 +0800

    refactor(preset-mini): move more cssvar from variables to each rule (#507)

--- packages/preset-mini/src/rules/variables.ts
+++ packages/preset-mini/src/rules/variables.ts
@@ -2,24 +2,20 @@ import type { Rule } from '@unocss/core'
 import { handler as h } from '../utils'
 
 const variablesAbbrMap: Record<string, string> = {
-  'backface': 'backface-visibility',
-  'break': 'word-break',
-  'case': 'text-transform',
-  'content': 'align-content',
-  'flex': 'flex',
-  'fw': 'font-weight',
-  'items': 'align-items',
-  'justify': 'justify-content',
-  'leading': 'line-height',
-  'lh': 'line-height',
-  'select': 'user-select',
-  'self': 'align-self',
-  'tracking': 'letter-spacing',
-  'vertical': 'vertical-align',
-  'visible': 'visibility',
-  'whitespace': 'white-space',
-  'word-spacing': 'word-spacing',
-  'ws': 'white-space',
+  backface: 'backface-visibility',
+  break: 'word-break',
+  case: 'text-transform',
+  content: 'align-content',
+  flex: 'flex',
+  fw: 'font-weight',
+  items: 'align-items',
+  justify: 'justify-content',
+  select: 'user-select',
+  self: 'align-self',
+  vertical: 'vertical-align',
+  visible: 'visibility',
+  whitespace: 'white-space',
+  ws: 'white-space',
 }
 
 export const cssVariables: Rule[] = [
