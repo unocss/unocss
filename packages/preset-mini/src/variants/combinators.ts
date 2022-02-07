730121415cfea4f55b9e6f71d1bd99cef9ec5085
Saya <chu121su12@gmail.com>
2022-02-07 09:27:51 +0800

    feat(preset-mini): update/add variants-[x] that target x as raw selector (#568)

--- packages/preset-mini/src/variants/combinators.ts
+++ packages/preset-mini/src/variants/combinators.ts
@@ -1,11 +1,30 @@
-import type { Variant } from '@unocss/core'
+import type { Variant, VariantHandler } from '@unocss/core'
 import { variantMatcher } from '../utils'
 
+const scopeMatcher = (strict: boolean, name: string, template: string) => {
+  const re = strict
+    ? new RegExp(`^${name}(?:-\\[(.+?)\\])[:-]`)
+    : new RegExp(`^${name}(?:-\\[(.+?)\\])?[:-]`)
+  return (matcher: string): VariantHandler | undefined => {
+    const match = matcher.match(re)
+    if (match) {
+      return {
+        matcher: matcher.slice(match[0].length),
+        selector: s => template.replace('&&-s', s).replace('&&-c', match[1] ?? '*'),
+      }
+    }
+  }
+}
+
 export const variantCombinators: Variant[] = [
-  variantMatcher('all', input => `${input} *`),
-  variantMatcher('children', input => `${input}>*`),
-  variantMatcher('next', input => `${input}+*`),
-  variantMatcher('sibling', input => `${input}+*`),
-  variantMatcher('siblings', input => `${input}~*`),
+  scopeMatcher(false, 'all', '&&-s &&-c'),
+  scopeMatcher(false, 'children', '&&-s>&&-c'),
+  scopeMatcher(false, 'next', '&&-s+&&-c'),
+  scopeMatcher(false, 'sibling', '&&-s+&&-c'),
+  scopeMatcher(false, 'siblings', '&&-s~&&-c'),
+  scopeMatcher(true, 'group', '&&-c &&-s'),
+  scopeMatcher(true, 'parent', '&&-c>&&-s'),
+  scopeMatcher(true, 'previous', '&&-c+&&-s'),
+  scopeMatcher(true, 'peer', '&&-c~&&-s'),
   variantMatcher('svg', input => `${input} svg`),
 ]
