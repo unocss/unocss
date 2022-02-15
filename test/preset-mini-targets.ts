e32c343646cfce283c259fc8339237b140ef0bfb
Saya <chu121su12@gmail.com>
2022-02-15 12:14:53 +0800

    wip

--- test/preset-mini-targets.ts
+++ test/preset-mini-targets.ts
@@ -715,6 +715,7 @@ export const presetMiniTargets: string[] = [
 
   // variants media
   'media-cssvar:block',
+  'media-cssvar,media2:block',
 
   // variants motion
   'motion-reduce:hover:translate-0',
