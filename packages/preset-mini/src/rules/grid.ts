5d40c5ae8c2dd95006ef40923cd62d056aa00952
Saya <chu121su12@gmail.com>
2022-02-19 16:36:31 +0800

    feat(preset-mini): use theme for some grid values

--- packages/preset-mini/src/rules/grid.ts
+++ packages/preset-mini/src/rules/grid.ts
@@ -1,31 +1,33 @@
 import type { Rule } from '@unocss/core'
-import { toArray } from '@unocss/core'
 import type { Theme } from '../theme'
 import { handler as h } from '../utils'
 
 const rowCol = (s: string) => s.replace('col', 'column')
+const rowColTheme = (s: string) => s[0] === 'r' ? 'Row' : 'Column'
 
-const autoDirection = (selector: string, theme: Theme) => {
-  const v = theme.fontSize?.[selector]
+const autoDirection = (c: string, theme: Theme, prop: string) => {
+  const v = theme[`gridAuto${rowColTheme(c)}`]?.[prop]
   if (v != null)
-    return toArray(v)[0]
+    return v
 
-  switch (selector) {
+  switch (prop) {
     case 'min': return 'min-content'
     case 'max': return 'max-content'
     case 'fr': return 'minmax(0,1fr)'
   }
 
-  return h.bracket.cssvar.auto.rem(selector)
+  return h.bracket.cssvar.auto.rem(prop)
 }
 
-export const grids: Rule[] = [
+export const grids: Rule<Theme>[] = [
   // displays
   ['grid', { display: 'grid' }],
   ['inline-grid', { display: 'inline-grid' }],
 
   // global
-  [/^(?:grid-)?(row|col)-(.+)$/, ([, c, v]) => ({ [`grid-${rowCol(c)}`]: h.bracket.cssvar.auto(v) })],
+  [/^(?:grid-)?(row|col)-(.+)$/, ([, c, v], { theme }) => ({
+    [`grid-${rowCol(c)}`]: theme[`grid${rowColTheme(c)}`]?.[v] ?? h.bracket.cssvar.auto(v),
+  })],
 
   // span
   [/^(?:grid-)?(row|col)-span-(.+)$/, ([, c, s]) => {
@@ -41,7 +43,7 @@ export const grids: Rule[] = [
   [/^(?:grid-)?(row|col)-end-(.+)$/, ([, c, v]) => ({ [`grid-${rowCol(c)}-end`]: h.bracket.cssvar(v) ?? v })],
 
   // auto flows
-  [/^(?:grid-)?auto-(rows|cols)-(.+)$/, ([, c, v], { theme }) => ({ [`grid-auto-${rowCol(c)}`]: autoDirection(v, theme) })],
+  [/^(?:grid-)?auto-(rows|cols)-(.+)$/, ([, c, v], { theme }) => ({ [`grid-auto-${rowCol(c)}`]: autoDirection(c, theme, v) })],
 
   // grid-auto-flow, auto-flow: uno
   // grid-flow: wind
@@ -49,7 +51,9 @@ export const grids: Rule[] = [
   [/^(?:grid-auto-flow|auto-flow|grid-flow)-((?:row|col)(?:-dense)?)$/, ([, v]) => ({ 'grid-auto-flow': rowCol(v).replace('-', ' ') })],
 
   // templates
-  [/^grid-(rows|cols)-(.+)$/, ([, c, v]) => ({ [`grid-template-${rowCol(c)}`]: h.bracket.cssvar(v) })],
+  [/^grid-(rows|cols)-(.+)$/, ([, c, v], { theme }) => ({
+    [`grid-template-${rowCol(c)}`]: theme[`gridTemplate${rowColTheme(c)}`]?.[v] ?? h.bracket.cssvar(v),
+  })],
   [/^grid-(rows|cols)-minmax-([\w.-]+)$/, ([, c, d]) => ({ [`grid-template-${rowCol(c)}`]: `repeat(auto-fill,minmax(${d},1fr))` })],
   [/^grid-(rows|cols)-(\d+)$/, ([, c, d]) => ({ [`grid-template-${rowCol(c)}`]: `repeat(${d},minmax(0,1fr))` })],
 
