9bf1570850f538af8eab2da9ee6ea2a2c06ee7a9
Saya <chu121su12@gmail.com>
2022-02-21 19:44:14 +0800

    fix(preset-mini): fix ring utility (#626)
    * fix(preset-mini): fix ring utility
    
    - update default `ring` value from 1 to 3
    - fix ring overriding shadow when used together
    
    * chore: update
    
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/preset-mini/src/rules/ring.ts
+++ packages/preset-mini/src/rules/ring.ts
@@ -1,17 +1,8 @@
 import type { Rule } from '@unocss/core'
+import { CONTROL_SHORTCUT_NO_MERGE } from '@unocss/core'
 import type { Theme } from '../theme'
 import { colorResolver, handler as h } from '../utils'
-import { shadowBase } from './shadow'
 import { varEmpty } from './static'
-
-const ringBase = {
-  ...shadowBase,
-  '--un-ring-inset': varEmpty,
-  '--un-ring-offset-width': '0px',
-  '--un-ring-offset-color': '#fff',
-  '--un-ring-width': '0px',
-  '--un-ring-color': 'rgba(147,197,253,0.5)',
-}
 
 export const rings: Rule<Theme>[] = [
   // size
@@ -19,7 +10,14 @@ export const rings: Rule<Theme>[] = [
     const value = h.px(d || '1')
     if (value) {
       return [
-        ringBase,
+        {
+          [CONTROL_SHORTCUT_NO_MERGE]: '',
+          '--un-ring-inset': varEmpty,
+          '--un-ring-offset-width': '0px',
+          '--un-ring-offset-color': '#fff',
+          '--un-ring-width': '0px',
+          '--un-ring-color': 'rgba(147,197,253,0.5)',
+        },
         {
           '--un-ring-width': value,
           '--un-ring-offset-shadow': 'var(--un-ring-inset) 0 0 0 var(--un-ring-offset-width) var(--un-ring-offset-color)',
