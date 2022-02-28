fcfb8861c2355d13849cceb389b02460a25abbde
Verite Makuza Mugabo <mugaboverite@gmail.com>
2022-02-28 21:45:47 +0200

    chore: setup remix

---
+++ test/fixtures/remix/unocss.config.ts
@@ -0,0 +1,18 @@
+import presetAttributify from '@unocss/preset-attributify'
+import presetUno from "@unocss/preset-uno"
+import presetWebFonts from '@unocss/preset-web-fonts'
+import { defineConfig } from "unocss"
+
+
+export default defineConfig({
+    presets: [
+        presetUno(),
+        presetAttributify(),
+        presetWebFonts({
+            provider: 'google',
+            fonts: {
+                sans: "Roboto"
+            }
+        }),
+    ],
+})
