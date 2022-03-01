fe2fb3d07260b643aa2a2a98037202f6dff2c598
Verite Makuza Mugabo <mugaboverite@gmail.com>
2022-03-01 16:27:09 +0200

    chore: update

--- test/fixtures/remix/unocss.config.ts
+++ test/fixtures/remix/unocss.config.ts
@@ -1,18 +1,18 @@
 import presetAttributify from '@unocss/preset-attributify'
-import presetUno from "@unocss/preset-uno"
+import presetUno from '@unocss/preset-uno'
 import presetWebFonts from '@unocss/preset-web-fonts'
-import { defineConfig } from "unocss"
-
+// eslint-disable-next-line/no-restricted-imports
+import { defineConfig } from 'unocss'
 
 export default defineConfig({
-    presets: [
-        presetUno(),
-        presetAttributify(),
-        presetWebFonts({
-            provider: 'google',
-            fonts: {
-                sans: "Roboto"
-            }
-        }),
-    ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'Roboto',
+      },
+    }),
+  ],
 })
