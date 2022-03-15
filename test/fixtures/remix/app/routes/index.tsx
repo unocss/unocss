d039f8b8dfed78dc88a930a3487328863cb8e9e3
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-03-15 19:26:00 +0100

    test(remix): add logo icon

--- test/fixtures/remix/app/routes/index.tsx
+++ test/fixtures/remix/app/routes/index.tsx
@@ -1,6 +1,7 @@
 export default function Index() {
   return (
-    <div className="bg-red-500 py-6 text-center">
+    <div className="bg-red-500 py-6 text-center flex gap-x-1rem items-center p-1rem">
+      <span className="logo"></span>
       <h1 className="color-white text-4xl font-sans">Welcome to Remix</h1>
     </div>
   )
--- test/fixtures/remix/unocss.config.ts
+++ test/fixtures/remix/unocss.config.ts
@@ -1,10 +1,14 @@
 import presetAttributify from '@unocss/preset-attributify'
 import presetUno from '@unocss/preset-uno'
 import presetWebFonts from '@unocss/preset-web-fonts'
+import presetIcons from '@unocss/preset-icons'
 /* eslint-disable no-restricted-imports */
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
+  shortcuts: [
+    { logo: 'i-logos-remix-icon w-3em h-3em transform transition-800 hover:rotate-360' },
+  ],
   presets: [
     presetUno(),
     presetAttributify(),
@@ -14,5 +18,6 @@ export default defineConfig({
         sans: 'Roboto',
       },
     }),
+    presetIcons(),
   ],
 })
