104999bb841b8ee961882eec8892ebea038c053e
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-17 06:20:03 +0800

    fix(autocomplete): improve `searchUsageBoundary`

--- packages/autocomplete/src/utils.ts
+++ packages/autocomplete/src/utils.ts
@@ -1,8 +1,10 @@
 export function searchUsageBoundary(line: string, index: number) {
   let start = index
   let end = index
-  while (start && /[^\s"']/.test(line.charAt(start - 1))) --start
-  while (end < line.length && /[^\s"']/.test(line.charAt(end))) ++end
+
+  const regex = /[^\s>"'`]/
+  while (start && regex.test(line.charAt(start - 1))) --start
+  while (end < line.length && regex.test(line.charAt(end))) ++end
 
   return {
     content: line.slice(start, end),
