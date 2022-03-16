6e0813d3401581bd058a175853f803c99f8b1242
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-16 20:13:20 +0800

    feat: static autocomplete

--- packages/autocomplete/src/index.ts
+++ packages/autocomplete/src/index.ts
@@ -1,10 +1,21 @@
 import type { UnoGenerator } from '@unocss/core'
 
 export function createAutocomplete(uno: UnoGenerator) {
-  function suggest(input: string) {
-    return [
-      input,
-    ]
+  const staticUtils = Object.keys(uno.config.rulesStaticMap)
+
+  async function suggest(input: string) {
+    const results: string[] = []
+
+    await uno.parseToken(input, '-').then((i) => {
+      if (i)
+        results.push(input)
+    })
+
+    results.push(
+      ...staticUtils.filter(i => i.startsWith(input)),
+    )
+
+    return results
   }
 
   return {
--- test/autocomplete.test.ts
+++ test/autocomplete.test.ts
@@ -12,7 +12,48 @@ const uno = createGenerator({
 const ac = createAutocomplete(uno)
 
 describe('autocomplete', () => {
-  it('should work', () => {
-    expect(ac.suggest('m-1')[0]).toMatchInlineSnapshot('"m-1"')
+  it('should work', async() => {
+    expect((await ac.suggest('m-1'))[0])
+      .toMatchInlineSnapshot('"m-1"')
+
+    expect((await ac.suggest('invalid'))[0])
+      .toBeUndefined()
+  })
+
+  it('should provide static autocomplete', async() => {
+    expect((await ac.suggest('fle')))
+      .toMatchInlineSnapshot(`
+        [
+          "flex",
+          "flex-inline",
+          "flex-1",
+          "flex-auto",
+          "flex-initial",
+          "flex-none",
+          "flex-row",
+          "flex-row-reverse",
+          "flex-col",
+          "flex-col-reverse",
+          "flex-wrap",
+          "flex-wrap-reverse",
+          "flex-nowrap",
+        ]
+      `)
+  })
+
+  it('should provide dynamic autocomplete', async() => {
+    expect((await ac.suggest('border-')))
+      .toMatchInlineSnapshot(`
+        [
+          "border-collapse",
+          "border-separate",
+          "border-solid",
+          "border-dashed",
+          "border-dotted",
+          "border-double",
+          "border-hidden",
+          "border-none",
+        ]
+      `)
   })
 })
