b99c247cd41a594a034413322d1638d05591a083
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-16 20:19:41 +0800

    refactor: code

--- packages/autocomplete/src/index.ts
+++ packages/autocomplete/src/index.ts
@@ -4,18 +4,24 @@ export function createAutocomplete(uno: UnoGenerator) {
   const staticUtils = Object.keys(uno.config.rulesStaticMap)
 
   async function suggest(input: string) {
-    const results: string[] = []
+    return await Promise.all([
+      suggestSelf(input),
+      suggestStatic(input),
+      suggestDynamic(input),
+    ]).then(i => i.flat())
+  }
 
-    await uno.parseToken(input, '-').then((i) => {
-      if (i)
-        results.push(input)
-    })
+  async function suggestSelf(input: string) {
+    const i = await uno.parseToken(input, '-')
+    return i ? [input] : []
+  }
 
-    results.push(
-      ...staticUtils.filter(i => i.startsWith(input)),
-    )
+  async function suggestStatic(input: string) {
+    return staticUtils.filter(i => i.startsWith(input))
+  }
 
-    return results
+  async function suggestDynamic(input: string) {
+    return [] // TODO:
   }
 
   return {
