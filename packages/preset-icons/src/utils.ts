9793e214bb114f17995062577f926f33ec609e77
Je Xia <i@jex.me>
2022-03-25 14:56:34 +0800

    fix(preset-icons): support deno std node polyfill (#759)
    Since deno std node polyfill added `stdout` support in https://deno.land/std@0.131.0/node/process.ts#L480, the `isNode` will be `true` when import this preset from esm.sh. But the polyfill also added deno version in `process.versions` in https://deno.land/std@0.131.0/node/_process/process.ts#L99, so we can hack it with `!process.versions.deno`

--- packages/preset-icons/src/utils.ts
+++ packages/preset-icons/src/utils.ts
@@ -1,2 +1,2 @@
-export const isNode = typeof process < 'u' && typeof process.stdout < 'u'
+export const isNode = typeof process < 'u' && typeof process.stdout < 'u' && !process.versions.deno
 export const isVSCode = isNode && !!process.env.VSCODE_CWD
