3ea0098c89883945a50150218924cbdd63ab8a86
chris-zhu <1633711653@qq.com>
2022-04-12 19:45:49 +0800

    fix: auto template with directions

--- packages/preset-mini/src/rules/border.ts
+++ packages/preset-mini/src/rules/border.ts
@@ -41,7 +41,7 @@ export const borders: Rule[] = [
   [/^(?:border-|b-)?(?:rounded|rd)-([bi][se]-[bi][se])(?:-(.+))?$/, handlerRounded],
 
   // style
-  [/^(?:border-|b-)(?:style-)?()(.+)$/, handlerBorderStyle, { autocomplete: ['(border|b)-style', `(border|b)-(${borderStyles.join('|')})`, '(border|b)-(r|l|t|b|x|y)-style', `(border|b)-(r|l|t|b|x|y)-(${borderStyles.join('|')})`, `(border|b)-(r|l|t|b|x|y)-style-(${borderStyles.join('|')})`, `(border|b)-style-(${borderStyles.join('|')})`] }],
+  [/^(?:border-|b-)(?:style-)?()(.+)$/, handlerBorderStyle, { autocomplete: ['(border|b)-style', `(border|b)-(${borderStyles.join('|')})`, '(border|b)-<directions>-style', `(border|b)-<directions>-(${borderStyles.join('|')})`, `(border|b)-<directions>-style-(${borderStyles.join('|')})`, `(border|b)-style-(${borderStyles.join('|')})`] }],
   [/^(?:border-|b-)([rltbsexy])-(?:style-)?(.+)$/, handlerBorderStyle],
 ]
 
