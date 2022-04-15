8df6f62055b6915d62ca6a22b3e726e3caa8e91a
chris-zhu <1633711653@qq.com>
2022-04-15 13:01:15 +0800

    chore(nuxt): optimize resolveOptions (#827)

--- packages/nuxt/src/options.ts
+++ packages/nuxt/src/options.ts
@@ -9,17 +9,18 @@ import type { UnocssNuxtOptions } from './types'
 export function resolveOptions(options: UnocssNuxtOptions) {
   if (options.presets == null) {
     options.presets = []
-    if (options.uno)
-      options.presets.push(presetUno(typeof options.uno === 'boolean' ? {} : options.uno))
-    if (options.wind)
-      options.presets.push(presetWind(typeof options.wind === 'boolean' ? {} : options.wind))
-    if (options.attributify)
-      options.presets.push(presetAttributify(typeof options.attributify === 'boolean' ? {} : options.attributify))
-    if (options.icons)
-      options.presets.push(presetIcons(typeof options.icons === 'boolean' ? {} : options.icons))
-    if (options.webFonts)
-      options.presets.push(presetWebFonts(typeof options.webFonts === 'boolean' ? {} : options.webFonts))
-    if (options.typography)
-      options.presets.push(presetTypography(typeof options.typography === 'boolean' ? {} : options.typography))
+    const presetMap = {
+      uno: presetUno,
+      attributify: presetAttributify,
+      icons: presetIcons,
+      webFonts: presetWebFonts,
+      typography: presetTypography,
+      wind: presetWind,
+    }
+    for (const [key, preset] of Object.entries(presetMap)) {
+      const option = options[key as keyof UnocssNuxtOptions]
+      if (option)
+        options.presets.push(preset(typeof option === 'boolean' ? {} : option))
+    }
   }
 }
