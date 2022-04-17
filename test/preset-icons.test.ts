acb5e565f16f289acbe8dbc304dac1d04f61f1c3
chris-zhu <1633711653@qq.com>
2022-04-17 12:35:19 +0800

    fix(preset-icons): fix auto mode in Reg (#834)

--- test/preset-icons.test.ts
+++ test/preset-icons.test.ts
@@ -4,12 +4,11 @@ import presetUno from '@unocss/preset-uno'
 import { describe, expect, test } from 'vitest'
 
 describe('preset-icons', () => {
-  const fixture1 = `
-<button class="i-carbon-sun dark:i-carbon-moon" />
-`
-  const fixture2 = `
-<button class="i-carbon-sun?bg dark:i-carbon-moon?bg" />
-`
+  const fixtures = [
+    '<button class="i-carbon-sun dark:i-carbon-moon" />',
+    '<button class="i-carbon-sun?bg dark:i-carbon-moon?bg" />',
+    '<button class="i-carbon-sun?bg dark:i-carbon-moon?auto" />',
+  ]
 
   const uno = createGenerator({
     presets: [
@@ -18,13 +17,8 @@ describe('preset-icons', () => {
     ],
   })
 
-  test('fixture1', async() => {
-    const { css, layers } = await uno.generate(fixture1)
-    expect(layers).toEqual(['icons', 'default'])
-    expect(css).toMatchSnapshot()
-  })
-  test('fixture2', async() => {
-    const { css, layers } = await uno.generate(fixture2)
+  test('fixtures', async() => {
+    const { css, layers } = await uno.generate(fixtures.join(' '))
     expect(layers).toEqual(['icons', 'default'])
     expect(css).toMatchSnapshot()
   })
