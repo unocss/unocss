be100b208c98a40e86eeea34607937e9a259e164
chris-zhu <1633711653@qq.com>
2022-04-17 12:06:44 +0800

    fix: update test

--- test/preset-icons.test.ts
+++ test/preset-icons.test.ts
@@ -4,15 +4,11 @@ import presetUno from '@unocss/preset-uno'
 import { describe, expect, test } from 'vitest'
 
 describe('preset-icons', () => {
-  const fixture1 = `
-<button class="i-carbon-sun dark:i-carbon-moon" />
-`
-  const fixture2 = `
-<button class="i-carbon-sun?bg dark:i-carbon-moon?bg" />
-`
-  const fixture3 = `
-<button class="i-carbon-sun?bg dark:i-carbon-moon?auto" />
-`
+  const fixtures = [
+    '<button class="i-carbon-sun dark:i-carbon-moon" />',
+    '<button class="i-carbon-sun?bg dark:i-carbon-moon?bg" />',
+    '<button class="i-carbon-sun?bg dark:i-carbon-moon?auto" />',
+  ]
 
   const uno = createGenerator({
     presets: [
@@ -21,18 +17,8 @@ describe('preset-icons', () => {
     ],
   })
 
-  test('fixture1', async() => {
-    const { css, layers } = await uno.generate(fixture1)
-    expect(layers).toEqual(['icons', 'default'])
-    expect(css).toMatchSnapshot()
-  })
-  test('fixture2', async() => {
-    const { css, layers } = await uno.generate(fixture2)
-    expect(layers).toEqual(['icons', 'default'])
-    expect(css).toMatchSnapshot()
-  })
-  test('fixture3', async() => {
-    const { css, layers } = await uno.generate(fixture3)
+  test('fixtures', async() => {
+    const { css, layers } = await uno.generate(fixtures.join(' '))
     expect(layers).toEqual(['icons', 'default'])
     expect(css).toMatchSnapshot()
   })
