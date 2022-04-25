8deb7f0d50b91367db9b9e481b921f5a70059c9f
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-26 03:12:44 +0800

    fix(preset-mini): auto complete for overflow

--- packages/preset-mini/src/rules/layout.ts
+++ packages/preset-mini/src/rules/layout.ts
@@ -9,6 +9,6 @@ const overflowValues = [
 ]
 
 export const overflows: Rule[] = [
-  [/^(?:overflow|of)-(.+)$/, ([, v]) => overflowValues.includes(v) ? { overflow: v } : undefined, { autocomplete: [`(overflow|of)-${overflowValues.join('|')}`, `(overflow|of)-(x|y)-${overflowValues.join('|')}`] }],
+  [/^(?:overflow|of)-(.+)$/, ([, v]) => overflowValues.includes(v) ? { overflow: v } : undefined, { autocomplete: [`(overflow|of)-(${overflowValues.join('|')})`, `(overflow|of)-(x|y)-(${overflowValues.join('|')})`] }],
   [/^(?:overflow|of)-([xy])-(.+)$/, ([, d, v]) => overflowValues.includes(v) ? { [`overflow-${d}`]: v } : undefined],
 ]
