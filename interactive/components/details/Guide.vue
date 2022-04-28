453f094ce279a1c93eb7622f44f44796116946e4
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-29 04:35:26 +0800

    feat: improve guide loading

--- interactive/components/details/Guide.vue
+++ interactive/components/details/Guide.vue
@@ -5,13 +5,21 @@ const { item } = defineProps<{
   item: GuideItem
 }>()
 
-const component = await item.component().then(i => i.default)
+let component = $shallowRef<any>()
+watch(item, async () => {
+  component = undefined
+  component = await item.component().then(i => i.default)
+}, { immediate: true })
 </script>
 
 <template>
-  <DetailsBase :title="item.title">
+  <DetailsBase v-if="component" :title="item.title">
     <div class="markdown-body max-w-full mt4 text-left">
       <component :is="component" />
     </div>
   </DetailsBase>
+  <div v-else ma animate-pulse animate-duration-600>
+    <div i-carbon-circle-dash w-6 h-6 animate-spin ma />
+    loading...
+  </div>
 </template>
--- interactive/components/details/Result.vue
+++ interactive/components/details/Result.vue
@@ -8,16 +8,8 @@ defineProps<{
 
 <template>
   <div py8 px8 of-auto h-full>
-    <Suspense>
-      <DetailsGuide v-if="item.type === 'guide'" :key="item.title" :item="item" />
-      <DetailsDoc v-else-if="'url' in item" :item="item" />
-      <DetailsRule v-else :item="item" />
-      <template #fallback>
-        <div ma animate-pulse animate-duration-600>
-          <div i-carbon-circle-dash w-6 h-6 animate-spin ma />
-          loading...
-        </div>
-      </template>
-    </Suspense>
+    <DetailsGuide v-if="item.type === 'guide'" :key="item.title" :item="item" />
+    <DetailsDoc v-else-if="'url' in item" :item="item" />
+    <DetailsRule v-else :item="item" />
   </div>
 </template>
