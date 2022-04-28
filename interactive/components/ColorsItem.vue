5c21d4ad2b746003f18f132f82bc5b15cdff0ebe
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-29 00:51:09 +0800

    docs: improve colors guide

--- interactive/components/ColorsItem.vue
+++ interactive/components/ColorsItem.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { kebabCase } from 'scule'
+
 const { level = 1 } = defineProps<{
-  item: string | Record<string, string>
+  item: Record<string, string>
   level?: number
 }>()
 
@@ -8,15 +10,18 @@ const ignores = ['inherit', 'current', 'transparent', 'DEFAULT']
 </script>
 
 <template>
-  <template v-if="typeof item === 'string'">
-    <div w-15 h-10 rounded border="~ base" shadow :style="{ background: item }" />
-  </template>
-  <template v-for="i of Object.entries(item)" v-else :key="i[0]">
+  <template v-for="i of Object.entries(item)" :key="i[0]">
     <div v-if="!ignores.includes(i[0]) && i[0].length > 1">
-      <div :style="{ opacity: 0.7 / level + 0.3 }" :class="level < 2 ? 'mt4' : 0">
-        {{ i[0] }}
+      <div :style="{ opacity: 0.7 / level + 0.3 }" :class="level < 2 ? 'mt4 font-bold' : 'text-sm'">
+        {{ kebabCase(i[0]) }}
       </div>
-      <div row gap2>
+      <template v-if="typeof i[1] === 'string'">
+        <div font-mono text-sm op30>
+          {{ i[1] }}
+        </div>
+        <div w-17 h-9 mt1 border="~ base" shadow :style="{ background: i[1] }" />
+      </template>
+      <div v-else row gap1 flex-wrap>
         <ColorsItem :item="i[1]" :level="level + 1" />
       </div>
     </div>
--- interactive/guides/colors.vue
+++ interactive/guides/colors.vue
@@ -4,7 +4,14 @@ const colors = (uno.config.theme as any).colors || {}
 </script>
 
 <template>
-  <div justify-center ma>
-    <ColorsItem :item="colors" />
+  <div ma>
+    <p>
+      The default color pattale of <code>@unocss/preset-mini</code> are extended from
+      <a href="https://tailwindcss.com/docs/customizing-colors" target="_blank">Tailwind's</a>.
+      The following table are dynamically generated from the theme of the current configuration.
+    </p>
+    <div justify-center ma>
+      <ColorsItem :item="colors" />
+    </div>
   </div>
 </template>
