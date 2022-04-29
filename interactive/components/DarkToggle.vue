d1b6cc824dc7118a21addd7e0bcc532d8239db6e
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-30 03:28:54 +0800

    docs: fix isDark reference

--- interactive/components/DarkToggle.vue
+++ interactive/components/DarkToggle.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import { isDark } from '#imports'
+
 function toggleDark() {
   isDark.value = !isDark.value
 }
 </script>
 
 <template>
-  <button class="!outline-none" :aria-label="isDark ? 'Switch to light theme' : 'Switch to dark theme'" @click="toggleDark">
+  <button
+    class="!outline-none"
+    :title="isDark ? 'Switch to light mode' : 'Switch to dark mode'"
+    @click="toggleDark"
+  >
     <span class="dark:i-carbon-moon i-carbon-sun block" aria-hidden="true" />
   </button>
 </template>
