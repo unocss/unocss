d73209a8e25dd184a13f59e50f01be8d6939c718
Saya <chu121su12@gmail.com>
2022-05-31 00:55:42 +0800

    feat(runtime): add tagify to the `full` build

--- packages/runtime/src/cdn/full.ts
+++ packages/runtime/src/cdn/full.ts
@@ -1,6 +1,7 @@
 import presetAttributify from '@unocss/preset-attributify'
 import presetUno from '@unocss/preset-uno'
-import { presetTypography } from '@unocss/preset-typography'
+import presetTypography from '@unocss/preset-typography'
+import presetTagify from '@unocss/preset-tagify'
 import init from '../index'
 
 init({
@@ -9,6 +10,7 @@ init({
       presetAttributify(),
       presetUno(),
       presetTypography(),
+      presetTagify(),
     ],
   },
 })
