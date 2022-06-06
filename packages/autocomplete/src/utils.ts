e43f8eb0fc0a502a550d30da5b9c33bc7b652222
TrickyPi <530257315@qq.com>
2022-06-06 22:15:58 +0800

    feat(vscode): support intelligent prompt for css file

--- packages/autocomplete/src/utils.ts
+++ packages/autocomplete/src/utils.ts
@@ -2,7 +2,7 @@ export function searchUsageBoundary(line: string, index: number) {
   let start = index
   let end = index
 
-  const regex = /[^\s>"'`]/
+  const regex = /[^\s>"'`;]/
   while (start && regex.test(line.charAt(start - 1))) --start
   while (end < line.length && regex.test(line.charAt(end))) ++end
 
