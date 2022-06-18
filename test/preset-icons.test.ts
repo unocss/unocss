bd3a6c45f50436f62ab5f6090e0ca10fcc9d3043
Joaquín Sánchez <joaquin.sanchez@fi2net.es>
2022-06-18 07:22:39 +0200

    feat: automatic svg prologue cleanup (#1111)

--- test/preset-icons.test.ts
+++ test/preset-icons.test.ts
@@ -22,6 +22,15 @@ describe('preset-icons', () => {
       presetIcons({
         unit: 'rem',
         scale: 2,
+        collections: {
+          custom: {
+            'circle-with-xml-preface': `
+<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg"><circle cx="60" cy="60" r="50"/></svg>
+`,
+          },
+        },
         customizations: {
           iconCustomizer(collection, icon, props) {
             if (!(collection === 'carbon' && icon === 'sun')) {
@@ -44,6 +53,12 @@ describe('preset-icons', () => {
   test('icon unit fixtures', async () => {
     const { css, layers } = await unoWithUnit.generate(fixtures.join(' '), { preflights: false })
     expect(layers).toEqual(['icons', 'default'])
+    expect(css).toMatchSnapshot()
+  })
+  test('svg prologue cleared', async () => {
+    const { css, layers } = await unoWithUnit.generate('<button class="i-custom:circle-with-xml-preface" />', { preflights: false })
+    expect(layers).toEqual(['icons', 'default'])
+    expect(css).toContain('data:image/svg+xml;utf8,%3Csvg')
     expect(css).toMatchSnapshot()
   })
 })
