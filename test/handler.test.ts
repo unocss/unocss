2f82781055507b60af1d6f2b49e5ea8e4d8a1afc
Saya <chu121su12@gmail.com>
2022-06-25 12:11:13 +0800

    wip

---
+++ test/handler.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest'
+import { handler as h } from '@unocss/preset-mini/utils'
+
+describe('value handler', () => {
+  it('bracket math function', () => {
+    expect(h.bracket('[calc(1-2)]')).eql('calc(1 - 2)')
+    expect(h.bracket('[min(1-2)]')).eql('min(1 - 2)')
+    expect(h.bracket('[max(1-2)]')).eql('max(1 - 2)')
+    expect(h.bracket('[clamp(1-2)]')).eql('clamp(1 - 2)')
+
+    expect(h.bracket('[calc(1+2)]')).eql('calc(1 + 2)')
+    expect(h.bracket('[calc(1/2)]')).eql('calc(1 / 2)')
+    expect(h.bracket('[calc(1*2)]')).eql('calc(1 * 2)')
+  })
+})
