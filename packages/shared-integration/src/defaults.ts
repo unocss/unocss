3278913df4323f3744831d526b429b1ead377264
TrickyPi <530257315@qq.com>
2022-06-29 18:13:01 +0800

    feat: ignore .d.ts by default

--- packages/shared-integration/src/defaults.ts
+++ packages/shared-integration/src/defaults.ts
@@ -1,4 +1,4 @@
 import { cssIdRE } from '@unocss/core'
 
-export const defaultExclude = [cssIdRE]
+export const defaultExclude = [cssIdRE, /\.d\.ts$/]
 export const defaultInclude = [/\.vue$/, /\.vue\?vue/, /\.svelte$/, /\.[jt]sx$/, /\.mdx?$/, /\.astro$/, /\.elm$/]
