ac30230656943aea8e46661addc6a48d072e284d
Dunqing <dengqing0821@gmail.com>
2022-07-08 05:19:32 +0000

    feat: direct use of expandVariantGroup

--- packages/transformer-variant-group/src/index.ts
+++ packages/transformer-variant-group/src/index.ts
@@ -1,21 +1,12 @@
 import type { SourceCodeTransformer } from '@unocss/core'
-import { expandVariantGroup, extractQuoted } from '@unocss/core'
+import { expandVariantGroup } from '@unocss/core'
 
 export default function transformerVariantGroup(): SourceCodeTransformer {
   return {
     name: 'variant-group',
     enforce: 'pre',
     transform(s) {
-      extractQuoted(
-        s.toString(),
-        {
-          details: true,
-          templateStaticOnly: true,
-          deep: true,
-        },
-      )
-        .filter(({ value: { length } }) => length)
-        .forEach(({ value, range }) => s.overwrite(...range, expandVariantGroup(value)))
+      expandVariantGroup(s)
     },
   }
 }
