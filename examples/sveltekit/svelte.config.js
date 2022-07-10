ccd3cff4418bb87923613073df2311dd828e1c16
Joaquín Sánchez <joaquin.sanchez@fi2net.es>
2022-07-10 07:42:06 +0200

    docs(vite): update SvelteKit entry and the example (#1237)

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -1,10 +1,5 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
-import UnoCss from 'unocss/vite'
-import { extractorSvelte } from '@unocss/core'
-import presetIcons from '@unocss/preset-icons'
-import presetUno from '@unocss/preset-uno'
-import { FileSystemIconLoader } from '@iconify/utils/lib/loader/node-loaders'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,39 +10,13 @@ const config = {
   kit: {
     adapter: adapter(),
 
-    vite: {
-      plugins: [
-        UnoCss({
-          extractors: [extractorSvelte],
-          shortcuts: [
-            { logo: 'i-logos:svelte-icon w-6em h-6em transform transition-800 hover:rotate-180' },
-            { foo: 'bg-yellow-400' },
-            { bar: 'bg-green-400' },
-          ],
-          presets: [
-            presetUno(),
-            presetIcons({
-              collections: {
-                custom: {
-                  // do not remove LF: testing trimCustomSvg on universal icon loader
-                  circle: `<svg viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
-<circle cx="60" cy="60" r="50"/>
-</svg>
-`,
-                },
-                customfsl: FileSystemIconLoader(
-                  './icons',
-                  svg => svg.replace('<svg ', '<svg fill="currentColor" '),
-                ),
-              },
-              extraProperties: {
-                'display': 'inline-block',
-                'vertical-align': 'middle',
-              },
-            }),
-          ],
-        }),
-      ],
+    prerender: {
+      default: true,
+    },
+
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ['PATCH', 'DELETE'],
     },
   },
 }
