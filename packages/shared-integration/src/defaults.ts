b190e37445d2f7ef3d461c2173865c2076927299
Adam Burgess <adamburgess@users.noreply.github.com>
2022-07-15 06:47:34 +1000

    fix(vscode): add html to the default includes (#1265)

--- packages/shared-integration/src/defaults.ts
+++ packages/shared-integration/src/defaults.ts
@@ -1,4 +1,4 @@
 import { cssIdRE } from '@unocss/core'
 
 export const defaultExclude = [cssIdRE]
-export const defaultInclude = [/\.vue$/, /\.vue\?vue/, /\.svelte$/, /\.[jt]sx$/, /\.mdx?$/, /\.astro$/, /\.elm$/]
+export const defaultInclude = [/\.vue$/, /\.vue\?vue/, /\.svelte$/, /\.[jt]sx$/, /\.mdx?$/, /\.astro$/, /\.elm$/, /\.html$/]
