d8d282affa8bfaf63318875b38e0230540509c87
snowingfox <1503401882@qq.com>
2022-07-26 14:11:40 +0800

    test: blocklist

--- test/transformer-attributify-jsx.test.ts
+++ test/transformer-attributify-jsx.test.ts
@@ -1,7 +1,10 @@
+import { isRegExp } from 'util/types'
 import MagicString from 'magic-string'
+import presetAttributify from '@unocss/preset-attributify'
+import presetUno from '@unocss/preset-uno'
 import { createGenerator } from '@unocss/core'
 import { describe, expect, test } from 'vitest'
-import transformerAttributifyJsx from '../packages/transformer-attributify-jsx'
+import transformerAttributifyJsx from '../packages/transformer-attributify-jsx/src'
 
 describe('transformerAttributifyJs', () => {
   const originalCode = `
@@ -27,12 +30,16 @@ describe('transformerAttributifyJs', () => {
   on-demand · instant · fully customizable
 </div>
   `
-  const code = new MagicString(originalCode)
+  const uno = createGenerator({
+    presets: [
+      presetUno(),
+      presetAttributify(),
+    ],
+  })
 
-  const uno = createGenerator({})
-
-  test('transform', () => {
-    transformerAttributifyJsx().transform(code, 'foo.js', { uno, tokens: new Set() } as any)
+  test('transform', async () => {
+    const code = new MagicString(originalCode)
+    await transformerAttributifyJsx().transform(code, 'app.tsx', { uno, tokens: new Set() } as any)
 
     expect(code.toString()).toMatchInlineSnapshot(`
       "
@@ -46,7 +53,7 @@ describe('transformerAttributifyJs', () => {
           </div>
           <div m2=\\"\\" flex=\\"\\" justify-center=\\"\\" text-2xl=\\"\\" op30=\\"\\" hover=\\"op80\\">
             <a
-              i-carbon-logo-github=\\"\\"
+              i-carbon-logo-github
               text-inherit=\\"\\"
               href=\\"https://github.com/unocss/unocss\\"
               target=\\"_blank\\"
@@ -59,5 +66,48 @@ describe('transformerAttributifyJs', () => {
       </div>
         "
     `)
+  })
+
+  test('blocklist', async () => {
+    const code = new MagicString(originalCode)
+    const blocklist = ['flex', 'absolute', /op[0-9]+/]
+
+    await transformerAttributifyJsx({
+      blocklist,
+    }).transform(code, 'app.jsx', { uno, tokens: new Set() } as any)
+
+    expect(code.toString()).toMatchInlineSnapshot(`
+      "
+          <div h-full=\\"\\" text-center=\\"\\" flex select-none=\\"\\">
+        <div ma=\\"\\">
+          <div text-5xl=\\"\\" fw100=\\"\\" animate-bounce-alt=\\"\\" animate-count-infinite=\\"\\" animate-duration-1s=\\"\\">
+            unocss
+          </div>
+          <div op30 text-lg=\\"\\" fw300=\\"\\" m1=\\"\\">
+            The instant on-demand Atomic CSS engine.
+          </div>
+          <div m2=\\"\\" flex justify-center=\\"\\" text-2xl=\\"\\" op30 hover=\\"op80\\">
+            <a
+              i-carbon-logo-github
+              text-inherit=\\"\\"
+              href=\\"https://github.com/unocss/unocss\\"
+              target=\\"_blank\\"
+            ></a>
+          </div>
+        </div>
+      </div>
+      <div absolute bottom-5=\\"\\" right-0=\\"\\" left-0=\\"\\" text-center=\\"\\" op30 fw300=\\"\\">
+        on-demand · instant · fully customizable
+      </div>
+        "
+    `)
+
+    const codeToString = code.toString()
+    blocklist.forEach((rule) => {
+      if (isRegExp(rule))
+        expect(new RegExp(`${rule.source}=""`).test(codeToString)).not.true
+      else
+        expect(codeToString).not.toMatch(`${rule}=""`)
+    })
   })
 })
