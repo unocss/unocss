82527b04f57842197716179362e05049e8fff812
Anthony Fu <anthonyfu117@hotmail.com>
2022-08-06 00:24:52 +0800

    chore: update examples

---
+++ examples/vite-vue3/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div text-center font-sans p4>
+    <div logo />
+    <div text-green5>
+      Hello UnoCSS + Vue
+    </div>
+  </div>
+</template>
--- examples/vite-vue3/vite.config.ts
+++ examples/vite-vue3/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import React from '@vitejs/plugin-react'
+import Vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import presetAttributify from '@unocss/preset-attributify'
 import presetIcons from '@unocss/preset-icons'
@@ -8,10 +8,10 @@ import presetUno from '@unocss/preset-uno'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    React(),
+    Vue(),
     UnoCSS({
       shortcuts: [
-        { logo: 'i-logos-react w-6em h-6em transform transition-800 hover:rotate-180' },
+        { logo: 'i-logos-vue w-6em h-6em transform transition-800' },
       ],
       presets: [
         presetUno(),
