b99ea250303a5edfb851956509f349f21cb95e73
Anthony Fu <anthonyfu117@hotmail.com>
2022-08-18 23:39:42 +0800

    chore: update deps

--- packages/preset-wind/src/variants/misc.ts
+++ packages/preset-wind/src/variants/misc.ts
@@ -1,7 +1,7 @@
 import type { Variant } from '@unocss/core'
 
 export const variantSpaceAndDivide: Variant = (matcher) => {
-  if (/^space-?([xy])-?(-?.+)$/.test(matcher) || /^divide-/.test(matcher)) {
+  if (/^space-?([xy])-?(-?.+)$/.test(matcher) || matcher.startsWith('divide-')) {
     return {
       matcher,
       selector: (input) => {
