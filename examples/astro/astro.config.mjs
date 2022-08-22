29677e1d7b28048cfef22287aca8f5da94b6b763
Anthony Fu <anthonyfu117@hotmail.com>
2022-08-22 16:36:04 +0800

    chore: update

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -1,30 +1,10 @@
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/vite'
-import presetIcons from '@unocss/preset-icons'
-import presetUno from '@unocss/preset-uno'
-import transformDirectives from '@unocss/transformer-directives'
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [
-      UnoCSS({
-        shortcuts: [
-          { 'i-logo': 'i-logos-astro w-6em h-6em transform transition-800' },
-        ],
-        transformers: [
-          transformDirectives(),
-        ],
-        presets: [
-          presetUno(),
-          presetIcons({
-            extraProperties: {
-              'display': 'inline-block',
-              'vertical-align': 'middle',
-            },
-          }),
-        ],
-      }),
+      UnoCSS(),
     ],
   },
 })
