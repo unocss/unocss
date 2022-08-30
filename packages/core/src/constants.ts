69f3861b1a878ea6da886514173bbbf37cdf2a3a
Frozen FIsh <76603360+sudongyuer@users.noreply.github.com>
2022-08-30 11:04:26 +0800

    chore: fix typo (#1495)
    * chore: fix typo
    
    * chore: lint fix
    
    Co-authored-by: chris-zhu <1633711653@qq.com>

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -2,7 +2,7 @@ export const LAYER_DEFAULT = 'default'
 export const LAYER_PREFLIGHTS = 'preflights'
 export const LAYER_SHORTCUTS = 'shortcuts'
 
-export const DEAFULT_LAYERS = {
+export const DEFAULT_LAYERS = {
   [LAYER_PREFLIGHTS]: -100,
   [LAYER_SHORTCUTS]: -10,
   [LAYER_DEFAULT]: 0,
