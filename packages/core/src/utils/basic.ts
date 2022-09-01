2044290fef5b5928958be09634a953f7d951bfb6
Frozen FIsh <76603360+sudongyuer@users.noreply.github.com>
2022-09-02 02:18:18 +0800

    chore: delete unused fn & add local ci test scirpt (#1510)

--- packages/core/src/utils/basic.ts
+++ packages/core/src/utils/basic.ts
@@ -6,11 +6,6 @@ export function uniq<T>(value: T[]): T[] {
   return Array.from(new Set(value))
 }
 
-export function mergeSet<T>(target: Set<T>, append: Set<T>): Set<T> {
-  append.forEach(i => target.add(i))
-  return target
-}
-
 export function isString(s: any): s is string {
   return typeof s === 'string'
 }
