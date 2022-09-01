145b68630fba350a1163a3d6b3db1b1efe7c31d4
FlowerF <1647231392@qq.com>
2022-09-02 00:52:44 +0800

    fix: add the colon operator support for react-jsx

--- test/transformer-attributify-jsx.test.ts
+++ test/transformer-attributify-jsx.test.ts
@@ -15,7 +15,7 @@ describe('transformerAttributifyJs', () => {
     <div op30 text-lg fw300 m1>
       The instant on-demand Atomic CSS engine.
     </div>
-    <div m2 flex justify-center text-2xl op30 hover="op80">
+    <div m2 flex justify-center text-2xl op30 hover:op80 hover:text-2xl>
       <a
         i-carbon-logo-github
         text-inherit
@@ -50,7 +50,7 @@ describe('transformerAttributifyJs', () => {
           <div op30=\\"\\" text-lg=\\"\\" fw300=\\"\\" m1=\\"\\">
             The instant on-demand Atomic CSS engine.
           </div>
-          <div m2=\\"\\" flex=\\"\\" justify-center=\\"\\" text-2xl=\\"\\" op30=\\"\\" hover=\\"op80\\">
+          <div m2=\\"\\" flex=\\"\\" justify-center=\\"\\" text-2xl=\\"\\" op30=\\"\\" hover-op80=\\"\\" hover-text-2xl=\\"\\">
             <a
               i-carbon-logo-github
               text-inherit=\\"\\"
@@ -68,7 +68,7 @@ describe('transformerAttributifyJs', () => {
 
   test('blocklist', async () => {
     const code = new MagicString(originalCode)
-    const blocklist = ['flex', 'absolute', /op[0-9]+/]
+    const blocklist: (string | RegExp)[] = ['flex', 'absolute']
 
     await transformerAttributifyJsx({
       blocklist,
@@ -80,10 +80,10 @@ describe('transformerAttributifyJs', () => {
           <div text-5xl=\\"\\" fw100=\\"\\" animate-bounce-alt=\\"\\" animate-count-infinite=\\"\\" animate-duration-1s=\\"\\">
             unocss
           </div>
-          <div op30 text-lg=\\"\\" fw300=\\"\\" m1=\\"\\">
+          <div op30=\\"\\" text-lg=\\"\\" fw300=\\"\\" m1=\\"\\">
             The instant on-demand Atomic CSS engine.
           </div>
-          <div m2=\\"\\" flex justify-center=\\"\\" text-2xl=\\"\\" op30 hover=\\"op80\\">
+          <div m2=\\"\\" flex justify-center=\\"\\" text-2xl=\\"\\" op30=\\"\\" hover-op80=\\"\\" hover-text-2xl=\\"\\">
             <a
               i-carbon-logo-github
               text-inherit=\\"\\"
@@ -93,7 +93,7 @@ describe('transformerAttributifyJs', () => {
           </div>
         </div>
       </div>
-      <div absolute bottom-5=\\"\\" right-0=\\"\\" left-0=\\"\\" text-center=\\"\\" op30 fw300=\\"\\">
+      <div absolute bottom-5=\\"\\" right-0=\\"\\" left-0=\\"\\" text-center=\\"\\" op30=\\"\\" fw300=\\"\\">
         on-demand · instant · fully customizable
       </div>"
     `)
