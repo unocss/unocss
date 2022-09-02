3322f4383e9b65b09546bb54d6b2f49debc47c70
Frozen FIsh <76603360+sudongyuer@users.noreply.github.com>
2022-09-02 12:23:00 +0800

    chore(helpers): improve validateFilterRE regex (#1503)
    * chore(helpers): improve validateFilterRE regex
    
    * Update packages/core/src/utils/helpers.ts
    
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/core/src/utils/helpers.ts
+++ packages/core/src/utils/helpers.ts
@@ -2,7 +2,7 @@ import type { ParsedUtil, RawUtil, StringifiedUtil, Variant, VariantObject } fro
 
 export const attributifyRE = /^\[(.+?)~?="(.*)"\]$/
 export const cssIdRE = /\.(css|postcss|sass|scss|less|stylus|styl)$/
-export const validateFilterRE = /(?!\d|-{2}|-\d)[a-zA-Z0-9\u00A0-\uFFFF-_:%-?]/
+export const validateFilterRE = /[\w\u00A0-\uFFFF-_:%-?]/
 export const CONTROL_SHORTCUT_NO_MERGE = '$$shortcut-no-merge'
 
 export function isAttributifySelector(selector: string) {
