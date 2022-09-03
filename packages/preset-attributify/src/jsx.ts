f31493ce2cd28544431dff41bc7c13cac74d88ab
FlowerF <1647231392@qq.com>
2022-09-03 09:10:51 +0800

    feat: more complete type support for tsx

--- packages/preset-attributify/src/jsx.ts
+++ packages/preset-attributify/src/jsx.ts
@@ -1,4 +1,95 @@
-export type UtilityNames =
+export type TwoStringsCompositionPrefix =
+  | 'm'
+  | 'p'
+
+export type TwoStringsCompositionSuffix =
+  | 'r'
+  | 'b'
+  | 'l'
+  | 't'
+  | 'a'
+
+/** Some words can compose with two strings to become a complete unocss rule such as ha, mr, mb */
+export type TwoStringsComposition = `${TwoStringsCompositionPrefix}${TwoStringsCompositionSuffix}` | 'ha' | 'wa'
+
+/** Some words can be a complete unocss rule by itself */
+export type SpecialSingleWord =
+  | 'container'
+  | 'flex'
+  | 'block'
+  | 'inline'
+  | 'block'
+  | 'table'
+  | 'isolate'
+  | 'absolute'
+  | 'relative'
+  | 'fixed'
+  | 'sticky'
+  | 'static'
+  | 'visible'
+  | 'invisible'
+  | 'grow'
+  | 'shrink'
+  | 'antialiased'
+  | 'italic'
+  | 'ordinal'
+  | 'overline'
+  | 'underline'
+  | 'uppercase'
+  | 'lowercase'
+  | 'capitalize'
+  | 'truncate'
+  | 'border'
+  | 'rounded'
+  | 'outline'
+  | 'ring'
+  | 'shadow'
+  | 'blur'
+  | 'grayscale'
+  | 'invert'
+  | 'sepia'
+  | 'transition'
+  | 'resize'
+  | 'transform'
+  | 'filter'
+
+export type StringNumberCompositionPrefix =
+  | 'op'
+  | 'opacity'
+  | 'fw'
+  | 'p'
+  | 'm'
+  | 'w'
+  | 'h'
+  | 'z'
+
+/** Some words can be a complete unocss rule by compose a string and a number, such as op80, fw300, p2, p10px */
+export type StringNumberComposition = `${StringNumberCompositionPrefix}${number}${'px' | ''}`
+
+export type PseudoPrefix =
+  | 'active'
+  | 'before'
+  | 'after'
+  | 'dark'
+  | 'light'
+  | 'first'
+  | 'last'
+  | 'focus'
+  | 'hover'
+  | 'link'
+  | 'root'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | '2xl'
+  | 'enabled'
+  | 'disabled'
+  | 'all'
+  | 'children'
+
+/** Some words can be used to separate utilities, such as font="mono light", text="sm white" */
+export type SeparateEnabled =
   | 'align'
   | 'animate'
   | 'backdrop'
@@ -40,30 +131,12 @@ export type UtilityNames =
   | 'underline'
   | 'w'
   | 'z'
+  | PseudoPrefix
 
-export type VariantNames =
-  | 'active'
-  | 'after'
-  | 'all'
-  | 'before'
-  | 'child'
-  | 'dark'
-  | 'enabled'
-  | 'first'
-  | 'focus'
-  | 'hover'
-  | 'last'
-  | 'lg'
-  | 'light'
-  | 'md'
-  | 'root'
-  | 'sm'
-  | 'xl'
-  | 'xxl'
+export type BasicAttributes = StringNumberComposition | SpecialSingleWord | TwoStringsComposition | SeparateEnabled
 
 export type AttributifyNames<Prefix extends string = ''> =
-  | `${Prefix}${UtilityNames}`
-  | `${Prefix}${VariantNames}`
-  | `${Prefix}${VariantNames}:${UtilityNames}`
+  | `${Prefix}${BasicAttributes}`
+  | `${Prefix}${PseudoPrefix}:${BasicAttributes}`
 
 export interface AttributifyAttributes extends Partial<Record<AttributifyNames, string | boolean>> {}
