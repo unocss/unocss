da625ab93ea066a69a4b1f38f41ea610b7e886fc
BTBMan <1498118157@qq.com>
2022-09-16 14:21:51 +0800

    wip: add icons for test

--- examples/vite-vue3/src/App.vue
+++ examples/vite-vue3/src/App.vue
@@ -4,5 +4,7 @@
     <div text-green5 text-lg>
       Hello UnoCSS + Vue
     </div>
+    <div i-custom-icon />
+    <div i-custom-icon-formatted /> <!-- \n has been replaced by empty string -->
   </div>
 </template>
