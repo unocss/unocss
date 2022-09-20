cc11fe6e6a5b3e2260dc0f334755f733369478df
qing.deng <dengqing0821@gmail.com>
2022-09-20 12:16:58 +0800

    fix: remove test

--- test/generate-async.test.ts
+++ test/generate-async.test.ts
@@ -23,26 +23,4 @@ describe('generate-async', () => {
     await uno.generate('rule')
     expect(order).eql([1, 2])
   })
-
-  test('preflight-first', async () => {
-    const order: number[] = []
-    const uno = createGenerator({
-      rules: [
-        [/^rule$/, () => new Promise(resolve => setTimeout(() => {
-          order.push(2)
-          resolve('/* rule */')
-        }, 20))],
-      ],
-      preflights: [
-        {
-          getCSS: () => new Promise(resolve => setTimeout(() => {
-            order.push(1)
-            resolve('/* preflight */')
-          }, 10)),
-        },
-      ],
-    })
-    await uno.generate('rule')
-    expect(order).eql([1, 2])
-  })
 })
