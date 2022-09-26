9276f453ab0595a11655d757cc1ced91fd1df554
Anthony Fu <anthonyfu117@hotmail.com>
2022-09-26 11:54:50 +0800

    chore: update

--- interactive/components/details/Base.vue
+++ interactive/components/details/Base.vue
@@ -10,7 +10,12 @@ const { copy, copied } = useClipboard()
   <div>
     <h2 text-3xl font-light>
       {{ title }}
-      <button :class="copied ? 'i-carbon-checkmark-outline' : 'i-carbon-copy'" w-4 h-4 op50 hover:op100 cursor-pointer title="Copy" @click="copy(title)" />
+      <button
+        :class="copied ? 'i-carbon-checkmark-outline text-green op100' : 'i-carbon-copy op50'"
+        w-4 h-4 hover:op100 cursor-pointer ma
+        title="Copy"
+        @click="copy(title)"
+      />
     </h2>
     <slot />
   </div>
