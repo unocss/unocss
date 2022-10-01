bc48c5da87193949c0f66c4d2561c6ed6b0060d0
jwrunner <jwrunner7@gmail.com>
2022-10-01 14:54:28 +0800

    remove unused code

--- examples/sveltekit/src/routes/+layout.svelte
+++ examples/sveltekit/src/routes/+layout.svelte
@@ -50,25 +50,4 @@
 		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
 		Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 	}
-/*
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-*/
-	/*h1 {*/
-	/*	color: #ff3e00;*/
-	/*	text-transform: uppercase;*/
-	/*	font-size: 4rem;*/
-	/*	font-weight: 100;*/
-	/*	line-height: 1.1;*/
-	/*	margin: 2rem auto;*/
-	/*	max-width: 14rem;*/
-	/*}*/
-	/*@media (min-width: 480px) {*/
-	/*	h1 {*/
-	/*		max-width: none;*/
-	/*	}*/
-	/*}*/
 </style>
