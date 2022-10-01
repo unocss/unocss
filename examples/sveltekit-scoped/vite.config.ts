9f3327f4747972778f754f8c9fb82a5a61f7d955
jwrunner <jwrunner7@gmail.com>
2022-10-01 15:49:31 +0800

    remove unused shortcuts

--- examples/sveltekit-scoped/vite.config.ts
+++ examples/sveltekit-scoped/vite.config.ts
@@ -13,8 +13,6 @@ const config: UserConfig = {
 			extractors: [extractorSvelte],
 			shortcuts: [
 				{ logo: 'i-logos:svelte-icon w-6em h-6em transform transition-800 hover:rotate-180' },
-				{ foo: 'bg-yellow-400' },
-				{ bar: 'bg-green-400' },
 			],
 			presets: [
 				presetUno(),
