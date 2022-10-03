fdf66575898b92f75cb40cf126c0e493ef596428
jwrunner <jwrunner7@gmail.com>
2022-10-03 14:31:59 +0800

    update mode target to svelte-scoped-compiled

--- examples/sveltekit-scoped/vite.config.ts
+++ examples/sveltekit-scoped/vite.config.ts
@@ -1,15 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
-import { extractorSvelte } from '@unocss/core'
 import presetIcons from '@unocss/preset-icons'
 import presetUno from '@unocss/preset-uno'
 
 export default defineConfig({
   plugins: [
     UnoCSS({
-      mode: 'svelte-scoped',
-      extractors: [extractorSvelte],
+      mode: 'svelte-scoped-compiled',
       shortcuts: [
         { logo: 'i-logos:svelte-icon w-6em h-6em transform transition-800 hover:rotate-180' },
       ],
