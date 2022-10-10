2f6614685bae626a7824214b4dbdde6cb7ce83c7
Chris <1633711653@qq.com>
2022-10-10 14:07:29 +0800

    feat(preset-icons): load collections

---
+++ scripts/icon-collections.ts
@@ -0,0 +1,21 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'fs-extra'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const out = path.resolve(__dirname, '../packages/preset-icons')
+
+async function prepareJSON() {
+  const dir = path.resolve(__dirname, '../node_modules/@iconify/json/json')
+  await fs.ensureDir(dir)
+
+  const collections = (await fs.readdir(dir)).map(file => file.replace(/\.json$/, ''))
+  await fs.writeJSON(path.resolve(out, 'src/collections.json'), collections)
+}
+
+async function prepare() {
+  await prepareJSON()
+}
+
+prepare()
