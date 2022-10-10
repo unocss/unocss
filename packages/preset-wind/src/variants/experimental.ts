ec34f06ceeb37ac975fc82abae8b01ed0cb12347
Anthony Fu <anthonyfu117@hotmail.com>
2022-10-10 11:10:40 +0800

    Update packages/preset-wind/src/variants/experimental.ts

--- packages/preset-wind/src/variants/experimental.ts
+++ packages/preset-wind/src/variants/experimental.ts
@@ -4,7 +4,7 @@ import { variantMatcher } from '@unocss/preset-mini/utils'
 
 export const variantStickyHover: Variant[] = [
   variantMatcher('@hover', (input) => {
-    warnOnce('The @hover variant is experimental and may be changed in breaking ways at any time.')
+    warnOnce('The @hover variant is experimental and may not follow semver.')
     return {
       parent: `${input.parent ? `${input.parent} $$ ` : ''}@media (hover: hover) and (pointer: fine)`,
       selector: `${input.selector || ''}:hover`,
