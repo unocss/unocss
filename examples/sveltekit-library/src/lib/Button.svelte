c528474b21bb67455c092297b4ec6a5cfc21f315
jwrunner <jwrunner7@gmail.com>
2022-10-11 12:20:01 +0800

    finish bare-bones example, add reset

--- examples/sveltekit-library/src/lib/Button.svelte
+++ examples/sveltekit-library/src/lib/Button.svelte
@@ -1,1 +1,5 @@
-<div class="bg-red-300">Hello</div>
+<script lang="ts">
+ export let onclick: () => any;
+</script>
+
+<button on:click={onclick} type="button" class="bg-red-300 p-2 rounded font-semibold"><slot /></button>
--- examples/sveltekit-library/src/routes/+page.svelte
+++ examples/sveltekit-library/src/routes/+page.svelte
@@ -1,16 +1,22 @@
 <script>
   import SvelteLogo from "$lib/SvelteLogo.svelte";
   import Button from "$lib/Button.svelte";
+  let bounce = true;
 </script>
 
-<h1>
-  <SvelteLogo
-    class="animate-bounce-alt animate-count-infinite animate-duration-1s"
-  /> SvelteKit + svelte-preprocess-unocss Component Library example
-</h1>
+<div class="p-3">
+  <h1 class="text-4xl my-5">
+    <SvelteLogo
+      class="{bounce
+        ? 'animate-bounce-alt'
+        : ''} animate-count-infinite animate-duration-1s"
+    /> SvelteKit + svelte-preprocess-unocss Component Library example
+  </h1>
 
-<Button />
+  <Button onclick={() => (bounce = !bounce)}>{bounce ? 'Stop' : 'Start'} Bouncing</Button>
 
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+  <p class="my-2">
+    Visit <a class="underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to
+    read the `svelte-package` documentation
+  </p>
+</div>
