a95f52d3d0e207b0a56e22a9c79d0060f1b18441
jwrunner <jwrunner7@gmail.com>
2022-10-11 12:19:35 +0800

    turn off sourceMap until fixed, add presetUno as default if no user config set

--- packages/svelte-preprocess-unocss/src/index.ts
+++ packages/svelte-preprocess-unocss/src/index.ts
@@ -1,22 +1,26 @@
 import type { UserConfig, UserConfigDefaults } from '@unocss/core'
 import { transformSvelteSFC } from '@unocss/vite'
+import presetUno from '@unocss/preset-uno'
 import type { PreprocessorGroup } from 'svelte/types/compiler/preprocess'
 import MagicString from 'magic-string'
-import type { SourceMap } from 'rollup'
-import remapping from '@ampproject/remapping'
-import type { EncodedSourceMap } from '@ampproject/remapping'
+// import type { SourceMap } from 'rollup'
+// import remapping from '@ampproject/remapping'
+// import type { EncodedSourceMap } from '@ampproject/remapping'
 import { createContext } from '../../shared-integration/src/context'
 
 export default function SveltePreprocessUnocss(
   configOrPath?: UserConfig | string,
-  defaults: UserConfigDefaults = {},
+  defaults: UserConfigDefaults = {
+    presets: [
+      presetUno(),
+    ],
+  },
 ): PreprocessorGroup {
   const ctx = createContext<UserConfig>(configOrPath as any, defaults)
-  // console.log('inited ctx')
   return {
     markup: async ({ content, filename }) => {
       let code = content
-      const maps: EncodedSourceMap[] = []
+      // const maps: EncodedSourceMap[] = []
 
       const transformers = (ctx.uno.config.transformers || [])
 
@@ -25,21 +29,30 @@ export default function SveltePreprocessUnocss(
         await t.transform(s, filename || '', ctx)
         if (s.hasChanged()) {
           code = s.toString()
-          maps.push(s.generateMap({ hires: true, source: filename }) as EncodedSourceMap)
+          // maps.push(s.generateMap({ hires: true, source: filename }) as EncodedSourceMap)
           s = new MagicString(code)
         }
       }
 
       const result = await transformSvelteSFC(code, filename || '', ctx.uno)
-      if (result) {
+
+      if (result?.code)
         code = result.code
-        maps.push(result.map as EncodedSourceMap)
+
+      if (result?.map) {
+        return {
+          code,
+          // map: result.map,
+        }
+      }
+      else {
+        return {
+          code,
+        }
       }
 
-      return {
-        code,
-        map: remapping(maps, () => null) as SourceMap,
-      }
+      // maps.push(result.map as EncodedSourceMap)
+      // map: remapping(maps, () => null) as SourceMap,
     },
   }
 }
