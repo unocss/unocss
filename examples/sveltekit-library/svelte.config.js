41ad74f0249ee092d83425f390111559f0bfdff9
jwrunner <jwrunner7@gmail.com>
2022-10-18 17:54:16 +0800

    feat: add ability to not combine classes when devving component libraries

--- examples/sveltekit-library/svelte.config.js
+++ examples/sveltekit-library/svelte.config.js
@@ -2,11 +2,19 @@ import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 import UnoCSS from '@unocss/svelte-preprocess-unocss'
 
+const mode = process.env.NODE_ENV
+const prod = mode === 'production'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [
     preprocess(),
-    UnoCSS({ options: { classPrefix: 'spu-' } }),
+    UnoCSS({
+      options: {
+        classPrefix: 'spu-',
+        combine: prod,
+      },
+    }),
   ],
 
   kit: {
--- packages/svelte-preprocess-unocss/src/index.ts
+++ packages/svelte-preprocess-unocss/src/index.ts
@@ -9,19 +9,23 @@ import { transformSvelteSFC } from '../../../packages/vite/src/modes/svelte-scop
 // unbuild throws an odd error unknown when trying to extend TransformSFCOptions here so I'm duplicating it's options
 export interface SveltePreprocessUnocssOptions {
   /**
- * Prefix for compiled class name
- * @default 'uno-'
- */
+   * Prefix for compiled class name
+   * @default 'uno-'
+   */
   classPrefix?: string
-
+  /**
+   * Add hash and combine recognized tokens (optimal for production); set false in dev mode for easy dev tools toggling to allow for design adjustments in the browser
+   * @default true
+   */
+  combine?: boolean
   /**
    * Hash function
    */
   hashFn?: (str: string) => string
   /**
-* Run @unocss/transformer-directives on style blocks
-* @default true
-*/
+   * Run @unocss/transformer-directives on style blocks
+   * @default true
+   */
   transformDirectives?: boolean
 }
 
