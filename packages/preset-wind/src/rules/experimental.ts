0003dbfc10c917f4f4224dc638af5b39135c9ce4
Chris <1633711653@qq.com>
2022-10-26 14:05:46 +0800

    chore: update

--- packages/preset-wind/src/rules/experimental.ts
+++ packages/preset-wind/src/rules/experimental.ts
@@ -6,7 +6,7 @@ export const zooms: Rule[] = [
   [/^zoom-(.+)$/, ([, d]) => {
     warnOnce('The zoom-* rules are non-standard and is not on a standards track. Do not use it on production sites facing the Web: it will not work for every user. There may also be large incompatibilities between implementations and the behavior may change in the future.')
     return { zoom: h.bracket.cssvar.fraction.global.percent(d) }
-  }, { autocomplete: 'zoom-<percent/100>' }],
+  }, { autocomplete: 'zoom-<percent>' }],
   ['zoom-normal', { zoom: 'normal' }],
   ['zoom-reset', { zoom: 'reset' }],
 ]
