3de4429db27e84d31f559dded881e7fe98266c1c
木杉 <zhmushan@qq.com>
2022-10-31 22:22:28 +0800

    fix(vscode): attributify value starts with number (#1811)

--- test/pos.test.ts
+++ test/pos.test.ts
@@ -15,7 +15,7 @@ describe('matched-positions', async () => {
       ],
     })
 
-    expect(await match(uno, '<div border="b gray4"></div>'))
+    expect(await match(uno, '<div border="b gray4 2 [&_span]:white"></div>'))
       .toMatchInlineSnapshot(`
         [
           [
@@ -27,6 +27,16 @@ describe('matched-positions', async () => {
             15,
             20,
             "[border=\\"gray4\\"]",
+          ],
+          [
+            21,
+            22,
+            "[border=\\"2\\"]",
+          ],
+          [
+            23,
+            37,
+            "[border=\\"[&_span]:white\\"]",
           ],
         ]
       `)
