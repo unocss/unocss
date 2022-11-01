25e0fdd8903544523ab7249f23d6417621011cd0
Saya <chu121su12@gmail.com>
2022-11-01 13:01:01 +0800

    fix rebase

--- packages/preset-mini/src/_variants/aria.ts
+++ packages/preset-mini/src/_variants/aria.ts
@@ -5,7 +5,7 @@ import { handler as h, variantGetParameter } from '../utils'
 export const variantAria: VariantObject = {
   name: 'aria',
   match(matcher, { theme }: VariantContext<Theme>) {
-    const variant = variantGetParameter('aria', matcher, [':', '-'])
+    const variant = variantGetParameter('aria-', matcher, [':', '-'])
     if (variant) {
       const [match, rest] = variant
       const aria = h.bracket(match) ?? theme.aria?.[match] ?? ''
