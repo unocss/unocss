52a71f409e4f937048982e0b52af45409c1e68ee
Saya <379924+chu121su12@users.noreply.github.com>
2022-11-03 02:45:11 +0800

    fix(core): fix for svelte extractor (#1830)

--- packages/core/src/extractors/svelte.ts
+++ packages/core/src/extractors/svelte.ts
@@ -1,6 +1,8 @@
 import type { Extractor } from '../types'
 import { splitCode } from './split'
 
+const rightTrimRe = /=$/
+
 export const extractorSvelte: Extractor = {
   name: 'svelte',
   order: 0,
@@ -8,7 +10,7 @@ export const extractorSvelte: Extractor = {
     let result = splitCode(code)
     if (id && id.endsWith('.svelte')) {
       result = result.map((r) => {
-        return r.startsWith('class:') ? r.slice(6) : r
+        return r.startsWith('class:') ? r.slice(6).replace(rightTrimRe, '') : r
       })
     }
     return new Set(result)
--- test/extractor.test.ts
+++ test/extractor.test.ts
@@ -1,21 +1,45 @@
-import { extractorSplit } from '@unocss/core'
+import { extractorSplit, extractorSvelte } from '@unocss/core'
 import { expect, it } from 'vitest'
 
 it('extractorSplit', async () => {
-  let code = ''
-  async function extract() {
+  async function extract(code: string) {
     return [...await extractorSplit.extract({ code, original: code }) || []]
   }
 
-  code = 'foo'
-  expect(await extract()).eql(['foo'])
+  expect(await extract('foo')).eql(['foo'])
+  expect(await extract('<div class="text-red border">foo</div>')).toContain('text-red')
+  expect(await extract('<div class="<sm:text-lg">foo</div>')).toContain('<sm:text-lg')
+  expect(await extract('"class=\"bg-white\""')).toContain('bg-white')
+})
 
-  code = '<div class="text-red border">foo</div>'
-  expect(await extract()).toContain('text-red')
+it('extractorSvelte uses regular split with non .svelte files', async () => {
+  async function extract(code: string) {
+    return [...await extractorSvelte.extract({ code, original: code }) || []]
+  }
 
-  code = '<div class="<sm:text-lg">foo</div>'
-  expect(await extract()).toContain('<sm:text-lg')
+  expect(await extract('foo')).eql(['foo'])
+  expect(await extract('<div class="text-red border">foo</div>')).toContain('text-red')
+  expect(await extract('<div class="<sm:text-lg">foo</div>')).toContain('<sm:text-lg')
+  expect(await extract('"class=\"bg-white\""')).toContain('bg-white')
 
-  code = '"class=\"bg-white\""'
-  expect(await extract()).toContain('bg-white')
+  expect(await extract('<div class:text-orange-400={foo} />')).toContain('class:text-orange-400=')
+  expect(await extract('class:text-gray-800={$page.url.pathname.startsWith(\'/test\')}')).toContain('class:text-gray-800=')
+  expect(await extract('<div class="data-[a~=b]:text-red">foo</div>')).toContain('data-[a~=b]:text-red')
+  expect(await extract('<div class:text-[32px]="{true}" />')).toContain('class:text-[32px]=')
+})
+
+it('extractorSvelte uses svelte-specific split with .svelte files', async () => {
+  async function extract(code: string) {
+    return [...await extractorSvelte.extract({ code, original: code, id: 'file.svelte' }) || []]
+  }
+
+  expect(await extract('foo')).eql(['foo'])
+  expect(await extract('<div class="text-red border">foo</div>')).toContain('text-red')
+  expect(await extract('<div class="<sm:text-lg">foo</div>')).toContain('<sm:text-lg')
+  expect(await extract('"class=\"bg-white\""')).toContain('bg-white')
+
+  expect(await extract('<div class:text-orange-400={foo} />')).toContain('text-orange-400')
+  expect(await extract('class:text-gray-800={$page.url.pathname.startsWith(\'/test\')}')).toContain('text-gray-800')
+  expect(await extract('<div class="data-[a~=b]:text-red">foo</div>')).toContain('data-[a~=b]:text-red')
+  expect(await extract('<div class:text-[32px]="{true}" />')).toContain('text-[32px]')
 })
