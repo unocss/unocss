72a3b908f5d98de96da258f7b5c4b8d7a22e8892
Jie Peng <im@jiepeng.me>
2022-12-02 18:56:01 +0800

    fix(next-example): move className to <Link> component
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- examples/next/components/navbar.tsx
+++ examples/next/components/navbar.tsx
@@ -5,11 +5,11 @@ function Navbar() {
     <nav className="px-10 bg-teal-700">
       <div className="relative flex items-center justify-between h-16">
         <div className="flex space-x-4">
-          <Link href="/" >
-            <a className="bg-teal-900 text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+          <Link href="/" className="bg-teal-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+            Home
           </Link>
-          <Link href="/blog" >
-            <a className="text-teal-100 hover:bg-teal-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Blog</a>
+          <Link href="/blog" className="text-teal-100 hover:bg-teal-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+            Blog
           </Link>
         </div>
       </div>
