f74ca75de854d125cfa87ea35952667d42c3aa41
Hongbusi <coderhbs@gmail.com>
2022-12-06 15:53:23 +0800

    chore(interactive): fix search duplicate execution

--- interactive/components/Search.vue
+++ interactive/components/Search.vue
@@ -13,13 +13,14 @@ const vFocus = {
 
 watch(
   () => route.query.s,
-  async () => {
-    input.value = String(route.query.s || '')
-    await excuteSearch()
+  async (val) => {
+    if (input.value === val) return
+    input.value = String(val || '')
+    await executeSearch()
   },
 )
 
-async function excuteSearch() {
+async function executeSearch() {
   if (input.value)
     isSearching.value = true
   try {
@@ -45,7 +46,7 @@ async function excuteSearch() {
 
 throttledWatch(
   input,
-  excuteSearch,
+  executeSearch,
   { throttle: 100, immediate: true },
 )
 
