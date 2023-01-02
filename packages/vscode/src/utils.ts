4bc32722b8fd5f9ddbe1a42914eba728633e54a2
Zam <24277775+zam157@users.noreply.github.com>
2023-01-02 13:08:35 +0800

    feat(vscode): better color preview (#2036)
    * feat(vscode): better color preview
    
    * feat(vscode): better color preview in suggestions
    
    * refactor(vscode): enhance readability

--- packages/vscode/src/utils.ts
+++ packages/vscode/src/utils.ts
@@ -1,10 +1,7 @@
 import path from 'path'
-import type { GenerateResult, UnoGenerator } from '@unocss/core'
+import type { UnoGenerator } from '@unocss/core'
 import prettier from 'prettier/standalone'
 import parserCSS from 'prettier/parser-postcss'
-import type { Theme } from '@unocss/preset-mini'
-import { parseColor } from '@unocss/preset-mini'
-import { colorToString } from '@unocss/preset-mini/utils'
 
 export function throttle<T extends ((...args: any) => any)>(func: T, timeFrame: number): T {
   let lastTime = 0
@@ -20,6 +17,11 @@ export function throttle<T extends ((...args: any) => any)>(func: T, timeFrame: 
       timer = setTimeout(func, timeFrame, ...args)
     }
   } as T
+}
+
+export const getCSS = async (uno: UnoGenerator, utilName: string) => {
+  const { css } = await uno.generate(utilName, { preflights: false, safelist: false })
+  return css
 }
 
 export async function getPrettiedCSS(uno: UnoGenerator, util: string) {
@@ -39,69 +41,84 @@ export async function getPrettiedMarkdown(uno: UnoGenerator, util: string) {
   return `\`\`\`css\n${(await getPrettiedCSS(uno, util)).prettified}\n\`\`\``
 }
 
-export function body2ColorValue(body: string, theme: Theme) {
-  const themeColorNames = Object.keys(theme.colors ?? {})
-  const colorNames = themeColorNames.concat(themeColorNames.map(colorName => colorName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()))
-
-  for (const colorName of colorNames) {
-    const nameIndex = body.indexOf(colorName)
-
-    if (nameIndex > -1) {
-      const parsedResult = parseColor(body.substring(nameIndex), theme)
-      if (parsedResult?.cssColor)
-        return parsedResult
-    }
+const getCssVariables = (code: string) => {
+  const regex = /(?<key>--\S+?):\s*(?<value>.+?);/gm
+  const cssVariables = new Map<string, string>()
+  for (const match of code.matchAll(regex)) {
+    const key = match.groups?.key
+    if (key)
+      cssVariables.set(key, match.groups?.value ?? '')
   }
 
-  return null
+  return cssVariables
 }
 
-const matchedAttributifyRE = /(?<=^\[.+~=").*(?="\]$)/
-const matchedValuelessAttributifyRE = /(?<=^\[).+(?==""\]$)/
-const _colorsMapCache = new Map<string, string>()
-export async function getColorsMap(uno: UnoGenerator, result: GenerateResult) {
-  const theme = uno.config.theme as Theme
-  const colorsMap = new Map<string, string>()
+const matchCssVarNameRegex = /var\((?<cssVarName>--[^,|)]+)(?:,\s*(?<fallback>[^)]+))?\)/gm
+const cssColorRegex = /(?:#|0x)(?:[a-f0-9]{3}|[a-f0-9]{6})\b|(?:rgb|hsl)a?\(.*\)/gm
 
-  for (const i of result.matched) {
-    if (!(await isColorUtility(uno, i)))
-      continue
+/**
+ * Get CSS color string from CSS string
+ *
+ * @example Input with CSS var
+ * ```css
+ *.dark [border="dark\:gray-700"] {
+ *  --un-border-opacity: 1;
+ *  border-color: rgba(55, 65, 81, var(--un-border-opacity));
+ *}
+ * ```
+ * return `rgba(55, 65, 81, 1)`
+ *
+ * @example Input with no-value CSS var and its fallback value
+ * ```css
+ *.bg-brand-primary {
+ *  background-color: hsla(217, 78%, 51%, var(--no-value, 0.5));
+ *}
+ * ```
+ * return `hsla(217, 78%, 51%, 0.5)`
+ *
+ * @example Input with no-value CSS var
+ * ```css
+ *.bg-brand-primary {
+ *  background-color: hsla(217, 78%, 51%, var(--no-value));
+ *}
+ * ```
+ * return `hsla(217, 78%, 51%)`
+ *
+ * @param str - CSS string
+ * @returns The **first** CSS color string (hex, rgb[a], hsl[a]) or `undefined`
+ */
+export const getColorString = (str: string) => {
+  let colorString = str.match(cssColorRegex)?.[0] // e.g rgba(248, 113, 113, var(--maybe-css-var))
 
-    const matchedValueless = i.match(matchedValuelessAttributifyRE)?.[0]
-    const colorKey = matchedValueless ?? i.replace('~="', '="')
+  if (!colorString)
+    return
 
-    const cachedColor = _colorsMapCache.get(colorKey)
-    if (cachedColor) {
-      colorsMap.set(colorKey, cachedColor)
-      continue
-    }
+  const cssVars = getCssVariables(str)
 
-    const matchedAttr = i.match(matchedAttributifyRE)?.[0] ?? matchedValueless
-    const body = (matchedAttr ?? i)
-      .split(':').slice(-1)[0] ?? '' // remove prefix e.g. `dark:` `hover:`
+  // replace `var(...)` with its value
+  for (const match of colorString.matchAll(matchCssVarNameRegex)) {
+    const matchedString = match[0]
+    const cssVarName = match.groups?.cssVarName
+    const fallback = match.groups?.fallback
 
-    if (body) {
-      const colorValue = body2ColorValue(body, theme)
-      if (colorValue) {
-        const colorString = colorToString(colorValue.cssColor!, colorValue.alpha)
-        colorsMap.set(colorKey, colorString)
-        _colorsMapCache.set(colorKey, colorString)
-      }
-    }
+    if (cssVarName && cssVars.get(cssVarName))
+      // rgba(248, 113, 113, var(--un-text-opacity)) => rgba(248, 113, 113, 1)
+      colorString = colorString.replaceAll(matchedString, cssVars.get(cssVarName) ?? matchedString)
+    else if (fallback)
+      // rgba(248, 113, 113, var(--no-value, 0.5)) => rgba(248, 113, 113, 0.5)
+      colorString = colorString.replaceAll(matchedString, fallback)
+
+    // rgba(248, 113, 113, var(--no-value)) => rgba(248, 113, 113)
+    colorString = colorString.replaceAll(/,?\s+var\(--.*?\)/gm, '')
   }
 
-  if (_colorsMapCache.size > 5000)
-    _colorsMapCache.clear()
+  // if (!(new TinyColor(colorString).isValid))
+  //   return
 
-  return colorsMap
+  return colorString
 }
 
 export function isSubdir(parent: string, child: string) {
   const relative = path.relative(parent, child)
   return relative && !relative.startsWith('..') && !path.isAbsolute(relative)
-}
-
-async function isColorUtility(uno: UnoGenerator, utilName: string) {
-  const { css } = await uno.generate(utilName, { preflights: false, safelist: false })
-  return css.includes('color')
 }
