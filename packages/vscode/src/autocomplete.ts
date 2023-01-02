be463b6748dfcf6558a8ebcfc0e91e7f876a2dff
Frozen FIsh <76603360+sudongyuer@users.noreply.github.com>
2023-01-02 14:48:47 +0800

    chore: update

--- packages/vscode/src/autocomplete.ts
+++ packages/vscode/src/autocomplete.ts
@@ -3,7 +3,7 @@ import { createAutocomplete } from '@unocss/autocomplete'
 import type { CompletionItemProvider, ExtensionContext } from 'vscode'
 import { CompletionItem, CompletionItemKind, CompletionList, MarkdownString, Range, languages } from 'vscode'
 import type { UnoGenerator, UnocssPluginContext } from '@unocss/core'
-import { getCSS, getColorString, getPrettiedCSS, getPrettiedMarkdown, isSubdir } from './utils'
+import { body2ColorValue, getPrettiedCSS, getPrettiedMarkdown, isSubdir } from './utils'
 import { log } from './log'
 import type { ContextLoader } from './contextLoader'
 import { isCssId } from './integration'
@@ -96,25 +96,23 @@ export async function registerAutoComplete(
         if (!result.suggestions.length)
           return
 
-        const completionItems: UnoCompletionItem[] = []
-        for (const [value, label] of result.suggestions) {
-          const css = await getCSS(ctx!.uno, value)
-          const colorString = getColorString(css)
-          const itemKind = colorString ? CompletionItemKind.Color : CompletionItemKind.EnumMember
-          const item = new UnoCompletionItem(label, itemKind, ctx!.uno)
+        const theme = ctx?.uno.config.theme
+        return new CompletionList(result.suggestions.map(([value, label]) => {
+          const colorValue = theme ? body2ColorValue(value, theme) : null
+          const itemKind = colorValue?.color ? CompletionItemKind.Color : CompletionItemKind.EnumMember
+
           const resolved = result.resolveReplacement(value)
-          
+          const item = new UnoCompletionItem(value, itemKind, ctx!.uno)
           item.insertText = resolved.replacement
           item.range = new Range(doc.positionAt(resolved.start), doc.positionAt(resolved.end))
 
-          if (colorString) {
-            item.documentation = colorString
+          if (colorValue?.color) {
+            item.documentation = colorValue?.color
             item.sortText = /-\d$/.test(label) ? '1' : '2' // reorder color completions
           }
-          completionItems.push(item)
-        }
 
-        return new CompletionList(completionItems, true)
+          return item
+        }), true)
       }
       catch (e) {
         log.appendLine(`⚠️ ${String(e)}`)
