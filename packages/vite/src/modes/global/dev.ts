d4f471705d1dd31d5553d33176204b7b5a47f512
LittleSound <464388324@qq.com>
2023-01-10 19:56:14 +0800

    fix: top level await

--- packages/vite/src/modes/global/dev.ts
+++ packages/vite/src/modes/global/dev.ts
@@ -168,12 +168,14 @@ export function GlobalModeDevPlugin({ uno, tokens, tasks, flushTasks, affectedMo
         // inject css modules to send callback on css load
         if (layer && code.includes('import.meta.hot')) {
           return `${code}
-if (import.meta.hot) {
-  try { await import.meta.hot.send('${WS_EVENT_PREFIX}', ['${layer}', __vite__css.slice(2,${2 + HASH_LENGTH})]); }
-  catch (e) { console.warn('[unocss-hmr]', e) }
-  if (!import.meta.url.includes('?'))
-    await new Promise(resolve => setTimeout(resolve, 100))
-}`
+;(async function() {
+  if (import.meta.hot) {
+    try { await import.meta.hot.send('${WS_EVENT_PREFIX}', ['${layer}', __vite__css.slice(2,${2 + HASH_LENGTH})]); }
+    catch (e) { console.warn('[unocss-hmr]', e) }
+    if (!import.meta.url.includes('?'))
+      await new Promise(resolve => setTimeout(resolve, 100))
+  }
+})()`
         }
       },
     },
