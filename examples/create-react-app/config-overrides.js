593b5117d69fec22d297ee27f2bc384df0329fa6
wangfengyuan <644301374@qq.com>
2023-01-16 09:40:15 +0800

    docs(example): fix eslint error

--- examples/create-react-app/config-overrides.js
+++ examples/create-react-app/config-overrides.js
@@ -1,27 +1,29 @@
-const path = require('path');
-const { override, addWebpackPlugin } = require('customize-cra');
-const UnoCss = require("@unocss/webpack").default;
-const { presetIcons, presetUno } = require("unocss");
+const path = require('path')
+const { override, addWebpackPlugin } = require('customize-cra')
+const UnoCss = require('@unocss/webpack').default
+const { presetIcons, presetUno } = require('unocss')
 
 module.exports = {
-    webpack: override(
-        addWebpackPlugin(
-            UnoCss({
-                shortcuts: [
-                    { logo: 'i-logos-react w-6em h-6em transform transition-800 hover:rotate-180' },
-                ],
-                presets: [
-                    presetIcons(),
-                    presetUno()
-                ],
-            }),
-        ),
-        (config) => {
-            const ModuleScopePlugin = config.resolve.plugins.find(plugin => plugin.constructor.name === 'ModuleScopePlugin');
-            ModuleScopePlugin.allowedPaths.push(path.join(__dirname, '_virtual_%2F__uno.css'));
-            config.optimization.realContentHash = true;
-            return config;
-        },
+  webpack: override(
+    addWebpackPlugin(
+      UnoCss({
+        shortcuts: [
+          { logo: 'i-logos-react w-6em h-6em transform transition-800 hover:rotate-180' },
+        ],
+        presets: [
+          presetIcons(),
+          presetUno(),
+        ],
+      }),
     ),
+    (config) => {
+      // import 'uno.css' will be resolved to import '/_virtual_%2F__uno.css', but create-react-app built-in plugin ModuleScopePlugin prevents users from importing from the virtual module path, so need to add it to the allowPaths
+      const ModuleScopePlugin = config.resolve.plugins.find(plugin => plugin.constructor.name === 'ModuleScopePlugin')
+      ModuleScopePlugin.allowedPaths.push(path.join(__dirname, '_virtual_%2F__uno.css'))
+      config.optimization.realContentHash = true
+      config.cache = false
+      return config
+    },
+  ),
 
-};
+}
