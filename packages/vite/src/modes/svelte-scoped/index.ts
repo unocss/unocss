c4b61c0f4889f5f80e2a0d3e180c928e4c9b70d1
jacob-8 <jwrunner7@gmail.com>
2023-01-20 17:35:01 +0800

    cleanup: no need to check for build mode in handleHotUpdate hook

--- packages/vite/src/modes/svelte-scoped/index.ts
+++ packages/vite/src/modes/svelte-scoped/index.ts
@@ -16,6 +16,7 @@ export function SvelteScopedPlugin({ ready, uno }: UnocssPluginContext): Plugin 
   return {
     name: 'unocss:svelte-scoped',
     enforce: 'pre',
+
     async configResolved(_viteConfig) {
       viteConfig = _viteConfig
       const { config } = await ready
@@ -24,6 +25,7 @@ export function SvelteScopedPlugin({ ready, uno }: UnocssPluginContext): Plugin 
         config.exclude || defaultExclude,
       )
     },
+
     transform(code, id) {
       if (id.includes('hooks.server')) {
         return {
@@ -34,12 +36,13 @@ export function SvelteScopedPlugin({ ready, uno }: UnocssPluginContext): Plugin 
         return
       return transformSvelteSFC(code, id, uno, { combine: viteConfig.command === 'build' })
     },
+
     handleHotUpdate(ctx) {
       const read = ctx.read
       if (filter(ctx.file)) {
         ctx.read = async () => {
           const code = await read()
-          return (await transformSvelteSFC(code, ctx.file, uno, { combine: viteConfig.command === 'build' }))?.code || code
+          return (await transformSvelteSFC(code, ctx.file, uno, { combine: false }))?.code || code
         }
       }
     },
