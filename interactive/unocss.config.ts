7ce275695725c82dc012f4c4f0c2702645c5a1d0
Anthony Fu <anthonyfu117@hotmail.com>
2023-01-24 16:40:40 +0100

    chore: fix interactive docs

--- interactive/unocss.config.ts
+++ interactive/unocss.config.ts
@@ -32,6 +32,11 @@ export default defineConfig({
     }),
   ],
   shortcuts: [
+    [/^badge-(.*)$/, ([, c]) => `bg-${c}4:10 text-${c}5 rounded`],
+    [/^badge-xs-(.*)$/, ([, c]) => `badge-${c} text-xs px2 py0.5`],
+    [/^badge-sm-(.*)$/, ([, c]) => `badge-${c} text-sm px3 py0.6`],
+    [/^badge-lg-(.*)$/, ([, c]) => `badge-${c} px3 py0.8`],
+    [/^badge-square-(.*)$/, ([, c]) => `badge-${c} w-7 h-7 text-lg font-200 flex flex-none items-center justify-center`],
     {
       'border-base': 'border-gray-500:10',
       'bg-base': 'bg-white dark:bg-hex-121212',
@@ -39,11 +44,6 @@ export default defineConfig({
       'link': 'op50 hover:op100 hover:text-cyan6',
       'divider': 'border-b border-base',
     },
-    [/^badge-xs-(.*)$/, ([, c]) => `badge-${c} text-xs px2 py0.5`],
-    [/^badge-sm-(.*)$/, ([, c]) => `badge-${c} text-sm px3 py0.6`],
-    [/^badge-lg-(.*)$/, ([, c]) => `badge-${c} px3 py0.8`],
-    [/^badge-square-(.*)$/, ([, c]) => `badge-${c} w-7 h-7 text-lg font-200 flex flex-none items-center justify-center`],
-    [/^badge-(.*)$/, ([, c]) => `bg-${c}4:10 text-${c}5 rounded`],
   ],
   transformers: [
     transformerVariantGroup(),
