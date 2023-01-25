unocss/unocss

commit b7a6687d218827f1dc70e7aa61aa67bfbff838d9

feat(vite, webpack): add `extraContent` option (#1963)
* feat(vite): add `watchExternal` option

* chore: lint

* refactor: api

Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

packages/vite/src/modes/global/build.ts
@@ -14,11 +14,12 @@ import {
   resolveLayer,
 } from '../../integration'
 import type { VitePluginConfig } from '../../types'
+import { setupExtraContent } from '../../../../shared-integration/src/extra-content'
 
-export function GlobalModeBuildPlugin({ uno, ready, extract, tokens, filter, getConfig }: UnocssPluginContext<VitePluginConfig>): Plugin[] {
+export function GlobalModeBuildPlugin(ctx: UnocssPluginContext<VitePluginConfig>): Plugin[] {
+  const { uno, ready, extract, tokens, filter, getConfig, tasks, flushTasks } = ctx
   const vfsLayers = new Set<string>()
   const layerImporterMap = new Map<string, string>()
-  let tasks: Promise<any>[] = []
   let viteConfig: ResolvedConfig
 
   // use maps to differentiate multiple build. using outDir as key
@@ -46,7 +47,7 @@ export function GlobalModeBuildPlugin({ uno, ready, extract, tokens, filter, get
   let lastTokenSize = 0
   let lastResult: GenerateResult | undefined
   async function generateAll() {
-    await Promise.all(tasks)
+    await flushTasks()
     if (lastResult && lastTokenSize === tokens.size)
       return lastResult
     lastResult = await uno.generate(tokens, { minify: true })
@@ -54,13 +55,16 @@ export function GlobalModeBuildPlugin({ uno, ready, extract, tokens, filter, get
     return lastResult
   }
 
+  let replaced = false
+
   return [
     {
       name: 'unocss:global:build:scan',
       apply: 'build',
       enforce: 'pre',
-      buildStart() {
-        tasks = []
+      async buildStart() {
+        vfsLayers.clear()
+        tasks.length = 0
         lastTokenSize = 0
         lastResult = undefined
       },
@@ -144,6 +148,16 @@ export function GlobalModeBuildPlugin({ uno, ready, extract, tokens, filter, get
       },
     },
     {
+      name: 'unocss:global:content',
+      enforce: 'pre',
+      configResolved(config) {
+        viteConfig = config
+      },
+      buildStart() {
+        tasks.push(setupExtraContent(ctx, viteConfig.command === 'serve'))
+      },
+    },
+    {
       name: 'unocss:global:build:generate',
       apply: 'build',
       async renderChunk(code, chunk, options) {
@@ -175,9 +189,6 @@ export function GlobalModeBuildPlugin({ uno, ready, extract, tokens, filter, get
     {
       name: 'unocss:global:build:bundle',
       apply: 'build',
-      configResolved(config) {
-        viteConfig = config
-      },
       enforce: 'post',
       // rewrite the css placeholders
       async generateBundle(options, bundle) {
@@ -189,12 +200,17 @@ export function GlobalModeBuildPlugin({ uno, ready, extract, tokens, filter, get
           return
 
         if (!vfsLayers.size) {
+          // If `vfsLayers` is empty and `replaced` is true, that means
+          // `generateBundle` hook is called on previous build pipeline. e.g. ssr
+          // Since we already replaced the layers and don't have any more layers
+          // to replace on current build pipeline, we can skip the warning.
+          if (replaced)
+            return
           const msg = '[unocss] entry module not found, have you add `import \'uno.css\'` in your main entry?'
           this.warn(msg)
           return
         }
 
-        let replaced = false
         const getLayer = (layer: string, input: string, replace = false) => {
           const re = new RegExp(`#--unocss-layer-start--${layer}--\\{start:${layer}\\}([\\s\\S]*?)#--unocss-layer-end--${layer}--\\{end:${layer}\\}`, 'g')
           if (replace)

examples/astro-vue/.gitignore
@@ -1,0 +1,20 @@
+# build output
+dist/
+.output/
+
+# dependencies
+node_modules/
+
+# logs
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+pnpm-debug.log*
+
+
+# environment variables
+.env
+.env.production
+
+# macOS-specific files
+.DS_Store

examples/astro-vue/astro.config.ts
@@ -1,0 +1,10 @@
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
+import vue from '@astrojs/vue'
+
+export default defineConfig({
+  integrations: [
+    vue(),
+    UnoCSS(),
+  ],
+})

examples/astro-vue/src/components/Card.astro
@@ -1,0 +1,21 @@
+---
+export interface Props {
+	title: string;
+	body: string;
+	href: string;
+}
+
+const { href, title, body } = Astro.props as Props;
+---
+
+<li class="border rounded inline-block px4 py2 m2 hover:shadow">
+	<a href={href}>
+		<h2 class="font-bold">
+			{title}
+			<span>&rarr;</span>
+		</h2>
+		<p>
+			{body}
+		</p>
+	</a>
+</li>

examples/astro-vue/src/components/Example.vue
@@ -1,0 +1,11 @@
+<template>
+  <div class=":uno: text-(xl red-400) mt-8">
+    Example Vue component
+  </div>
+</template>
+
+<style scoped>
+div {
+  @apply mt-8;
+}
+</style>

examples/astro-vue/src/env.d.ts
@@ -1,0 +1,1 @@
+/// <reference types="astro/client" />

examples/astro-vue/src/layouts/Layout.astro
@@ -1,0 +1,19 @@
+---
+export interface Props {
+	title: string;
+}
+
+const { title } = Astro.props as Props;
+---
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width" />
+		<meta name="generator" content={Astro.generator} />
+		<title>{title}</title>
+	</head>
+	<body class="font-sans text-center">
+		<slot />
+	</body>
+</html>

examples/astro-vue/src/pages/foo.astro
@@ -1,0 +1,15 @@
+---
+import Layout from '../layouts/Layout.astro';
+import Card from '../components/Card.astro';
+---
+
+<Layout title="Foo Page">
+  <div class="text-center text-lg mt-10">Foo</div>
+  <ul role="list" class="link-card-grid">
+    <Card
+      href="/"
+      title="Home"
+      body="Go home."
+    />
+  </ul>
+</Layout>

commit 3b414daf91cf57bce03edfa5a885ab09b3ef17e2

fix: `vite build --watch` not emitting anything (#2108)
There was a regression introduced by b7a6687d218827f1dc70e7aa61aa67bfbff838d9
where vite projects built using `vite build --watch` will only generate once.
Subsequent generations will just leave the placeholder.

See https://github.com/unocss/unocss/pull/963

packages/vite/src/modes/global/build.ts
@@ -93,8 +93,10 @@ export function GlobalModeBuildPlugin(ctx: UnocssPluginContext<VitePluginConfig>
       },
       load(id) {
         const layer = resolveLayer(getPath(id))
-        if (layer)
+        if (layer) {
+          vfsLayers.add(layer)
           return getLayerPlaceholder(layer)
+        }
       },
       moduleParsed({ id, importedIds }) {
         if (!layerImporterMap.has(id))