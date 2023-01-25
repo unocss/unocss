3b5fad468dd573f6803c1160151a5398b4add853
Artem Melnyk <51422045+MellKam@users.noreply.github.com>
2023-01-25 12:31:43 +0100

    feat(vscode): configure multiple subdirs (#2101)

--- packages/vscode/src/index.ts
+++ packages/vscode/src/index.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import type { ExtensionContext } from 'vscode'
+import type { ExtensionContext, StatusBarItem } from 'vscode'
 import { StatusBarAlignment, commands, window, workspace } from 'vscode'
 import { version } from '../package.json'
 import { log } from './log'
@@ -7,6 +7,23 @@ import { registerAnnotations } from './annotation'
 import { registerAutoComplete } from './autocomplete'
 import { ContextLoader } from './contextLoader'
 import { registerSelectionStyle } from './selectionStyle'
+
+const registerRoot = async (ext: ExtensionContext, status: StatusBarItem, cwd: string) => {
+  const contextLoader = new ContextLoader(cwd)
+
+  await contextLoader.ready
+
+  const hasConfig = await contextLoader.loadContextInDirectory(cwd)
+  // TODO: improve this to re-enable after configuration created
+  if (!hasConfig)
+    throw new Error(`➖ No config found in ${cwd}`)
+
+  registerAutoComplete(cwd, contextLoader, ext)
+  registerAnnotations(cwd, contextLoader, status, ext)
+  registerSelectionStyle(cwd, contextLoader)
+
+  return contextLoader
+}
 
 export async function activate(ext: ExtensionContext) {
   log.appendLine(`⚪️ UnoCSS for VS Code v${version}\n`)
@@ -24,34 +41,50 @@ export async function activate(ext: ExtensionContext) {
     return
   }
 
-  const root = config.get<string>('root')
-  const cwd = root ? path.resolve(projectPath, root) : projectPath
-
-  const contextLoader = new ContextLoader(cwd)
-
-  ext.subscriptions.push(
-    commands.registerCommand('unocss.reload', async () => {
-      log.appendLine('🔁 Reloading...')
-      await contextLoader.reload()
-      log.appendLine('✅ Reloaded.')
-    }),
-  )
-
-  await contextLoader.ready
-
-  const hasConfig = await contextLoader.loadContextInDirectory(cwd)
-  // TODO: improve this to re-enable after configuration created
-  if (!hasConfig) {
-    log.appendLine('➖ No config found, UnoCSS is disabled')
-    return
-  }
-
   const status = window.createStatusBarItem(StatusBarAlignment.Right, 200)
   status.text = 'UnoCSS'
 
-  registerAutoComplete(cwd, contextLoader, ext)
-  registerAnnotations(cwd, contextLoader, status, ext)
-  registerSelectionStyle(cwd, contextLoader)
+  const root = config.get<string | string[]>('root')
+
+  if (Array.isArray(root) && root.length) {
+    const cwds = root.map(dir => path.resolve(projectPath, dir))
+
+    try {
+      const contextLoaders = await Promise.all(cwds.map(cwd => registerRoot(ext, status, cwd)))
+
+      ext.subscriptions.push(
+        commands.registerCommand('unocss.reload', async () => {
+          log.appendLine('🔁 Reloading...')
+          await Promise.all(contextLoaders.map(ctxLoader => ctxLoader.reload))
+          log.appendLine('✅ Reloaded.')
+        }),
+      )
+    }
+    catch (error) {
+      log.appendLine((error as Error).message)
+    }
+    return
+  }
+
+  // now if the root is an array, then it is an empty array
+  const cwd = root && !Array.isArray(root)
+    ? path.resolve(projectPath, root)
+    : projectPath
+
+  try {
+    const contextLoader = await registerRoot(ext, status, cwd)
+
+    ext.subscriptions.push(
+      commands.registerCommand('unocss.reload', async () => {
+        log.appendLine('🔁 Reloading...')
+        await contextLoader.reload()
+        log.appendLine('✅ Reloaded.')
+      }),
+    )
+  }
+  catch (error) {
+    log.appendLine((error as Error).message)
+  }
 }
 
 export function deactivate() {}
