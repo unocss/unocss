bee5a9a6900295905f971385c61d3089b66db725
sibbng <sibbngheid@gmail.com>
2023-02-14 00:46:46 +0300

    fix(transformer-variant-group): properly scan arbitrary variants in the same line

--- test/transformer-variant-group.test.ts
+++ test/transformer-variant-group.test.ts
@@ -23,6 +23,7 @@ describe('transformer-variant-group', () => {
       'hover:(!m-2 p-2)',
       'md:(w-1/2 h-[calc(100%-4rem)])',
       'hover:(\n!m-2 \np-2\n)',
+      '[&]:(w-4 h-4) [&]:(w-4 h-4)',
     ]
 
     for (const c of cases) {
