unocss/unocss

commit 03c56c8e7468a4a382f7c587a8e53a42cf46c03e

fix(vscode): highlight classes with `><` characters (#2256)

packages/shared-common/src/index.ts
@@ -98,7 +98,7 @@ export function getMatchedPositions(code: string, matched: string[], hasVariantG
 
   // highlight for plain classes
   let start = 0
-  code.split(/([\s"'`;<>*]|:\(|\)"|\)\s)/g).forEach((i) => {
+  code.split(/([\s"'`;*]|:\(|\)"|\)\s)/g).forEach((i) => {
     const end = start + i.length
     if (isPug) {
       result.push(...getPlainClassMatchedPositionsForPug(i, plain, start))
@@ -133,7 +133,7 @@ export function getMatchedPositions(code: string, matched: string[], hasVariantG
         const [, pre, sep, body] = match
         const index = match.index!
         let start = index + pre.length + sep.length + 1
-        body.split(/([\s"'`;<>*]|:\(|\)"|\)\s)/g).forEach((i) => {
+        body.split(/([\s"'`;*]|:\(|\)"|\)\s)/g).forEach((i) => {
           const end = start + i.length
           const full = pre + sep + i
           if (plain.has(full)) {

test/pos.test.ts
@@ -110,28 +110,41 @@ describe('matched-positions', async () => {
       presets: [
         presetUno(),
       ],
+      shortcuts: {
+        '<custom-shortcut': 'text-lg',
+      },
     })
 
-    expect(await match(uno, '<div class="bg-gray-900 h-4 hover:scale-100"></div>'))
+    expect(await match(uno, '<div class="bg-gray-900 h-4 hover:scale-100 [&>span]:text-white <custom-shortcut"></div>'))
       .toMatchInlineSnapshot(`
-      [
         [
-          12,
-          23,
-          "bg-gray-900",
-        ],
-        [
-          24,
-          27,
-          "h-4",
-        ],
-        [
-          28,
-          43,
-          "hover:scale-100",
-        ],
-      ]
-    `)
+          [
+            12,
+            23,
+            "bg-gray-900",
+          ],
+          [
+            24,
+            27,
+            "h-4",
+          ],
+          [
+            28,
+            43,
+            "hover:scale-100",
+          ],
+          [
+            44,
+            63,
+            "[&>span]:text-white",
+          ],
+          [
+            64,
+            80,
+            "<custom-shortcut",
+          ],
+        ]
+      `)
   })
 
   test('variant-group', async () => {

commit be6cf6b851127ede4e2ef5052538bbd1e4b92738

fix(vscode): fix the regressions caused by #2256

packages/core/src/utils/variantGroup.ts
@@ -6,7 +6,7 @@ const regexCache: Record<string, RegExp> = {}
 export function makeRegexClassGroup(separators = ['-', ':']) {
   const key = separators.join('|')
   if (!regexCache[key])
-    regexCache[key] = new RegExp(`((?:[!@\\w+:_/-]|\\[&?>?:?\\S*\\])+?)(${key})\\(((?:[~!\\w\\s:/\\\\,%#.$?-]|\\[.*?\\])+?)\\)(?!\\s*?=>)`, 'gm')
+    regexCache[key] = new RegExp(`((?:[!@\\w+:_/-]|\\[&?>?:?\\S*\\])+?)(${key})\\(((?:[~!<>\\w\\s:/\\\\,%#.$?-]|\\[.*?\\])+?)\\)(?!\\s*?=>)`, 'gm')
   regexCache[key].lastIndex = 0
   return regexCache[key]
 }

packages/shared-common/src/index.ts
@@ -88,17 +88,27 @@ export function getMatchedPositions(code: string, matched: string[], hasVariantG
   Array.from(matched)
     .forEach((v) => {
       const match = isAttributifySelector(v)
-      if (!match)
+
+      if (!match) {
+        // highlight classes that includes `><`
+        if (v.match(/[><]/)) {
+          for (const match of code.matchAll(new RegExp(escapeRegExp(v), 'g'))) {
+            const start = match.index!
+            const end = start + match[0].length
+            result.push([start, end, match[0]])
+          }
+        }
         plain.add(v)
-      else if (!match[2])
+      }
+      else if (!match[2]) {
         plain.add(match[1])
-      else
-        attributify.push(match)
+      }
+      else { attributify.push(match) }
     })
 
   // highlight for plain classes
   let start = 0
-  code.split(/([\s"'`;*]|:\(|\)"|\)\s)/g).forEach((i) => {
+  code.split(/([\s"'`;<>*]|:\(|\)"|\)\s)/g).forEach((i) => {
     const end = start + i.length
     if (isPug) {
       result.push(...getPlainClassMatchedPositionsForPug(i, plain, start))

test/pos.test.ts
@@ -18,7 +18,7 @@ describe('matched-positions', async () => {
       ],
     })
 
-    expect(await match(uno, '<div border="b gray4 2 [&_span]:white"></div>'))
+    expect(await match(uno, '<div border="b gray4 2 [&_span]:white" hover="[&>span]:text-white" border></div>'))
       .toMatchInlineSnapshot(`
         [
           [
@@ -40,6 +40,16 @@ describe('matched-positions', async () => {
             23,
             37,
             "[border=\\"[&_span]:white\\"]",
+          ],
+          [
+            46,
+            65,
+            "[hover=\\"[&>span]:text-white\\"]",
+          ],
+          [
+            67,
+            73,
+            "border",
           ],
         ]
       `)
@@ -152,12 +162,15 @@ describe('matched-positions', async () => {
       presets: [
         presetUno(),
       ],
+      shortcuts: {
+        '<custom-shortcut': 'text-lg',
+      },
       transformers: [
         transformerVariantGroup(),
       ],
     })
 
-    expect(await match(uno, '<div class="hover:(h-4 w-4 bg-green-300) disabled:opacity-50"></div>'))
+    expect(await match(uno, '<div class="hover:(h-4 w-4 bg-green-300 <custom-shortcut) disabled:opacity-50"></div>'))
       .toMatchInlineSnapshot(`
         [
           [
@@ -176,8 +189,13 @@ describe('matched-positions', async () => {
             "hover:bg-green-300",
           ],
           [
-            41,
-            60,
+            40,
+            56,
+            "hover:<custom-shortcut",
+          ],
+          [
+            58,
+            77,
             "disabled:opacity-50",
           ],
         ]