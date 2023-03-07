9f52d63cd53229584d4d36fd1a4a726b80fb0c15
kirin <wangkyrin@gmail.com>
2023-03-07 11:03:00 +0800

    fix: watcher

--- packages/cli/src/watcher.ts
+++ packages/cli/src/watcher.ts
@@ -10,10 +10,13 @@ export const getWatcher = async (options?: CliOptions) => {
 
   const { watch } = await import('chokidar')
   const ignored = ['**/{.git,node_modules}/**']
-  return watch(options?.patterns as string[], {
+  // cli may create multiple watchers
+  const newWatcher = watch(options?.patterns as string[], {
     ignoreInitial: true,
     ignorePermissionErrors: true,
     ignored,
     cwd: options?.cwd || process.cwd(),
   })
+  watcher = newWatcher
+  return newWatcher
 }
