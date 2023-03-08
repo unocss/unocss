228928e4f5b8d9fedb62dd4895bb9fd5f8296f8a
sibbng <sibbngheid@gmail.com>
2023-03-08 16:59:41 +0300

    feat(transformer-attributify-jsx): add babel transformer

--- packages/transformer-attributify-jsx/build.config.ts
+++ packages/transformer-attributify-jsx/build.config.ts
@@ -3,11 +3,15 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     'src/index',
+    'src/babel',
   ],
   clean: true,
   declaration: true,
   externals: [
     'magic-string',
+    '@babel/core',
+    '@babel/preset-typescript',
+    '@babel/plugin-syntax-jsx',
   ],
   rollup: {
     emitCJS: true,
