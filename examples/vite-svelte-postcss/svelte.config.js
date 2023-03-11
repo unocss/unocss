37bba970d7b6e4a2260ef7dbf50a1519788e8169
sibbng <sibbngheid@gmail.com>
2023-03-11 03:20:20 +0300

    use `vitePreprocess`

--- examples/vite-svelte-postcss/svelte.config.js
+++ examples/vite-svelte-postcss/svelte.config.js
@@ -1,12 +1,5 @@
-import sveltePreprocess from 'svelte-preprocess'
-import unocss from '@unocss/postcss'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess({
-    postcss: {
-      plugins: [unocss()],
-    },
-  }),
+  preprocess: [vitePreprocess()],
 }
