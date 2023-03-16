20fe4d67f61f0bf5f4306ee53dbd2a9c3272028b
Saya <chu121su12@gmail.com>
2023-03-16 08:56:28 +0800

    fix(preset-wind): use 0px multiple on space-x and divide-x rules

--- packages/preset-wind/src/rules/divide.ts
+++ packages/preset-wind/src/rules/divide.ts
@@ -21,8 +21,11 @@ export const divides: Rule[] = [
 ]
 
 function handlerDivide([, d, s]: string[], { theme }: RuleContext<Theme>): CSSEntries | undefined {
-  const v = theme.lineWidth?.[s || 'DEFAULT'] ?? h.bracket.cssvar.px(s || '1')
+  let v = theme.lineWidth?.[s || 'DEFAULT'] ?? h.bracket.cssvar.px(s || '1')
   if (v != null) {
+    if (v === '0')
+      v = '0px'
+
     const results = directionMap[d].map((item): [string, string] => {
       const key = `border${item}-width`
       const value = (item.endsWith('right') || item.endsWith('bottom'))
