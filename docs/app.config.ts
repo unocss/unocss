f29b5995f85210d7b25a1189de8acdb1c79b2819
Luc Potage <luc.potage@gmail.com>
2023-03-20 15:23:43 +0100

    chore: upgrade nuxt and @nuxt-themes/docus

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,26 +1,18 @@
 export default defineAppConfig({
+  github: {
+    owner: 'unocss',
+    repo: 'unocss',
+    branch: 'main',
+  },
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: 'UnoCSS',
+    description: 'The instant on-demand Atomic CSS engine',
     socials: {
-      twitter: 'nuxtstudio',
-      github: 'nuxt-themes/docus',
-    },
-    aside: {
-      level: 0,
-      exclude: [],
+      twitter: '@antfu7',
+      github: 'unocss/unocss',
     },
     header: {
-      logo: true,
-    },
-    footer: {
-      iconLinks: [
-        {
-          href: 'https://nuxt.com',
-          icon: 'simple-icons:nuxtdotjs',
-        },
-      ],
+      logo: 'Logo',
     },
   },
 })
