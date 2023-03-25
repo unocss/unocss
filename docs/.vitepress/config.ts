c17b5cf4ec49bff90354d08f9bb2ec06890cbc90
userquin <userquin@gmail.com>
2023-03-25 18:09:09 +0100

    chore: add license and some seo entries

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -108,14 +108,30 @@ function sidebarGuide() {
   ]
 }
 
+// TODO: verify this is the correct url
+const ogUrl = 'https://unocss.dev/'
+const ogImage = `${ogUrl}og-image.png`
+const title = 'UnoCSS'
+const description = 'The instant on-demand Atomic CSS engine'
+
 export default defineConfig({
   lang: 'en-US',
-  title: 'UnoCSS',
-  titleTemplate: 'UnoCSS',
-  description: 'The instant on-demand Atomic CSS engine',
+  title,
+  titleTemplate: title,
+  description,
   head: [
-    ['meta', { name: 'og:title', content: 'UnoCSS' }],
-    ['meta', { property: 'og:image', content: '/cover/default.png' }],
+    ['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' }],
+    ['link', { rel: 'alternate icon', href: '/favicon.ico', type: 'image/png', sizes: '16x16' }],
+    ['meta', { name: 'author', content: 'Anthony Fu' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { name: 'og:title', content: title }],
+    ['meta', { name: 'og:description', content: description }],
+    ['meta', { property: 'og:image', content: ogImage }],
+    ['meta', { name: 'twitter:title', content: title }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:image', content: ogImage }],
+    ['meta', { name: 'twitter:site', content: '@antfu7' }],
+    ['meta', { name: 'twitter:url', content: ogUrl }],
   ],
   lastUpdated: true,
   cleanUrls: true,
@@ -142,5 +158,9 @@ export default defineConfig({
       { icon: 'github', link: 'https://github.com/unocss/unocss' },
       { icon: 'discord', link: 'http://chat.antfu.me' },
     ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2021-PRESENT Anthony Fu',
+    },
   },
 })
