9977dbeb27edb87b2295be35baec1b96ad1ee2b0
userquin <userquin@gmail.com>
2023-03-26 14:27:40 +0200

    chore: cleanup

--- docs/.vitepress/theme/features.ts
+++
@@ -1,6 +0,0 @@
-// @unocss-include
-import type { Feature } from './types'
-
-export const features: Feature[] = [
-  { icon: 'i', darkIcon: '', title: '', details: '', link: '', linkText: '' },
-]
--- docs/.vitepress/theme/types.ts
+++
@@ -1,8 +0,0 @@
-export interface Feature {
-  icon: string
-  darkIcon: string
-  title: string
-  details: string
-  link?: string
-  linkText?: string
-}
