514d57f99a1aa2c364ba588688be648a70045106
Chris <1633711653@qq.com>
2023-04-04 16:59:22 +0800

    fix(preset-mini): align with line-clamp

--- packages/preset-wind/src/rules/line-clamp.ts
+++ packages/preset-wind/src/rules/line-clamp.ts
@@ -7,11 +7,12 @@ export const lineClamps: Rule[] = [
     'display': '-webkit-box',
     '-webkit-box-orient': 'vertical',
     '-webkit-line-clamp': v,
-    'line-clamp': v,
   }), { autocomplete: ['line-clamp', 'line-clamp-<num>'] }],
 
   ...['none', ...globalKeywords].map(keyword => [`line-clamp-${keyword}`, {
+    'overflow': 'visible',
+    'display': 'block',
+    '-webkit-box-orient': 'horizontal',
     '-webkit-line-clamp': keyword,
-    'line-clamp': keyword,
   }] as Rule),
 ]
