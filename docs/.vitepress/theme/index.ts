e4722dd80e12c1026a573b303dd463471d819fff
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-07 22:09:40 +0200

    docs: disable gradient on Safari

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -15,7 +15,15 @@ export default {
       'home-features-after': () => h(HomePage),
     })
   },
-  enhanceApp(/* { app, router, siteData } */) {
-    // ...
-  },
+}
+
+if (typeof window !== 'undefined') {
+  // detect browser, add to class for conditional styling
+  const browser = navigator.userAgent.toLowerCase()
+  if (browser.includes('chrome'))
+    document.documentElement.classList.add('browser-chrome')
+  else if (browser.includes('firefox'))
+    document.documentElement.classList.add('browser-firefox')
+  else if (browser.includes('safari'))
+    document.documentElement.classList.add('browser-safari')
 }
