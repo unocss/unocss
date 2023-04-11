6d36da81a028f3d98e4221487b5215270cfa28cf
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-04-11 15:56:05 +0800

    refactor(preset-rem-to-px): remove redundant code (#2489)

--- packages/preset-rem-to-px/src/index.ts
+++ packages/preset-rem-to-px/src/index.ts
@@ -20,7 +20,7 @@ export default function remToPxPreset(options: RemToPxOptions = {}): Preset {
     postprocess: (util) => {
       util.entries.forEach((i) => {
         const value = i[1]
-        if (value && typeof value === 'string' && remRE.test(value))
+        if (typeof value === 'string' && remRE.test(value))
           i[1] = value.replace(remRE, (_, p1) => `${p1 * baseFontSize}px`)
       })
     },
