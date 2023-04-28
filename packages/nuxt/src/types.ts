a73a49d62b8ecef3c0ca9dd857e2ed19c6a39f0c
Oumar Barry <oumar.barry1202@gmail.com>
2023-04-28 22:25:36 +0000

    feat(nuxt): add vite mode config

--- packages/nuxt/src/types.ts
+++ packages/nuxt/src/types.ts
@@ -9,6 +9,19 @@ import type { PresetWindOptions } from '@unocss/preset-wind'
 
 export interface UnocssNuxtOptions extends UserConfig {
   /**
+   * CSS Generation mode
+   *
+   * - `global` - generate a single CSS sheet for entire App
+   * - `dist-chunk` - generate a CSS sheet for each code chunk on build, great for MPA
+   * - `per-module` - generate a CSS sheet for each module, can be scoped
+   * - `vue-scoped` - inject generated CSS to Vue SFC's `<style scoped>` for isolation
+   * - `shadow-dom` - inject generated CSS to `Shadow DOM` css style block for each web component
+   *
+   * @default 'global'
+   */
+  mode?: 'global' | 'per-module' | 'vue-scoped' | 'svelte-scoped' | 'dist-chunk' | 'shadow-dom'
+
+  /**
    * Injecting `uno.css` entry
    *
    * @default true
