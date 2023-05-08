2e0cf279b5f57bdcab89a992666d9c520a0d44a2
Techassi <git@techassi.dev>
2023-05-08 17:42:25 +0200

    Add custom compile class name test

--- test/transformer-compile-class.test.ts
+++ test/transformer-compile-class.test.ts
@@ -34,22 +34,26 @@ describe('transformer-compile-class', () => {
 </div>
     `.trim())
     expect(result.code.trim()).toMatchInlineSnapshot(`
-      "<div class=\\"uno-pe1esh\\">
-      <div class=\\"foo bar\\">
+      "<div class=\\"uno-: bg-red-500 text-xl font-bold border border-gray-200 dark:hover:bg-green-500 transform scale-5\\">
+      <div class=\\":uno: foo bar\\">
 
-      <div class=\\"uno-cbgd7b foo\\">
-        <div class=\\"uno-s9yxer\\"/>
+      <div class=\\":uno: text-center sm:text-left foo\\">
+        <div class=\\":uno\\"/>
       </div>"
     `)
     expect(result.css).toMatchInlineSnapshot(`
-      "/* layer: shortcuts */
-      .uno-pe1esh{--un-scale-x:0.05;--un-scale-y:0.05;transform:translateX(var(--un-translate-x)) translateY(var(--un-translate-y)) translateZ(var(--un-translate-z)) rotate(var(--un-rotate)) rotateX(var(--un-rotate-x)) rotateY(var(--un-rotate-y)) rotateZ(var(--un-rotate-z)) skewX(var(--un-skew-x)) skewY(var(--un-skew-y)) scaleX(var(--un-scale-x)) scaleY(var(--un-scale-y)) scaleZ(var(--un-scale-z));border-width:1px;--un-border-opacity:1;border-color:rgba(229,231,235,var(--un-border-opacity));--un-bg-opacity:1;background-color:rgba(239,68,68,var(--un-bg-opacity));font-size:1.25rem;line-height:1.75rem;font-weight:700;}
-      .dark .uno-pe1esh:hover{--un-bg-opacity:1;background-color:rgba(34,197,94,var(--un-bg-opacity));}
-      .uno-cbgd7b{text-align:center;}
-      .uno-s9yxer{font-size:0.875rem;line-height:1.25rem;font-weight:700;}
-      .uno-s9yxer:hover{--un-text-opacity:1;color:rgba(248,113,113,var(--un-text-opacity));}
+      "/* layer: default */
+      .scale-5{--un-scale-x:0.05;--un-scale-y:0.05;transform:translateX(var(--un-translate-x)) translateY(var(--un-translate-y)) translateZ(var(--un-translate-z)) rotate(var(--un-rotate)) rotateX(var(--un-rotate-x)) rotateY(var(--un-rotate-y)) rotateZ(var(--un-rotate-z)) skewX(var(--un-skew-x)) skewY(var(--un-skew-y)) scaleX(var(--un-scale-x)) scaleY(var(--un-scale-y)) scaleZ(var(--un-scale-z));}
+      .transform{transform:translateX(var(--un-translate-x)) translateY(var(--un-translate-y)) translateZ(var(--un-translate-z)) rotate(var(--un-rotate)) rotateX(var(--un-rotate-x)) rotateY(var(--un-rotate-y)) rotateZ(var(--un-rotate-z)) skewX(var(--un-skew-x)) skewY(var(--un-skew-y)) scaleX(var(--un-scale-x)) scaleY(var(--un-scale-y)) scaleZ(var(--un-scale-z));}
+      .border{border-width:1px;}
+      .border-gray-200{--un-border-opacity:1;border-color:rgba(229,231,235,var(--un-border-opacity));}
+      .bg-red-500{--un-bg-opacity:1;background-color:rgba(239,68,68,var(--un-bg-opacity));}
+      .dark .dark\\\\:hover\\\\:bg-green-500:hover{--un-bg-opacity:1;background-color:rgba(34,197,94,var(--un-bg-opacity));}
+      .text-center{text-align:center;}
+      .text-xl{font-size:1.25rem;line-height:1.75rem;}
+      .font-bold{font-weight:700;}
       @media (min-width: 640px){
-      .uno-cbgd7b{text-align:left;}
+      .sm\\\\:text-left{text-align:left;}
       }"
     `)
   })
@@ -60,5 +64,19 @@ describe('transformer-compile-class', () => {
 
     expect(order1.css).toBe(order2.css)
     expect(order1.code).toBe(order2.code)
+  })
+
+  test('custom compile class name', async () => {
+    const result = await transform(`
+<div class=":uno-foo: bg-red-500 text-xl">`.trim())
+
+    expect(result.code.trim()).toMatchInlineSnapshot(`
+    "<div class=\\"uno-foo\\">"
+    `)
+
+    expect(result.css).toMatchInlineSnapshot(`
+      "/* layer: shortcuts */
+      .uno-foo{--un-bg-opacity:1;background-color:rgba(239,68,68,var(--un-bg-opacity));font-size:1.25rem;line-height:1.75rem;}"
+    `)
   })
 })
