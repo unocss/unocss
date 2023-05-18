2b2096fcc869d1002b29de2f47b7e93e9be87e9c
jacob-8 <jwrunner7@gmail.com>
2023-05-18 09:54:06 +0800

    make slash error code easier to read

--- packages/svelte-scoped/src/vite/getReset.ts
+++ packages/svelte-scoped/src/vite/getReset.ts
@@ -10,17 +10,20 @@ export function getReset(injectReset: string): string {
   if (injectReset.startsWith('@unocss/reset'))
     return readFileSync(resolve(_dirname, `../node_modules/${injectReset}`), 'utf-8')
 
-  if (injectReset.startsWith('.') || injectReset.startsWith('/')) {
+  if (injectReset.startsWith('.')) {
     const resolved = resolve(process.cwd(), injectReset)
     if (!isFile(resolved))
-      throw new Error(`${injectReset} given as your injectReset value is not a valid file path relative to the root of your project, where your vite config file sits. To give an example, if you placed a reset.css in your src directory, "./src/reset.css" would work.`)
+      throw new Error(`"${injectReset}" given as your injectReset value is not a valid file path relative to the root of your project, where your vite config file sits. To give an example, if you placed a reset.css in your src directory, "./src/reset.css" would work.`)
 
     return readFileSync(resolved, 'utf-8')
   }
 
+  if (injectReset.startsWith('/'))
+    throw new Error(`Your injectReset value: "${injectReset}" is not a valid file path. To give an example, if you placed a reset.css in your src directory, "./src/reset.css" would work.`)
+
   const resolvedFromNodeModules = resolve(process.cwd(), 'node_modules', injectReset)
   if (!isFile(resolvedFromNodeModules))
-    throw new Error(`${injectReset} given as your injectReset value is not a valid file path relative to your project's node_modules folder. Can you confirm that you've installed ${injectReset}?`)
+    throw new Error(`"${injectReset}" given as your injectReset value is not a valid file path relative to your project's node_modules folder. Can you confirm that you've installed "${injectReset}"?`)
 
   return readFileSync(resolvedFromNodeModules, 'utf-8')
 }
