153d158e1caa8527f9d18dd2be195c95bfabc3fa
Jacob Bowdoin <7559478+jacob-8@users.noreply.github.com>
2023-05-22 20:30:31 +0800

    fix(svelte-scoped): @unocss/reset module resolution (#2644)

--- examples/sveltekit-preprocess/vite.config.ts
+++ examples/sveltekit-preprocess/vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import UnoCSS from '@unocss/svelte-scoped/vite'
 
 export default defineConfig({
