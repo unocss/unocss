384879e4713331c38e364b33ac272c0650794018
Jacob Bowdoin <7559478+jacob-8@users.noreply.github.com>
2023-05-31 22:05:41 +0800

    feat(svelte-scoped): Add theme() to transform style block pipeline (#2689)

---
+++ packages/svelte-scoped/test/cases/theme/OutputDev.svelte
@@ -0,0 +1,12 @@
+<div class="_mr-1_gvdwv7" />
+
+<style>
+  :global(._mr-1_gvdwv7) {
+    margin-right: 0.25rem;
+  }
+  div {
+    margin-bottom: 0.25rem;
+    background: #3b82f6;
+    margin-right: 0.875rem;
+  }
+</style>
