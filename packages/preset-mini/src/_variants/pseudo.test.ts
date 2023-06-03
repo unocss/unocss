a71ff6d3843a520d3bc12653d5aa83303c35bc9b
Anthony Fu <anthonyfu117@hotmail.com>
2023-06-03 14:23:19 +0200

    chore: update

--- packages/preset-mini/src/_variants/pseudo.test.ts
+++ packages/preset-mini/src/_variants/pseudo.test.ts
@@ -2,6 +2,7 @@ import { expect, test } from 'vitest'
 import { createGenerator } from '@unocss/core'
 import { variantPseudoClassesAndElements } from './pseudo'
 
+// https://github.com/unocss/unocss/issues/2713
 test('pseudo variant order', async () => {
   const uno = createGenerator({
     variants: [
@@ -10,16 +11,17 @@ test('pseudo variant order', async () => {
     rules: [
       [/^foo-(\d)$/, ([_, a]) => ({ text: `foo-${a}` })],
     ],
-    mergeSelectors: false,
   })
 
-  const result = await uno.generate([
+  const css = await uno.generate([
     'foo-1',
     'hover:foo-2',
     'disabled:foo-3',
-  ])
+  ]).then(r => r.css)
 
-  expect(result.css)
+  expect(css.indexOf('foo-1')).toBeLessThan(css.indexOf('foo-2'))
+  expect(css.indexOf('foo-2')).toBeLessThan(css.indexOf('foo-3'))
+  expect(css)
     .toMatchInlineSnapshot(`
       "/* layer: default */
       .foo-1{text:foo-1;}
