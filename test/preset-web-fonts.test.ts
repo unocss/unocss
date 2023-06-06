dc97a3a0a3ac2c66149cf586e5d173254d2bcfb4
Chris <hizyyv@gmail.com>
2023-06-06 17:58:00 +0800

    chore: add test case

--- test/preset-web-fonts.test.ts
+++ test/preset-web-fonts.test.ts
@@ -61,3 +61,55 @@ test('web-fonts (inline: true)', async () => {
   const { css } = await uno.generate(classes)
   expect(css).toContain('@font-face')
 })
+
+test('web-fonts weigth sort', async () => {
+  const uno = createGenerator({
+    presets: [
+      presetMini(),
+      presetWebFonts({
+        provider: 'google',
+        fonts: {
+          mono: 'Fira Mono:1000,200',
+          lato: [
+            {
+              name: 'Lato',
+              weights: ['1000', '200'],
+              italic: true,
+            },
+          ],
+        },
+        inlineImports: false,
+      }),
+    ],
+  })
+
+  const { css } = await uno.generate(classes)
+  const importUrl = css.match(/@import url\('(.*)'\)/)![1]
+  expect(importUrl).toMatchInlineSnapshot('"https://fonts.googleapis.com/css2?family=Fira+Mono:wght@200;1000&family=Lato:ital,wght@0,200;0,1000;1,200;1,1000&display=swap"')
+})
+
+test('web-fonts weigth deduplicate', async () => {
+  const uno = createGenerator({
+    presets: [
+      presetMini(),
+      presetWebFonts({
+        provider: 'google',
+        fonts: {
+          mono: 'Fira Mono:200,1000,1000',
+          lato: [
+            {
+              name: 'Lato',
+              weights: ['1000', '200', '1000', '400'],
+              italic: true,
+            },
+          ],
+        },
+        inlineImports: false,
+      }),
+    ],
+  })
+
+  const { css } = await uno.generate(classes)
+  const importUrl = css.match(/@import url\('(.*)'\)/)![1]
+  expect(importUrl).toMatchInlineSnapshot('"https://fonts.googleapis.com/css2?family=Fira+Mono:wght@200;1000&family=Lato:ital,wght@0,200;0,400;0,1000;1,200;1,400;1,1000&display=swap"')
+})
