818e4a34a3aaae8e18ba1c9f7f645b9be73c10bc
jacob-8 <jwrunner7@gmail.com>
2023-06-07 08:01:26 +0800

    chore: remove redundant presetWind usage

--- examples/next/uno.config.ts
+++ examples/next/uno.config.ts
@@ -1,12 +1,14 @@
-// uno.config.ts
 import {
   defineConfig,
   presetAttributify,
   presetIcons,
   presetUno,
-  presetWind,
 } from 'unocss'
 
 export default defineConfig({
-  presets: [presetUno(), presetWind(), presetAttributify(), presetIcons()],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+  ],
 })
