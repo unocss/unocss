3aba14e08ca2817edb8ec079b673a24f7e8ea4fa
Arunanshu Biswas <mydellpc07@gmail.com>
2023-06-15 18:17:17 +0530

    fix(bunny): create import url according to new url format

--- packages/preset-web-fonts/src/providers/bunny.ts
+++ packages/preset-web-fonts/src/providers/bunny.ts
@@ -1,3 +1,26 @@
-import { createGoogleCompatibleProvider } from './google'
+import type { Provider, WebFontsProviders } from '../types'
 
-export const BunnyFontsProvider = createGoogleCompatibleProvider('bunny', 'https://fonts.bunny.net')
+export function createBunnyFontsProvider(
+  name: WebFontsProviders,
+  host: string,
+): Provider {
+  return {
+    name,
+    getImportUrl(fonts): string {
+      const fontFamilies = fonts.map((font) => {
+        const { name, weights = [400], italic } = font
+        const formattedName = name.toLowerCase().replace(/\s/g, '-')
+        const weightString = weights ? weights.join(',') : ''
+        const italicSuffix = italic ? 'i' : ''
+        return `${formattedName}:${weightString}${italicSuffix}`
+      })
+
+      return `${host}/css?family=${fontFamilies.join('|')}`
+    },
+  }
+}
+
+export const BunnyFontsProvider: Provider = createBunnyFontsProvider(
+  'bunny',
+  'https://fonts.bunny.net',
+)
