6a81c62a7603606b117cdc9c02447b8ec1bac4fc
Lucas Vargas <lvargasnoronha@gmail.com>
2023-06-23 09:09:43 -0300

    fix(vscode): color preview with !important (#2787)

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,6 +1,7 @@
 import { mergeDeep } from '@unocss/core'
 import { getComponent } from '@unocss/preset-mini/utils'
 import { expect, it } from 'vitest'
+import { getColorString } from '@unocss/vscode/utils'
 
 it('mergeDeep', () => {
   expect(mergeDeep<any>({
@@ -57,4 +58,40 @@ it('getComponents', () => {
   expect(fn1('comma,separated,once')).eql(['comma', 'separated,once'])
   expect(fn1('comma(),separated(value)')).eql(['comma()', 'separated(value)'])
   expect(fn1('not(comma,separated)')).eql(['not(comma,separated)', ''])
+})
+
+it('getColorString', () => {
+  const textAmber = `
+  /* layer: default */
+  .text-amber {
+    --un-text-opacity: 1;
+    color: rgba(251, 191, 36, var(--un-text-opacity));
+  }`
+
+  const textAmberImportant = `
+  /* layer: default */
+  .\!text-amber {
+    --un-text-opacity: 1 !important;
+    color: rgba(251, 191, 36, var(--un-text-opacity)) !important;
+  }`
+
+  const bgAmber = `
+  /* layer: default */
+  .bg-amber {
+    --un-bg-opacity: 1;
+    background-color: rgba(251, 191, 36, var(--un-bg-opacity));
+  }`
+
+  const bgAmberImportant = `
+  /* layer: default */
+  .\!bg-amber {
+    --un-bg-opacity: 1 !important;
+    background-color: rgba(251, 191, 36, var(--un-bg-opacity)) !important;
+  }
+  `
+
+  expect(getColorString(textAmber)).eql('rgba(251, 191, 36, 1)')
+  expect(getColorString(textAmberImportant)).eql('rgba(251, 191, 36, 1)')
+  expect(getColorString(bgAmber)).eql('rgba(251, 191, 36, 1)')
+  expect(getColorString(bgAmberImportant)).eql('rgba(251, 191, 36, 1)')
 })
