072b804cfc951bad729ff46f5a2567136f4f2386
tzyito <568063539@qq.com>
2023-07-10 10:03:37 +0800

    fix(eslint-plugin):add sort-rules test case

--- test/sort-rules.test.ts
+++ test/sort-rules.test.ts
@@ -8,6 +8,13 @@ describe('sort rules', () => {
     presets: [
       presetUno(),
     ],
+    shortcuts: [
+      {
+        'flex-center': 'items-center',
+        'disblock': 'block',
+        'bg-my-white': 'bg-white',
+      }
+    ]
   })
 
   async function sort(tokens: string) {
@@ -18,19 +25,24 @@ describe('sort rules', () => {
     expect(await sort('pt-2 p-4 foo'))
       .toMatchInlineSnapshot('"foo p-4 pt-2"')
     expect(await sort('hover:focus:p1 hover:mt1 hover:m2 pt-2 p-4'))
-      .toMatchInlineSnapshot('"p-4 pt-2 hover:m2 hover:mt1 hover:focus:p1"')
+      .toMatchInlineSnapshot('"hover:focus:p1 hover:m2 hover:mt1 p-4 pt-2"')
     expect(await sort('hover:opacity-75 opacity-50 hover:scale-150 scale-125'))
-      .toMatchInlineSnapshot('"scale-125 opacity-50 hover:scale-150 hover:opacity-75"')
+      .toMatchInlineSnapshot('"hover:opacity-75 hover:scale-150 opacity-50 scale-125"')
     expect(await sort('lg:grid-cols-4 grid sm:grid-cols-3 grid-cols-2'))
       .toMatchInlineSnapshot('"grid grid-cols-2 lg:grid-cols-4 sm:grid-cols-3"')
     expect(await sort('items-center py1 bg-gray:20 pl3 pr2 rounded-full text-xs op50 hover:op100'))
-      .toMatchInlineSnapshot('"items-center rounded-full bg-gray:20 py1 pl3 pr2 text-xs op50 hover:op100"')
+      .toMatchInlineSnapshot('"bg-gray:20 hover:op100 items-center op50 pl3 pr2 py1 rounded-full text-xs"')
   })
 
   test('variant group', async () => {
     expect(await sort('hover:pt-2 hover:p-4 foo'))
       .toMatchInlineSnapshot('"foo hover:p-4 hover:pt-2"')
     expect(await sort('hover:(pt-2 p-4) hover:text-red hover:focus:(m1 mx2) foo'))
-      .toMatchInlineSnapshot('"foo hover:(p-4 pt-2 text-red) hover:focus:(m1 mx2)"')
+      .toMatchInlineSnapshot('"foo hover:focus:(m1 mx2) hover:(p-4 pt-2 text-red)"')
+  })
+
+  test('shortcuts variant', async () => {
+    expect(await sort('flex-center disblock bg-my-white items-center'))
+      .toMatchInlineSnapshot('"bg-my-white disblock flex-center items-center"')
   })
 })
