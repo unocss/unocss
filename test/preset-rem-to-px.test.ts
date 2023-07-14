a3826c3989e8a13e2f75c5755e68a65021bb78c0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-15 00:01:56 +0200

    fix: remove grid-gap from preset-rem-to-px.test.ts

--- test/preset-rem-to-px.test.ts
+++ test/preset-rem-to-px.test.ts
@@ -20,7 +20,7 @@ describe('rem-to-px', () => {
         .-p2{padding:-8px;}
         .m4{margin:16px;}
         .mx2{margin-left:8px;margin-right:8px;}
-        .gap2{grid-gap:8px;gap:8px;}"
+        .gap2{gap:8px;}"
       `)
   })
 
@@ -33,7 +33,7 @@ describe('rem-to-px', () => {
         .\\\\!-p2{padding:-8px !important;}
         .\\\\!m4{margin:16px !important;}
         .\\\\!mx2{margin-left:8px !important;margin-right:8px !important;}
-        .\\\\!gap2{grid-gap:8px !important;gap:8px !important;}"
+        .\\\\!gap2{gap:8px !important;}"
       `)
   })
 })
