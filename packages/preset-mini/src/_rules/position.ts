b6da0f16469aab0611da7752b41bbee26ae20081
chizukicn <chizukicn@outlook.com>
2023-07-23 01:22:12 +0800

    chore: revert others change.

--- packages/preset-mini/src/_rules/position.ts
+++ packages/preset-mini/src/_rules/position.ts
@@ -34,8 +34,6 @@ export const justifies: StaticRule[] = [
   ['justify-around', { 'justify-content': 'space-around' }],
   ['justify-evenly', { 'justify-content': 'space-evenly' }],
   ['justify-stretch', { 'justify-content': 'stretch' }],
-  ['justify-left', { 'justify-content': 'left' }],
-  ['justify-right', { 'justify-content': 'right' }],
   ...makeGlobalStaticRules('justify', 'justify-content'),
 
   // items
