e0982104319178b44c8a40a9f0add1d4a16eb523
Nir Lahad <nirlah@gmail.com>
2023-09-10 14:01:19 +0300

    fix(preset-mini): remove `font-smoothing` from antialiased rules (#3072)
    Co-authored-by: Chris <1633711653@qq.com>

--- packages/preset-mini/src/_rules/static.ts
+++ packages/preset-mini/src/_rules/static.ts
@@ -131,11 +131,9 @@ export const fontSmoothings: Rule[] = [
   ['antialiased', {
     '-webkit-font-smoothing': 'antialiased',
     '-moz-osx-font-smoothing': 'grayscale',
-    'font-smoothing': 'grayscale',
   }],
   ['subpixel-antialiased', {
     '-webkit-font-smoothing': 'auto',
     '-moz-osx-font-smoothing': 'auto',
-    'font-smoothing': 'auto',
   }],
 ]
