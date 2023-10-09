fe617a3a4d749390c419d35492dedd8a7c2f3ecf
Dunqing <dengqing0821@gmail.com>
2023-10-09 12:11:11 +0800

    feat: remove dead code

--- packages/postcss/src/theme.ts
+++ packages/postcss/src/theme.ts
@@ -2,9 +2,6 @@ import type { UnoGenerator } from '@unocss/core'
 import { transformThemeFn } from '@unocss/rule-utils'
 import type { Root } from 'postcss'
 
-export function themeFnRE(directiveName: string) {
-  return new RegExp(`${directiveName}\\((.*?)\\)`, 'g')
-}
 export async function parseTheme(root: Root, uno: UnoGenerator) {
   root.walkDecls((decl) => {
     decl.value = transformThemeFn(decl.value, uno.config.theme)
