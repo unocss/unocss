ee38f041354256143e2707f6bd4aa8b2dc08b2d9
Saya <chu121su12@gmail.com>
2023-10-15 21:30:06 +0800

    lint

--- packages/preset-uno/src/shortcuts.ts
+++ packages/preset-uno/src/shortcuts.ts
@@ -2,5 +2,5 @@ import type { Shortcut } from '@unocss/core'
 import type { Theme } from '@unocss/preset-mini'
 
 export const mediaHover: Shortcut<Theme>[] = [
-    [/^(group|peer|parent|previous)-mouse-hover\b(.+)$/, ([, tag, rest]) => `${tag}-hover:media-mouse${rest}`],
+  [/^(group|peer|parent|previous)-mouse-hover\b(.+)$/, ([, tag, rest]) => `${tag}-hover:media-mouse${rest}`],
 ]
