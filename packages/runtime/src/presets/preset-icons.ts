92e0514bebe6bb3fd2ff638a6513a1f6d808b2a4
Saya <chu121su12@gmail.com>
2023-10-19 16:22:55 +0800

    force type

--- packages/runtime/src/presets/preset-icons.ts
+++ packages/runtime/src/presets/preset-icons.ts
@@ -2,8 +2,9 @@ import type { IconsOptions } from '@unocss/preset-icons'
 import { createPresetIcons } from '@unocss/preset-icons'
 import { loadIcon } from '@iconify/utils'
 import { createCDNFetchLoader } from '../../../preset-icons/src/core'
+import type { RuntimeContext } from '..'
 
-window.__unocss_runtime = window.__unocss_runtime ?? { presets: {} }
+window.__unocss_runtime = window.__unocss_runtime ?? {} as RuntimeContext
 window.__unocss_runtime.presets = Object.assign(window.__unocss_runtime?.presets ?? {}, (() => {
   const presetIcons = createPresetIcons(async (options) => {
     const { cdn } = options
