66914e5c0741bded4b9969996fc9802956f7dc9d
Saya <chu121su12@gmail.com>
2023-11-29 23:22:48 +0800

    chore: update missing jsdoc

--- packages/preset-mini/src/_utils/utilities.ts
+++ packages/preset-mini/src/_utils/utilities.ts
@@ -175,6 +175,7 @@ export function parseColor(body: string, theme: Theme, key?: ThemeColorKeys): Pa
  *
  * @param property - Property for the css value to be created.
  * @param varName - Base name for the opacity variable.
+ * @param [key] - Theme key to select the color from.
  * @param [shouldPass] - Function to decide whether to pass the css.
  * @return object.
  */
