e4d262091268d94279a8f849e862bc0bea25cd56
kwaa <50108258+kwaa@users.noreply.github.com>
2023-11-30 22:07:56 +0800

    fix(postbuild): remove runtime patch

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -10,24 +10,6 @@ function patchCjs(cjsModulePath: string, name: string) {
   )
 }
 
-function patchTsupCjs(cjsModuleName: string, name: string) {
-  let file = resolve(`${cjsModuleName}.cjs`)
-  let content = readFileSync(file, 'utf-8')
-  writeFileSync(
-    file,
-    content.replace(`module.exports = __`, `exports.default = __`),
-    { encoding: 'utf-8' },
-  )
-  file = resolve(`${cjsModuleName}.d.cts`)
-  content = readFileSync(file, 'utf-8').replace(`, ${name} as default `, ' ')
-  content += `\nexport = ${name};`
-  writeFileSync(
-    file,
-    content,
-    { encoding: 'utf-8' },
-  )
-}
-
 // @unocss/eslint-config
 patchCjs(resolve('./packages/eslint-config/dist/flat.cjs'), 'flat')
 patchCjs(resolve('./packages/eslint-config/dist/index.cjs'), 'index')
@@ -37,9 +19,6 @@ patchCjs(resolve('./packages/eslint-plugin/dist/index.cjs'), 'index')
 
 // @unocss/postcss
 patchCjs(resolve('./packages/postcss/dist/index.cjs'), 'unocss')
-
-// @unocss/runtime
-patchTsupCjs('./packages/runtime/dist/index', 'init')
 
 // unocss
 patchCjs(resolve('./packages/unocss/dist/postcss.cjs'), 'postcss__default')
