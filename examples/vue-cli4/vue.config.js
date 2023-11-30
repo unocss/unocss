8b3bdeaafc538debd8ab829ded9f42783ebdceaf
userquin <userquin@gmail.com>
2023-11-30 15:37:29 +0100

    chore: test vue cli with dynamic import

--- examples/vue-cli4/vue.config.js
+++ examples/vue-cli4/vue.config.js
@@ -1,4 +1,34 @@
 const process = require('node:process')
+
+module.exports = function () {
+  return import('@unocss/webpack').then((m) => {
+    const UnoCSS = m.default
+    return {
+      configureWebpack: {
+        plugins: [
+          UnoCSS({}),
+        ],
+      },
+      chainWebpack(config) {
+        config.module.rule('vue').uses.delete('cache-loader')
+        config.module.rule('tsx').uses.delete('cache-loader')
+        config.merge({
+          cache: false,
+        })
+      },
+      css: {
+        extract: process.env.NODE_ENV === 'development'
+          ? {
+              filename: '[name].css',
+            }
+          : {
+              filename: '[name].[hash:9].css',
+            },
+      },
+    }
+  })
+}
+/*
 const UnoCSS = require('@unocss/webpack').default
 
 module.exports = {
@@ -24,3 +54,4 @@ module.exports = {
         },
   },
 }
+*/
--- examples/vue-cli5/vue.config.js
+++ examples/vue-cli5/vue.config.js
@@ -1,4 +1,37 @@
 const process = require('node:process')
+
+module.exports = function () {
+  return import('@unocss/webpack').then((m) => {
+    const UnoCSS = m.default
+    return {
+      configureWebpack: {
+        devtool: 'inline-source-map',
+        plugins: [
+          UnoCSS(),
+        ],
+        optimization: {
+          realContentHash: true,
+        },
+      },
+      chainWebpack(config) {
+        config.module.rule('vue').uses.delete('cache-loader')
+        config.module.rule('tsx').uses.delete('cache-loader')
+        config.merge({
+          cache: false,
+        })
+      },
+      css: {
+        extract: process.env.NODE_ENV === 'development'
+          ? {
+              filename: 'css/[name].css',
+              chunkFilename: 'css/[name].css',
+            }
+          : true,
+      },
+    }
+  })
+}
+/*
 const UnoCSS = require('@unocss/webpack').default
 
 module.exports = {
@@ -27,3 +60,4 @@ module.exports = {
       : true,
   },
 }
+*/
