ad0aa70d9a75fa6ce17dc021aa5c47bc3c1300c4
userquin <userquin@gmail.com>
2023-12-01 12:47:56 +0100

    chore: cleanup vue-cli examples

--- examples/vue-cli4/vue.config.js
+++ examples/vue-cli4/vue.config.js
@@ -28,30 +28,3 @@ module.exports = function () {
     }
   })
 }
-/*
-const UnoCSS = require('@unocss/webpack').default
-
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      UnoCSS({}),
-    ],
-  },
-  chainWebpack(config) {
-    config.module.rule('vue').uses.delete('cache-loader')
-    config.module.rule('tsx').uses.delete('cache-loader')
-    config.merge({
-      cache: false,
-    })
-  },
-  css: {
-    extract: process.env.NODE_ENV === 'development'
-      ? {
-          filename: '[name].css',
-        }
-      : {
-          filename: '[name].[hash:9].css',
-        },
-  },
-}
-*/
--- examples/vue-cli5/vue.config.js
+++ examples/vue-cli5/vue.config.js
@@ -31,33 +31,3 @@ module.exports = function () {
     }
   })
 }
-/*
-const UnoCSS = require('@unocss/webpack').default
-
-module.exports = {
-  configureWebpack: {
-    devtool: 'inline-source-map',
-    plugins: [
-      UnoCSS(),
-    ],
-    optimization: {
-      realContentHash: true,
-    },
-  },
-  chainWebpack(config) {
-    config.module.rule('vue').uses.delete('cache-loader')
-    config.module.rule('tsx').uses.delete('cache-loader')
-    config.merge({
-      cache: false,
-    })
-  },
-  css: {
-    extract: process.env.NODE_ENV === 'development'
-      ? {
-          filename: 'css/[name].css',
-          chunkFilename: 'css/[name].css',
-        }
-      : true,
-  },
-}
-*/
