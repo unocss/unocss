14908f683d73fc36247f5cc31d1407c70a6f42c4
Anthony Fu <anthonyfu117@hotmail.com>
2023-12-05 03:02:53 +0100

    chore: update dts

--- packages/inspector/client/auto-imports.d.ts
+++ packages/inspector/client/auto-imports.d.ts
@@ -288,4 +288,5 @@ declare global {
 declare global {
   // @ts-ignore
   export type { Component, ComponentPublicInstance, ComputedRef, ExtractDefaultPropTypes, ExtractPropTypes, ExtractPublicPropTypes, InjectionKey, PropType, Ref, VNode, WritableComputedRef } from 'vue'
+  import('vue')
 }
