5ea7fba563b2446350836c640d052c89c686e008
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-02 03:03:57 +0100

    chore: update types

--- packages/preset-legacy-compat/src/comma-color.ts
+++ packages/preset-legacy-compat/src/comma-color.ts
@@ -1,5 +1,5 @@
 export function toCommaStyleColorFunction(str: string) {
-  return str.replace(/(rgb|hsl)a?\(([^)]+)\)/g, (_, fn, v) => {
+  return str.replace(/(rgb|hsl)a?\(([^)]+)\)/g, (_, fn: string, v: string) => {
     const [rgb, alpha] = v.split(/\//g).map(i => i.trim())
     if (alpha && !fn.endsWith('a'))
       fn += 'a'
