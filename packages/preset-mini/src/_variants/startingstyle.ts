cee80b6c91fa2fb0422d847666bba74c17e7f904
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-03-15 17:06:29 +0100

    fix: align variant naming with tailwind (may change again)

--- packages/preset-mini/src/_variants/startingstyle.ts
+++ packages/preset-mini/src/_variants/startingstyle.ts
@@ -1,13 +1,13 @@
 import type { Variant } from '@unocss/core'
 
 export const variantStartingStyle: Variant = {
-  name: 'start',
+  name: 'starting',
   match(matcher) {
-    if (!matcher.startsWith('start:'))
+    if (!matcher.startsWith('starting:'))
       return
 
     return {
-      matcher: matcher.slice(6),
+      matcher: matcher.slice(9),
       handle: (input, next) => next({
         ...input,
         parent: `@starting-style`,
