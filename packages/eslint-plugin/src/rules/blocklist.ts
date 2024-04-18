09e3bea733878e6a6bc3f62a0b3d7d4edc5877b7
Anthony Fu <anthonyfu117@hotmail.com>
2024-04-18 13:07:29 +0200

    fix(eslint-plugin): compactible with eslint v9

--- packages/eslint-plugin/src/rules/blocklist.ts
+++ packages/eslint-plugin/src/rules/blocklist.ts
@@ -82,14 +82,15 @@ export default createRule({
       },
     }
 
+    const parserServices = context?.sourceCode.parserServices || context.parserServices
     // @ts-expect-error missing-types
-    if (context.parserServices == null || context.parserServices.defineTemplateBodyVisitor == null) {
+    if (parserServices == null || parserServices.defineTemplateBodyVisitor == null) {
       return scriptVisitor
     }
     else {
       // For Vue
       // @ts-expect-error missing-types
-      return context.parserServices?.defineTemplateBodyVisitor(templateBodyVisitor, scriptVisitor)
+      return parserServices?.defineTemplateBodyVisitor(templateBodyVisitor, scriptVisitor)
     }
   },
 }) as any as ESLintUtils.RuleWithMeta<[], ''>
