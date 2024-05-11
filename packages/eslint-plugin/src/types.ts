3eb98b56db6e525df5910f69ece15ef8e863316c
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-05-11 11:36:16 +0800

    chore: update

--- packages/eslint-plugin/src/types.ts
+++ packages/eslint-plugin/src/types.ts
@@ -1,11 +1,3 @@
-declare module 'eslint' {
-  interface SharedConfigurationSettings {
-    unocss?: {
-      configPath?: string
-    }
-  }
-}
-
 declare module '@typescript-eslint/utils/ts-eslint' {
   interface SharedConfigurationSettings {
     unocss?: {
