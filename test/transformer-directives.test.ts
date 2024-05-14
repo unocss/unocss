e18acf524c698667519f40eaf5bdcc7be7754811
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-05-14 11:02:13 +0800

    chore: update test

--- test/transformer-directives.test.ts
+++ test/transformer-directives.test.ts
@@ -234,11 +234,11 @@ describe('transformer-directives', () => {
           background-color: rgb(255 255 255 / var(--un-bg-opacity));
         }
         .btn:hover {
-          border-width: 1px;
-        }
-        .btn:hover {
           --un-bg-opacity: 1;
           background-color: rgb(59 130 246 / var(--un-bg-opacity));
+        }
+        .btn:hover {
+          border-width: 1px;
         }
         "
       `)
