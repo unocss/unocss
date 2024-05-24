5265670f3f79ff60d1c42cfc9c88a029700bf39e
Enkot <taras.batenkov@gmail.com>
2024-05-24 19:17:50 +0300

    feat: nuxt layers support

---
+++ examples/nuxt3-layers/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class=" text-center px-12 py-20 foo">
+    <span text="5xl hover:red blue  " cursor="default">Hello Nuxt 3</span>
+    <br>
+    <div i-carbon-car text-4xl inline-block />
+    <br>
+    <button btn>
+      Button
+    </button>
+  </main>
+</template>
+
+<style>
+@import '@unocss/reset/tailwind.css';
+</style>
