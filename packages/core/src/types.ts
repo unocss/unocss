4e10b3ae970894353128af2685a7a037a6fab1db
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-05-30 16:17:46 +0800

    feat(preset-mini): add originThemeColor option

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -71,6 +71,10 @@ export interface RuleContext<Theme extends object = object> {
    * Available only when `details` option is enabled.
    */
   variants?: Variant<Theme>[]
+  /**
+   * show origin theme color
+   */
+  showOriginThemeColor?: boolean
 }
 
 declare const SymbolShortcutsNoMerge: unique symbol
@@ -941,4 +945,9 @@ export interface GenerateOptions<T extends boolean> {
    * If return extended "matched" with payload and count
    */
   extendedInfo?: T
+
+  /**
+   * show origin theme color
+   */
+  showOriginThemeColor?: boolean
 }
