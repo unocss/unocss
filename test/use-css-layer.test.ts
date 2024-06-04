8d191116ba4c699370d0a6185c764d05666a581a
lzl0304 <develop0304@outlook.com>
2024-06-04 15:43:44 +0800

    docs: update the outputToCssLayers example

--- test/use-css-layer.test.ts
+++ test/use-css-layer.test.ts
@@ -1,5 +1,6 @@
 import { createGenerator } from '@unocss/core'
 import { describe, expect, it } from 'vitest'
+import presetUno from '@unocss/preset-uno'
 
 describe('use-css-layer', () => {
   it('static', async () => {
@@ -18,6 +19,26 @@ describe('use-css-layer', () => {
       outputToCssLayers: true,
     })
     const { css } = await uno.generate('a b abc abcd d4 c5', { preflights: false })
+    expect(css).toMatchSnapshot()
+  })
+
+  it('change layer name', async () => {
+    const uno = createGenerator({
+      presets: [presetUno()],
+      shortcuts: {
+        'custom-shortcut': 'text-lg text-orange hover:text-teal',
+      },
+      outputToCssLayers: {
+        cssLayerName: (layer: string) => {
+          if (layer === 'default')
+            return 'utilities'
+
+          if (layer === 'shortcuts')
+            return 'utilities.shortcuts'
+        },
+      },
+    })
+    const { css } = await uno.generate('w-1 h-1 custom-shortcut', { preflights: false })
     expect(css).toMatchSnapshot()
   })
 })
