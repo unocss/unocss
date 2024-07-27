5e3f6e8efbc30bdadf900a015e8a64c56ae91cf0
tzyito <568063539@qq.com>
2024-07-27 14:20:30 +0800

    fix(playground):  inject lz-string to parse url

--- playground/src/composables/reload.ts
+++ playground/src/composables/reload.ts
@@ -6,34 +6,44 @@ export function importMapsPlugin(): Plugin {
     name: 'vite-plugin-inject-import-maps',
     transformIndexHtml(html) {
       const customScript = `
-          const r = (packageName, version) => {
-            return \`https://esm.sh/\${packageName}@\${version}\`
-          }
-          const updateImportMap = (newImportsVersion) => {
-            const map = {
-              imports: {
-                '@unocss/autocomplete': r('@unocss/autocomplete', newImportsVersion),
-                '@unocss/core': r('@unocss/core', newImportsVersion),
-                '@unocss/preset-rem-to-px': r('@unocss/preset-rem-to-px', newImportsVersion),
-                '@unocss/extractor-arbitrary-variants': r('@unocss/extractor-arbitrary-variants', newImportsVersion),
-                '@unocss/transformer-attributify-jsx-babel': r('@unocss/transformer-attributify-jsx-babel', newImportsVersion),
-                'unocss': r('unocss', newImportsVersion),
-                '@unocss/vite': r('@unocss/vite', newImportsVersion),
-              }
-            }
-              const script = document.createElement('script')
-              script.setAttribute('type','importmap')
-              script.textContent = JSON.stringify(map, null, 2)
-              document.head.appendChild(script)
-          }
-          const version = new URL(window.location.href).searchParams.get('version') || '0.61.5'
-          if (version) {
-            updateImportMap(version)
-          }
+const r = (packageName, version) => {
+  if(version) {
+    return \`https://esm.run\${packageName}@\${version}\`
+  }
+  return \`https://esm.run\${packageName}\`
+}
+const updateImportMap = async (newImportsVersion) => {
+  const map = {
+    imports: {
+      '@unocss/autocomplete': r('@unocss/autocomplete', newImportsVersion),
+      '@unocss/core': r('@unocss/core', newImportsVersion),
+      '@unocss/preset-rem-to-px': r('@unocss/preset-rem-to-px', newImportsVersion),
+      '@unocss/extractor-arbitrary-variants': r('@unocss/extractor-arbitrary-variants', newImportsVersion),
+      '@unocss/transformer-attributify-jsx-babel': r('@unocss/transformer-attributify-jsx-babel', newImportsVersion),
+      'unocss': r('unocss', newImportsVersion),
+      '@unocss/vite': r('@unocss/vite', newImportsVersion),
+    }
+  }
+    const script = document.createElement('script')
+    script.setAttribute('type','importmap')
+    script.textContent = JSON.stringify(map, null, 2)
+    const head = document.head
+    head.insertBefore(script, head.firstChild)
+}
+const url = new URL(window.location.href).searchParams.get('version')
+const version = LZString.decompressFromEncodedURIComponent(url) || ''
+updateImportMap(version)
       `
       return {
         html,
         tags: [
+          {
+            tag: 'script',
+            attrs: {
+              src: 'https://cdn.jsdelivr.net/npm/lz-string',
+            },
+            injectTo: 'head-prepend',
+          },
           {
             tag: 'script',
             children: customScript,
