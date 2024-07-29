498e5ac13fa423944e3f2b1bd47d7dc02c5671d7
Anthony Fu <github@antfu.me>
2024-07-29 19:04:39 +0200

    chore: update

---
+++ test/meta-rules-order.test.ts
@@ -0,0 +1,18 @@
+import { expect, it } from 'vitest'
+import { rules as rulesMini } from '../packages/preset-mini/src/_rules/default'
+import { rules as rulesWind } from '../packages/preset-wind/src/rules/default'
+
+it('should rules in preset-wind should have same order as preset-mini', () => {
+  let diff = 0
+  for (const rule of rulesMini) {
+    const indexMini = rulesMini.indexOf(rule)
+    const indexWind = rulesWind.indexOf(rule)
+    expect(indexMini).toBeGreaterThanOrEqual(0)
+    expect(indexWind).toBeGreaterThanOrEqual(0)
+    expect(
+      indexMini + diff,
+      `rule ${rule[0]} is not in the same order as preset`,
+    ).toBeLessThanOrEqual(indexWind)
+    diff = indexWind - indexMini
+  }
+})
