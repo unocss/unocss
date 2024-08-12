bcac919cff7edc347b6c2649534a0ff3e06c53e8
Samuel Burkhard <15785987+samydoesit@users.noreply.github.com>
2024-08-12 11:13:09 +0200

    docs: fix nuxt-layers example (#4043)

--- examples/nuxt3-layers/nuxt.config.ts
+++ examples/nuxt3-layers/nuxt.config.ts
@@ -4,6 +4,8 @@ export default defineNuxtConfig({
     '@unocss/nuxt',
   ],
   unocss: {
+    attributify: true,
+    icons: true,
     nuxtLayers: true,
     shortcuts: [
       ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
