791779e9031a7d0ac949fb3cb086d8b934fa632d
Eric Nie <enie123@users.noreply.github.com>
2024-09-23 15:34:19 +0800

    Simplify logic in transformer-directives

--- packages/transformer-directives/src/transform.ts
+++ packages/transformer-directives/src/transform.ts
@@ -9,6 +9,12 @@ import { handleApply } from './apply'
 import { handleFunction } from './functions'
 import { handleScreen } from './screen'
 
+interface EmbedCssStylePosition {
+  startOffset: number
+  startLine: number
+  startColumn: number
+}
+
 export async function transformDirectives(
   code: MagicString,
   uno: UnoGenerator,
@@ -34,40 +40,71 @@ export async function transformDirectives(
   if (!hasApply && !hasThemeFn && !hasScreen)
     return
 
-  const ast = parse(parseCode, {
-    parseCustomProperty: true,
-    parseAtrulePrelude: false,
-    positions: true,
-    filename,
-    offset,
-  })
+  const handleTransformDirectives = async (cssCode: string, position?: EmbedCssStylePosition) => {
+    const ast = parse(cssCode, {
+      parseCustomProperty: true,
+      parseAtrulePrelude: false,
+      positions: true,
 
-  if (ast.type !== 'StyleSheet')
-    return
+      filename,
+      line: position?.startLine,
+      offset: position?.startOffset ?? offset,
+      column: position?.startColumn,
+    })
 
-  const stack: Promise<void>[] = []
+    if (ast.type !== 'StyleSheet')
+      return
 
-  const ctx: TransformerDirectivesContext = {
-    options,
-    applyVariable,
-    uno,
-    code,
-    filename,
-    offset,
+    const stack: Promise<void>[] = []
+    const ctx: TransformerDirectivesContext = {
+      options,
+      applyVariable,
+      uno,
+      code,
+      filename,
+      offset,
+    }
+
+    const processNode = async (node: CssNode, _item: ListItem<CssNode>, _list: List<CssNode>) => {
+      if (hasScreen && node.type === 'Atrule')
+        handleScreen(ctx, node)
+
+      if (node.type === 'Function')
+        handleFunction(ctx, node)
+
+      if (hasApply && node.type === 'Rule')
+        await handleApply(ctx, node)
+    }
+
+    walk(ast, (...args) => stack.push(processNode(...args)))
+
+    await Promise.all(stack)
   }
 
-  const processNode = async (node: CssNode, _item: ListItem<CssNode>, _list: List<CssNode>) => {
-    if (hasScreen && node.type === 'Atrule')
-      handleScreen(ctx, node)
-
-    if (node.type === 'Function')
-      handleFunction(ctx, node)
-
-    if (hasApply && node.type === 'Rule')
-      await handleApply(ctx, node)
+  // Two Cases:
+  // 1. Complete stylesheet (e.g. .css file)
+  // 2. Stylesheet embed in code as <style> </style>
+  const STYLE_CHECK_RE = /<style[^>]*>[\s\S]*?<\/style>/i
+  // Note: STYLE_RE is stateful with g flag
+  if (!STYLE_CHECK_RE.test(parseCode)) {
+    await handleTransformDirectives(parseCode)
   }
+  else {
+    const computePosition = (index: number): EmbedCssStylePosition => {
+      const prefix = parseCode.slice(0, index)
+      const lines = prefix.split('\n')
+      return {
+        startOffset: index,
+        startLine: lines.length,
+        startColumn: lines[lines.length - 1].length,
+      }
+    }
 
-  walk(ast, (...args) => stack.push(processNode(...args)))
-
-  await Promise.all(stack)
+    const STYLE_RE = /(<style[^>]*>)([\s\S]*?)<\/style>/gi
+    for (const match of Array.from(parseCode.matchAll(STYLE_RE))) {
+      const cssStartIndex = match.index + match[1].length
+      const position = computePosition(cssStartIndex)
+      await handleTransformDirectives(match[2], position)
+    }
+  }
 }
