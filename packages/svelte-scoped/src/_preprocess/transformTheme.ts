f2abb6ec89d09496c5ae20c27e76a3c6730b6a8f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-09-27 15:24:48 +0200

    feat: only replace theme when useThemeFn is true

--- packages/svelte-scoped/src/_preprocess/transformTheme.ts
+++ packages/svelte-scoped/src/_preprocess/transformTheme.ts
@@ -4,11 +4,16 @@ interface Theme { [key: string]: any }
 
 export const themeRE = /theme\((.+?)\)/g
 
-export function transformTheme(s: MagicString, theme: Theme): MagicString {
-  return s.replace(themeRE, (_, match) => {
-    const argumentsWithoutQuotes = match.slice(1, -1)
-    return getThemeValue(argumentsWithoutQuotes, theme)
-  })
+export function transformTheme(s: MagicString, theme: Theme, useThemeFn: boolean): MagicString {
+  if (useThemeFn) {
+    return s.replace(themeRE, (_, match) => {
+      const argumentsWithoutQuotes = match.slice(1, -1)
+      return getThemeValue(argumentsWithoutQuotes, theme)
+    })
+  }
+  if (!useThemeFn)
+    return s
+  return s
 }
 
 export function getThemeValue(rawArguments: string, theme: Theme): string {
