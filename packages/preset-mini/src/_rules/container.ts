cbb6e61274493d2c486046eecf2a6ae50c60efae
Anthony Fu <github@antfu.me>
2024-10-04 10:57:50 +0800

    feat(preset-mini): graduate some experimental features

--- packages/preset-mini/src/_rules/container.ts
+++ packages/preset-mini/src/_rules/container.ts
@@ -1,10 +1,7 @@
 import type { Rule } from '@unocss/core'
-import { warnOnce } from '@unocss/core'
 
 export const containerParent: Rule[] = [
   [/^@container(?:\/(\w+))?(?:-(normal))?$/, ([, l, v]) => {
-    warnOnce('The container query rule is experimental and may not follow semver.')
-
     return {
       'container-type': v ?? 'inline-size',
       'container-name': l,
