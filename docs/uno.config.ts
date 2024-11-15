aa1713fbf7716a9d8d4aa40b3ecf765e3f4bc195
Chris <1633711653@qq.com>
2024-11-15 13:34:22 +0800

    pref(preset-wind): add category to animates & describe animate docs (#4265)

--- docs/uno.config.ts
+++ docs/uno.config.ts
@@ -2,6 +2,25 @@
 import { defineConfig, presetAttributify, presetIcons, presetUno, transformerDirectives } from 'unocss'
 
 export default defineConfig({
+  theme: {
+    animation: {
+      keyframes: {
+        custom: '{0%, 100% { transform: scale(0.5); } 50% { transform: scale(1); }}',
+      },
+      durations: {
+        custom: '2s',
+      },
+      timingFns: {
+        custom: 'cubic-bezier(0.4,0,.6,1)',
+      },
+      properties: {
+        custom: { 'transform-origin': 'center' },
+      },
+      counts: {
+        custom: 'infinite',
+      },
+    },
+  },
   presets: [
     presetUno(),
     presetIcons(),
