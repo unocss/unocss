c2df1288d32c592a3a379be7a12ac38e72d720fe
Vin Yap <82420632+vineryap@users.noreply.github.com>
2024-11-29 21:03:34 +0900

    fix(eslint-plugin): improve class ordering for Svelte components (#4205)
    Co-authored-by: Anthony Fu <github@antfu.me>
    Co-authored-by: Chris <hizyyv@gmail.com>

--- packages/eslint-plugin/src/rules/order.ts
+++ packages/eslint-plugin/src/rules/order.ts
@@ -1,6 +1,8 @@
 import type { TSESTree } from '@typescript-eslint/types'
 import type { ESLintUtils } from '@typescript-eslint/utils'
 import type { RuleListener } from '@typescript-eslint/utils/ts-eslint'
+import type { SvelteAttribute, SvelteLiteral, SvelteMustacheTag } from 'svelte-eslint-parser/lib/ast/html'
+import { AST_TOKEN_TYPES } from '@typescript-eslint/types'
 import { AST_NODES_WITH_QUOTES, CLASS_FIELDS } from '../constants'
 import { createRule, syncAction } from './_'
 
@@ -19,24 +21,33 @@ export default createRule({
   },
   defaultOptions: [],
   create(context) {
-    function checkLiteral(node: TSESTree.Literal) {
+    function checkLiteral(node: TSESTree.Literal | SvelteLiteral, addSpace?: 'before' | 'after' | undefined) {
       if (typeof node.value !== 'string' || !node.value.trim())
         return
       const input = node.value
-      const sorted = syncAction(
+      let sorted = syncAction(
         context.settings.unocss?.configPath,
         'sort',
         input,
       ).trim()
+
+      if (addSpace === 'before')
+        sorted = ` ${sorted}`
+      else if (addSpace === 'after')
+        sorted += ' '
+
       if (sorted !== input) {
+        const nodeOrToken: TSESTree.Token | TSESTree.Node = node.type === 'SvelteLiteral' ? { type: AST_TOKEN_TYPES.String, value: node.value, loc: node.loc, range: node.range } : node
+
         context.report({
-          node,
+          node: nodeOrToken,
+          loc: node.loc,
           messageId: 'invalid-order',
           fix(fixer) {
             if (AST_NODES_WITH_QUOTES.includes(node.type))
               return fixer.replaceTextRange([node.range[0] + 1, node.range[1] - 1], sorted)
             else
-              return fixer.replaceText(node, sorted)
+              return fixer.replaceText(nodeOrToken, sorted)
           },
         })
       }
@@ -49,10 +60,38 @@ export default createRule({
             checkLiteral(node.value)
         }
       },
-      SvelteAttribute(node: any) {
+      SvelteAttribute(node: SvelteAttribute) {
         if (node.key.name === 'class') {
-          if (node.value?.[0].type === 'SvelteLiteral')
-            checkLiteral(node.value[0])
+          if (!node.value.length)
+            return
+
+          function checkExpressionRecursively(expression: SvelteMustacheTag['expression']) {
+            if (expression.type !== 'ConditionalExpression')
+              return
+
+            if (expression.consequent.type === 'Literal') {
+              checkLiteral(expression.consequent as TSESTree.Literal)
+            }
+            if (expression.alternate) {
+              if (expression.alternate.type === 'ConditionalExpression') {
+                checkExpressionRecursively(expression.alternate)
+              }
+              else if (expression.alternate.type === 'Literal') {
+                checkLiteral(expression.alternate as TSESTree.Literal)
+              }
+            }
+          }
+
+          (node.value).forEach((obj, i) => {
+            if (obj.type === 'SvelteMustacheTag') {
+              checkExpressionRecursively(obj.expression)
+            }
+            else if (obj.type === 'SvelteLiteral') {
+              const addSpace: 'before' | 'after' | undefined = node.value?.[i - 1]?.type === 'SvelteMustacheTag' ? 'before' : node.value?.[i + 1]?.type === 'SvelteMustacheTag' ? 'after' : undefined
+
+              checkLiteral(obj, addSpace)
+            }
+          })
         }
       },
     }
