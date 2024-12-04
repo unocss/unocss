848485343c845706dd0ed63b87ec55ba2e0e8149
Anthony Fu <github@antfu.me>
2024-12-04 14:05:23 +0800

    chore: update deps

--- interactive/composables/utils.ts
+++ interactive/composables/utils.ts
@@ -1,5 +1,5 @@
-import { mdnIndex } from '~/data/mdn-index'
 import type { RuleItem } from '~/types'
+import { mdnIndex } from '~/data/mdn-index'
 
 export function getDocs(item: RuleItem) {
   return item.features?.map(i => mdnIndex.value.find(s => s.title === i)!) || []
