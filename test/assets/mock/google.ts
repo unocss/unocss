e745da8e9f96705431d51a82e34fc7f1d5b99a8a
Chris <hizyyv@gmail.com>
2024-12-18 22:24:37 +0800

    test: upgrade msw usage

---
+++ test/assets/mock/google.ts
@@ -0,0 +1,20 @@
+export default `
+/* cyrillic-ext */
+@font-face {
+  font-family: 'Fira Code';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://fonts.gstatic.com/s/firacode/v22/uU9eCBsR6Z2vfE9aq3bL0fxyUs4tcw4W_D1sJV37NuzlojwUKaJO.woff2) format('woff2');
+  unicode-range: U+0460-052F, U+1C80-1C8A, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
+}
+/* cyrillic */
+@font-face {
+  font-family: 'Fira Code';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://fonts.gstatic.com/s/firacode/v22/uU9eCBsR6Z2vfE9aq3bL0fxyUs4tcw4W_D1sJVT7NuzlojwUKaJO.woff2) format('woff2');
+  unicode-range: U+0301, U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
+}
+`.trim()
--- test/setup.ts
+++ test/setup.ts
@@ -1,11 +1,12 @@
 import { http, HttpResponse } from 'msw'
 import { setupServer } from 'msw/node'
 import { afterAll, afterEach, beforeAll } from 'vitest'
-import { staticFontsourceMockData, variableFontsourceMockData, variableMockData } from './assets/fontsource-mock'
+import { staticFontsourceMockData, variableFontsourceMockData, variableMockData } from './assets/mock/fontsource'
+import googleResponse from './assets/mock/google'
 
 export const restHandlers = [
-  http.get(/google/, () => {
-    return new HttpResponse('@font-face mocked {}', { status: 200, headers: { 'Content-Type': 'text/css' } })
+  http.get(/googleapis/, () => {
+    return new HttpResponse(googleResponse, { status: 200, headers: { 'Content-Type': 'text/css' } })
   }),
   http.get('https://api.fontsource.org/v1/variable/:id', () => {
     return HttpResponse.json(variableMockData, { status: 200 })
@@ -25,6 +26,6 @@ export const restHandlers = [
 
 const server = setupServer(...restHandlers)
 
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }))
 afterAll(() => server.close())
 afterEach(() => server.resetHandlers())
