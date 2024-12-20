dd1545b2380e0f38a0ca8d369c0793f8d03c5456
Chris <hizyyv@gmail.com>
2024-12-20 13:06:39 +0800

    feat(preset-typography): support element modifiers (#4341)

--- packages/preset-typography/src/preflights/default.ts
+++ packages/preset-typography/src/preflights/default.ts
@@ -160,3 +160,41 @@ export function DEFAULT(theme: Theme) {
     },
   }
 }
+
+const modifiers = [
+  ['headings', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'th'],
+  ['h1'],
+  ['h2'],
+  ['h3'],
+  ['h4'],
+  ['h5'],
+  ['h6'],
+  ['p'],
+  ['a'],
+  ['blockquote'],
+  ['figure'],
+  ['figcaption'],
+  ['strong'],
+  ['em'],
+  ['kbd'],
+  ['code'],
+  ['pre'],
+  ['ol'],
+  ['ul'],
+  ['li'],
+  ['table'],
+  ['thead'],
+  ['tr'],
+  ['th'],
+  ['td'],
+  ['img'],
+  ['video'],
+  ['hr'],
+]
+
+export function getElements(modifier: string) {
+  for (const [name, ...selectors] of modifiers) {
+    if (name === modifier)
+      return selectors.length > 0 ? selectors : [name]
+  }
+}
