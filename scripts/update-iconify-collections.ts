bbd26a99f8347a9992daa99172d3b43a08a7d4cc
ntnyq <ntnyq13@gmail.com>
2025-01-04 21:34:34 +0800

    ci: add schedule to update iconify collections

---
+++ scripts/update-iconify-collections.ts
@@ -0,0 +1,32 @@
+import { writeFile } from 'node:fs/promises'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { execa } from 'execa'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const ICONIFY_COLLECTION = path.resolve(__dirname, '../packages/preset-icons/src/collections.json')
+
+async function update() {
+  try {
+    const collections: string[] = []
+    const res = await fetch('https://api.github.com/repos/iconify/icon-sets/contents/json')
+    const data: Array<{ name: string, type: 'file' | 'dir' }> = await res.json()
+
+    for (const item of data) {
+      if (item.type === 'file' && item.name.endsWith('.json')) {
+        collections.push(item.name.replace(/\.json$/, ''))
+      }
+    }
+
+    await writeFile(ICONIFY_COLLECTION, JSON.stringify(collections))
+
+    execa('eslint', ['--fix', '--no-ignore', ICONIFY_COLLECTION])
+  }
+  catch (err) {
+    console.log(err)
+  }
+}
+
+update()
