d3ec2e4463be0dc2f730e078333f6122a54372d3
Anthony Fu <github@antfu.me>
2025-01-13 21:31:44 +0800

    chore: update test file

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,5 +3,5 @@ import { defineWorkspace } from 'vitest/config'
 export default defineWorkspace([
   'vitest.config.ts',
   'packages-integrations/svelte-scoped/vitest.config.ts',
-  'packages-engine/runtime/vitest.config.ts',
+  'packages-integrations/runtime/vitest.config.ts',
 ])
