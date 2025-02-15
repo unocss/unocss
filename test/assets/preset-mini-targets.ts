2c50955d706a3af6b1ab69ed763a2ce4c987c2d8
eyemono.moe <59188998+eyemono-moe@users.noreply.github.com>
2025-02-15 16:57:35 +0900

    feat(preset-mini): add `container-type: size` support for `@container` rule (#4423)

--- test/assets/preset-mini-targets.ts
+++ test/assets/preset-mini-targets.ts
@@ -1239,6 +1239,10 @@ export const presetMiniTargets: string[] = [
   '@container/label',
   '@container-normal',
   '@container/label-normal',
+  '@container-inline-size',
+  '@container/label-inline-size',
+  '@container-size',
+  '@container/label-size',
 
   // variants - container query (@)
   '@sm:text-red',
