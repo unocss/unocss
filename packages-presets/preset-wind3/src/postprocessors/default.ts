db001e09a6cd6bebcef9513f3ca6e64cb1b87c07
Anthony Fu <github@antfu.me>
2025-02-18 14:57:29 +0800

    chore: update

--- packages-presets/preset-wind3/src/postprocessors/default.ts
+++ packages-presets/preset-wind3/src/postprocessors/default.ts
@@ -1,10 +1,10 @@
 import type { Postprocessor } from '@unocss/core'
-import type { PresetWindOptions } from '..'
+import type { PresetWind3Options } from '..'
 import { toArray } from '@unocss/core'
 import presetMini from '@unocss/preset-mini'
 import { important } from './important'
 
-export function postprocessors(options: PresetWindOptions): Postprocessor[] {
+export function postprocessors(options: PresetWind3Options): Postprocessor[] {
   return [
     ...toArray(presetMini(options).postprocess),
     ...important(options.important),
