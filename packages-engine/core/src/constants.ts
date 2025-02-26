1a68a654c122818a38e124a5282e63c3639b0fec
Chris <hizyyv@gmail.com>
2025-02-26 14:17:20 +0800

    feat(core): add `theme` layer order

--- packages-engine/core/src/constants.ts
+++ packages-engine/core/src/constants.ts
@@ -2,9 +2,11 @@ export const LAYER_DEFAULT = 'default'
 export const LAYER_PREFLIGHTS = 'preflights'
 export const LAYER_SHORTCUTS = 'shortcuts'
 export const LAYER_IMPORTS = 'imports'
+export const LAYER_THEME = 'theme'
 
 export const DEFAULT_LAYERS = {
   [LAYER_IMPORTS]: -200,
+  [LAYER_THEME]: -150,
   [LAYER_PREFLIGHTS]: -100,
   [LAYER_SHORTCUTS]: -10,
   [LAYER_DEFAULT]: 0,
