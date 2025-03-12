72dce329e15bdad14c40d429bf8a56de1a2a9ca3
xiaomo <wegi866@gmail.com>
2025-03-12 17:32:13 +0800

    fix(inspector): fix inspector typescript error (#4496)
    Co-authored-by: Chris <hizyyv@gmail.com>

--- playground/src/auto-imports.d.ts
+++ playground/src/auto-imports.d.ts
@@ -127,7 +127,6 @@ declare global {
   const shallowReactive: typeof import('vue')['shallowReactive']
   const shallowReadonly: typeof import('vue')['shallowReadonly']
   const shallowRef: typeof import('vue')['shallowRef']
-  const showPreflights: typeof import('./composables/prettier')['showPreflights']
   const syncRef: typeof import('@vueuse/core')['syncRef']
   const syncRefs: typeof import('@vueuse/core')['syncRefs']
   const templateRef: typeof import('@vueuse/core')['templateRef']
@@ -484,7 +483,6 @@ declare module 'vue' {
     readonly shallowReactive: UnwrapRef<typeof import('vue')['shallowReactive']>
     readonly shallowReadonly: UnwrapRef<typeof import('vue')['shallowReadonly']>
     readonly shallowRef: UnwrapRef<typeof import('vue')['shallowRef']>
-    readonly showPreflights: UnwrapRef<typeof import('./composables/prettier')['showPreflights']>
     readonly syncRef: UnwrapRef<typeof import('@vueuse/core')['syncRef']>
     readonly syncRefs: UnwrapRef<typeof import('@vueuse/core')['syncRefs']>
     readonly templateRef: UnwrapRef<typeof import('@vueuse/core')['templateRef']>
