4523aa248c881add4d0f4fc2fb8739759705b9f5
Chris <hizyyv@gmail.com>
2025-03-17 13:53:10 +0800

    fix(intereactive): update `shiki` usage & remove `nuxt/devtools` dep

--- interactive/composables/shiki.ts
+++ interactive/composables/shiki.ts
@@ -1,5 +1,6 @@
 import type { HighlighterCore } from 'shiki/core'
 import { createHighlighterCore } from 'shiki/core'
+import { createOnigurumaEngine } from 'shiki/engine/oniguruma'
 import langCss from 'shiki/langs/css.mjs'
 import langJs from 'shiki/langs/javascript.mjs'
 import vitesseDark from 'shiki/themes/vitesse-dark.mjs'
@@ -7,7 +8,7 @@ import vitesseLight from 'shiki/themes/vitesse-light.mjs'
 
 export const shiki = computedAsync<HighlighterCore>(async () => {
   return await createHighlighterCore({
-    loadWasm: () => import('shiki/wasm'),
+    engine: createOnigurumaEngine(() => import('shiki/wasm')),
     themes: [
       vitesseDark,
       vitesseLight,
