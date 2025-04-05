961c79933cbfa80fc1c1a122ed63360f6982145a
jrr997 <zackchangjx@foxmail.com>
2025-04-05 20:07:33 +0800

    fix: optimize shortcuts ordering

--- packages-integrations/svelte-scoped/test/cases/basic/OutputProd.svelte
+++ packages-integrations/svelte-scoped/test/cases/basic/OutputProd.svelte
@@ -2,8 +2,8 @@
 
 <style>
   :global(.usp-dyjh78) {
+    font-weight: 600;
     --un-bg-opacity: 1;
     background-color: rgb(239 68 68 / var(--un-bg-opacity));
-    font-weight: 600;
   }
 </style>
--- packages-integrations/svelte-scoped/test/cases/preflights/OutputProd.svelte
+++ packages-integrations/svelte-scoped/test/cases/preflights/OutputProd.svelte
@@ -108,13 +108,13 @@
     --un-backdrop-sepia: ;
   }
   :global(.usp-ff1ijb) {
-    margin-bottom: 0.25rem;
     --un-shadow: var(--un-shadow-inset) 0 1px 3px 0
         var(--un-shadow-color, rgb(0 0 0 / 0.1)),
       var(--un-shadow-inset) 0 1px 2px -1px var(--un-shadow-color, rgb(0 0 0 /
               0.1));
     box-shadow: var(--un-ring-offset-shadow), var(--un-ring-shadow),
       var(--un-shadow);
+    margin-bottom: 0.25rem;
   }
   div {
     border-radius: 0.25rem;
