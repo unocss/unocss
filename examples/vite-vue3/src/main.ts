0300cc6dde11f99cb578087602c045db43029d01
Anthony Fu <anthonyfu117@hotmail.com>
2025-04-12 10:23:31 +0900

    fix(vite): more efficient and robust global build mode (#4586)
    Co-authored-by: sapphi-red <sapphi-red@users.noreply.github.com>

--- examples/vite-vue3/src/main.ts
+++ examples/vite-vue3/src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'uno.css'
 
+// @ts-expect-error missing types
+window.onload = () => import('uno:icons.css')
 createApp(App).mount('#app')
