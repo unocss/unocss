fefee5cbc0ee35d0bda4c74a0a50ba4b72b179b3
Chris <hizyyv@gmail.com>
2025-04-18 17:31:36 +0800

    chore(preset-wind4): file structure adjustment (#4602)

--- packages-presets/preset-wind4/src/utils/constant.ts
+++ packages-presets/preset-wind4/src/utils/constant.ts
@@ -1,24 +1,6 @@
-import type { Arrayable } from '@unocss/core'
-import { toArray } from '@unocss/core'
-
 export const PRESET_NAME = '@unocss/preset-wind4'
-
-/**
- * Used to track theme keys.
- *
- * eg: colors:red-100
- *
- * @internal
- */
-export const trackedTheme = new Set<string>([])
-
-export function themeTracking(key: string, props: Arrayable<string> = 'DEFAULT') {
-  const k = `${key}:${toArray(props).join('-')}`
-  if (!trackedTheme.has(k)) {
-    trackedTheme.add(k)
-  }
-}
-
-export function generateThemeVariable(key: string, props: Arrayable<string>) {
-  return `var(--${key}-${toArray(props).join('-')})`
+export const CONTROL_NO_NEGATIVE = '$$mini-no-negative'
+export const SpecialColorKey = {
+  transparent: 'transparent',
+  current: 'currentColor',
 }
