679b67db0c29312a5e92325c3097922d06ac5f2e
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-04-26 00:21:40 +0800

    fix(transformer-attributify-jsx): blocks Astro MDX file rendering

--- packages-presets/preset-attributify/src/extractor.ts
+++ packages-presets/preset-attributify/src/extractor.ts
@@ -8,8 +8,8 @@ const strippedPrefixes = [
 ]
 
 const splitterRE = /[\s'"`;]+/g
-// eslint-disable-next-line regexp/no-super-linear-backtracking, regexp/no-dupe-disjunctions
-const elementRE = /<[^>\s]*\s((?:'[^']*'|"[^"]*"|`[^`]*`|\{[^}]*\}|=>|[^>]*?)*)/g
+
+const elementRE = /<[^>\s/"'`]+\s((?:'[^']*'|"[^"]*"|`[^`]*`|\{[^}]*\}|=>|[^>"'`{])*)/g
 const valuedAttributeRE = /(\?|(?!\d|-{2}|-\d)[\w\u00A0-\uFFFF:!%.~<-]+)=?(?:"([^"]*)"|'([^']*)'|\{([^}]*)\})?/g
 
 export const defaultIgnoreAttributes = ['placeholder', 'fill', 'opacity', 'stroke-opacity']
