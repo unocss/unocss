158b4405e23a6a92b1f8bf5dbc72fee9fe1a3b49
Andreas Fehn <fehnomenal@fehn.systems>
2025-06-25 04:13:42 +0200

    feat(svelte-scoped): handle clsx like classes (#4758)

--- packages-integrations/svelte-scoped/src/_preprocess/transformClasses/findClasses.test.ts
+++ packages-integrations/svelte-scoped/src/_preprocess/transformClasses/findClasses.test.ts
@@ -72,4 +72,141 @@ describe(findClasses, () => {
     ]
     expect(result).toEqual(expected)
   })
+
+  it('finds clsx like classes (singleton array)', () => {
+    const code = '<span class={["abc"]} />'
+    const result = findClasses(code)
+    const expected: FoundClass[] = [
+      {
+        body: 'abc',
+        start: 15,
+        end: 18,
+        type: 'regular',
+      },
+    ]
+    expect(result).toEqual(expected)
+  })
+
+  it('finds clsx like classes (simple array)', () => {
+    // eslint-disable-next-line no-template-curly-in-string
+    const code = '<span class={["abc def", `ghi`, `button-${dynamic}`]} />'
+    const result = findClasses(code)
+    const expected: FoundClass[] = [
+      {
+        body: 'abc def',
+        start: 15,
+        end: 22,
+        type: 'regular',
+      },
+      {
+        body: 'ghi',
+        start: 26,
+        end: 29,
+        type: 'regular',
+      },
+    ]
+    expect(result).toEqual(expected)
+  })
+
+  it('finds clsx like classes (conditional array)', () => {
+    const code = '<span class={["abc def", true && `ghi`, false ? \'jkl\' : "mno"]} />'
+    const result = findClasses(code)
+    const expected: FoundClass[] = [
+      {
+        body: 'abc def',
+        start: 15,
+        end: 22,
+        type: 'regular',
+      },
+      {
+        body: 'ghi',
+        start: 34,
+        end: 37,
+        type: 'regular',
+      },
+      {
+        body: 'jkl',
+        start: 49,
+        end: 52,
+        type: 'regular',
+      },
+      {
+        body: 'mno',
+        start: 57,
+        end: 60,
+        type: 'regular',
+      },
+    ]
+    expect(result).toEqual(expected)
+  })
+
+  it('finds clsx like classes (object)', () => {
+    const code = '<span class={{ "abc def": true, "ghi": !bool, show }} />'
+    const result = findClasses(code)
+    const expected: FoundClass[] = [
+      {
+        body: 'abc def',
+        start: 15,
+        end: 24,
+        type: 'clsxObject',
+      },
+      {
+        body: 'ghi',
+        start: 32,
+        end: 37,
+        type: 'clsxObject',
+      },
+      {
+        body: 'show',
+        start: 46,
+        end: 50,
+        type: 'clsxObjectShorthand',
+      },
+    ]
+    expect(result).toEqual(expected)
+  })
+
+  it('finds clsx like classes (complex array)', () => {
+    const code = '<span class={[{ "abc def": true, "ghi": !bool, show }, "jkl", ["mno"], { pqr: 0 }]} />'
+    const result = findClasses(code)
+    const expected: FoundClass[] = [
+      {
+        body: 'abc def',
+        start: 16,
+        end: 25,
+        type: 'clsxObject',
+      },
+      {
+        body: 'ghi',
+        start: 33,
+        end: 38,
+        type: 'clsxObject',
+      },
+      {
+        body: 'show',
+        start: 47,
+        end: 51,
+        type: 'clsxObjectShorthand',
+      },
+      {
+        body: 'jkl',
+        start: 56,
+        end: 59,
+        type: 'regular',
+      },
+      {
+        body: 'mno',
+        start: 64,
+        end: 67,
+        type: 'regular',
+      },
+      {
+        body: 'pqr',
+        start: 73,
+        end: 76,
+        type: 'clsxObject',
+      },
+    ]
+    expect(result).toEqual(expected)
+  })
 })
--- packages-integrations/svelte-scoped/src/_preprocess/transformClasses/findClasses.ts
+++ packages-integrations/svelte-scoped/src/_preprocess/transformClasses/findClasses.ts
@@ -1,3 +1,6 @@
+import * as acorn from 'acorn'
+import { walk } from 'zimmerframe'
+
 export interface FoundClass {
   body: string
   start: number
@@ -5,26 +8,29 @@ export interface FoundClass {
   type: ClassForms
 }
 
-type ClassForms = 'regular' | 'directive' | 'directiveShorthand'
+type ClassForms = 'regular' | 'directive' | 'directiveShorthand' | 'clsxObject' | 'clsxObjectShorthand'
 
 const classesRE = /class=(["'`])([\s\S]*?)\1/g // class="mb-1"
 const classDirectivesRE = /class:(\S+?)="?\{/g // class:mb-1={foo} and class:mb-1="{foo}"
 const classDirectivesShorthandRE = /class:([^=>\s/]+)[{>\s/]/g // class:logo (compiled to class:uno-1hashz={logo})
+const classClsxRE = /(?<prefix>class=\{\w*)[[{(]/g // class={[...]}, class={{...}} and class={clsx(...)} (in fact any function call)
 
 export function findClasses(code: string) {
   const matchedClasses = [...code.matchAll(classesRE)]
   const matchedClassDirectives = [...code.matchAll(classDirectivesRE)]
   const matchedClassDirectivesShorthand = [...code.matchAll(classDirectivesShorthandRE)]
+  const matchedClassClsx = [...code.matchAll(classClsxRE)]
 
   const classes = parseMatches(matchedClasses, 'regular', 'class="'.length)
   const classDirectives = parseMatches(matchedClassDirectives, 'directive', 'class:'.length)
   const classDirectivesShorthand = parseMatches(matchedClassDirectivesShorthand, 'directiveShorthand', 'class:'.length)
+  const classClsx = parseMatchesWithAcorn(matchedClassClsx, code)
 
-  return [...classes, ...classDirectives, ...classDirectivesShorthand]
+  return [...classes, ...classDirectives, ...classDirectivesShorthand, ...classClsx]
 }
 
 function parseMatches(matches: RegExpMatchArray[], type: ClassForms, prefixLength: number) {
-  return matches.map((match) => {
+  return matches.map((match): FoundClass => {
     const body = match[type === 'regular' ? 2 : 1]
     const start = match.index! + prefixLength
     return {
@@ -38,4 +44,86 @@ function parseMatches(matches: RegExpMatchArray[], type: ClassForms, prefixLengt
 
 function hasBody(foundClass: FoundClass) {
   return foundClass.body
+}
+
+function parseMatchesWithAcorn(matches: RegExpMatchArray[], code: string) {
+  return matches.flatMap((match): FoundClass[] => {
+    const start = match.index! + match.groups!.prefix.length
+
+    const ast = acorn.parseExpressionAt(code, start, {
+      sourceType: 'module',
+      ecmaVersion: 16,
+      locations: true,
+    })
+
+    const classes: FoundClass[] = []
+
+    function fromProperty(body: string, node: acorn.AnyNode, property: acorn.Property | acorn.AssignmentProperty): FoundClass {
+      return {
+        body,
+        start: node.start,
+        end: node.end,
+        type: property.shorthand ? 'clsxObjectShorthand' : 'clsxObject',
+      }
+    }
+
+    function fromString(body: string, node: acorn.AnyNode): FoundClass {
+      return {
+        body,
+        start: node.start + 1,
+        end: node.end - 1,
+        type: 'regular',
+      }
+    }
+
+    walk(
+      ast as acorn.AnyNode,
+      { property: undefined as acorn.Property | acorn.AssignmentProperty | undefined },
+      {
+        Property(node, { visit }) {
+          visit(node.key, { property: node })
+        },
+
+        Identifier(node, { state, next }) {
+          if (state.property) {
+            classes.push(fromProperty(node.name, node, state.property))
+          }
+
+          next()
+        },
+
+        Literal(node, { state, next }) {
+          if (typeof node.value === 'string') {
+            const body = node.value
+
+            if (state.property) {
+              classes.push(fromProperty(body, node, state.property))
+            }
+            else {
+              classes.push(fromString(body, node))
+            }
+          }
+
+          next()
+        },
+
+        TemplateLiteral(node, { state, next }) {
+          if (node.expressions.length === 0 && node.quasis.length === 1) {
+            const body = node.quasis[0].value.raw
+
+            if (state.property) {
+              classes.push(fromProperty(body, node, state.property))
+            }
+            else {
+              classes.push(fromString(body, node))
+            }
+          }
+
+          next()
+        },
+      },
+    )
+
+    return classes
+  }).filter(hasBody)
 }
