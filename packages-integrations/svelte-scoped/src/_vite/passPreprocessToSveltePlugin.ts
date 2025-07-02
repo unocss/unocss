4e4afb5d6ce149d852ac649057f258309996fdaa
Andreas Fehn <fehnomenal@fehn.systems>
2025-07-02 17:24:59 +0200

    chore(svelte-scoped): resolve deprecation of preprocessing from vite plugins

--- packages-integrations/svelte-scoped/src/_vite/passPreprocessToSveltePlugin.ts
+++ packages-integrations/svelte-scoped/src/_vite/passPreprocessToSveltePlugin.ts
@@ -1,24 +1,44 @@
 import type { Plugin } from 'vite'
 import type { SvelteScopedContext } from '../preprocess'
 import type { UnocssSvelteScopedViteOptions } from './types'
+import { preprocess } from 'svelte/compiler'
 import UnocssSveltePreprocess from '../preprocess'
 
 export function PassPreprocessToSveltePlugin(context: SvelteScopedContext, options: UnocssSvelteScopedViteOptions = {}): Plugin {
-  let commandIsBuild = true
-  const isBuild = () => commandIsBuild
+  let preprocessor: ReturnType<typeof UnocssSveltePreprocess>
 
-  return {
+  const plugin = {
     name: 'unocss:svelte-scoped:pass-preprocess',
     enforce: 'pre',
 
-    configResolved({ command }) {
-      commandIsBuild = command === 'build'
+    configResolved({ command, plugins }) {
+      preprocessor = UnocssSveltePreprocess(options, context, () => command === 'build')
+
+      // use the exact same id filter as vite-plugin-svelte itself
+      const svelteIdFilter = plugins.find(p => p.name === 'vite-plugin-svelte:config')?.api.idFilter
+      plugin.transform.filter = svelteIdFilter
     },
 
-    api: {
-      sveltePreprocess: UnocssSveltePreprocess(options, context, isBuild),
+    transform: {
+      filter: {
+        // filled above
+      },
+
+      // after base preprocess, but before compile
+      order: 'pre',
+
+      async handler(source, id) {
+        const { code, map } = await preprocess(source, preprocessor, { filename: id })
+
+        if (typeof map === 'string')
+          return { code, map }
+
+        return { code }
+      },
     },
-  }
+  } satisfies Plugin
+
+  return plugin
 }
 
 // reference: https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/faq.md#how-do-i-add-a-svelte-preprocessor-from-a-vite-plugin
