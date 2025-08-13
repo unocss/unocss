b8c4c753f6e47038fca3c6adb1930182d9b411b5
Jungzl <13jungzl@gmail.com>
2025-08-13 11:38:45 +0800

    fix: replace CoolLabsFontsProvider with GoogleCompatibleProvider

--- packages-presets/preset-web-fonts/src/providers/coollabs.ts
+++ packages-presets/preset-web-fonts/src/providers/coollabs.ts
@@ -1,36 +1,5 @@
-import type { Provider, WebFontsProviders } from '../types'
-import type { FontAxis } from './google'
-import { generateFontAxes } from './google'
+import type { Provider } from '../types'
+import { createGoogleCompatibleProvider } from './google'
 
-export function createCoolLabsCompatibleProvider(name: WebFontsProviders, host: string): Provider {
-  return {
-    name,
-    getImportUrl(fonts) {
-      const strings = fonts
-        .map((i) => {
-          let name = i.name.replace(/\s+/g, '+')
-          const axisValues: FontAxis[] = []
-          if (i.italic)
-            axisValues.push({ axis: 'ital', values: ['0', '1'] })
-
-          if (i.width)
-            axisValues.push({ axis: 'wdth', values: Array.isArray(i.width) ? i.width : [i.width.toString()] })
-
-          if (i.weights?.length)
-            axisValues.push({ axis: 'wght', values: i.weights.map(w => w.toString()) })
-
-          if (axisValues.length) {
-            name += ':'
-            name += axisValues.map(a => a.axis).join(',')
-            name += '@'
-            name += generateFontAxes(axisValues)
-          }
-          return `family=${name}`
-        })
-        .join('&')
-      return `${host}/css2?${strings}&display=swap`
-    },
-  }
-}
-
-export const CoolLabsFontsProvider: Provider = createCoolLabsCompatibleProvider('coollabs', 'https://api.fonts.coollabs.io')
+// https://fonts.coollabs.io/
+export const CoolLabsFontsProvider: Provider = createGoogleCompatibleProvider('coollabs', 'https://api.fonts.coollabs.io')
