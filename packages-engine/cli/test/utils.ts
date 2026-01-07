522dd67e2b5ecbbcd60631ea90a3a9ba24c8a2c1
Chris <hizyyv@gmail.com>
2026-01-07 22:26:54 +0800

    chore: update

--- packages-engine/cli/test/utils.ts
+++ packages-engine/cli/test/utils.ts
@@ -33,11 +33,9 @@ export function readFile(testDir: string, targetFile?: string) {
 
 export async function runCli(files: Record<string, string>, options?: { transformFile?: string, args?: string[], outFile?: string }) {
   const testDir = getTestDir()
+  const hasConfigFile = Object.keys(files).some(f => f.includes('uno.config') || f.includes('unocss.config'))
 
-  if (
-    !Object.keys(files).some(f => f.includes('uno.config') || f.includes('unocss.config'))
-    || !options?.args?.includes('--preset')
-  ) {
+  if (!hasConfigFile && !(options?.args?.includes('--preset'))) {
     files['uno.config.ts'] = `
 import { defineConfig, presetWind3, transformerDirectives } from 'unocss'
 export default defineConfig({
