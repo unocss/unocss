9c8bd0d78aba59060d9e473f5e2289b3e1892a03
micaiguai <1399880823@qq.com>
2026-03-13 11:07:17 +0800

    fix(preset-wind4): move test case to preset-wind4

--- test/preset-wind4.test.ts
+++ test/preset-wind4.test.ts
@@ -464,6 +464,32 @@ describe('preset-wind4', () => {
       "
     `)
   })
+
+  it('basic variable prefix', async () => {
+    const uno = await createGenerator({
+      presets: [
+        presetWind4({
+          variablePrefix: 'foo-',
+          preflights: {
+            reset: false,
+            theme: false,
+            property: true,
+          },
+        }),
+      ],
+    })
+    const { css } = await uno.generate('bg-white')
+    expect(css).toMatchInlineSnapshot(`
+    "/* layer: properties */
+    @supports ((-webkit-hyphens: none) and (not (margin-trim: inline))) or ((-moz-orient: inline) and (not (color:rgb(from red r g b)))){*, ::before, ::after, ::backdrop{--foo-bg-opacity:100%;}}
+    @property --foo-bg-opacity{syntax:"<percentage>";inherits:false;initial-value:100%;}
+    /* layer: default */
+    .bg-white{background-color:color-mix(in srgb, var(--colors-white) var(--foo-bg-opacity), transparent);}
+    @supports (color: color-mix(in lab, red, red)){
+    .bg-white{background-color:color-mix(in oklab, var(--colors-white) var(--foo-bg-opacity), transparent);}
+    }"
+  `)
+  })
 })
 
 describe('important', () => {
