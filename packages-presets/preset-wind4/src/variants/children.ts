48822103ca13409b1ab25ff3d47fb9715e31cb90
Jack-sh1 <shennalin2@gmail.com>
2026-05-19 19:35:14 +0800

    revert: restore variant logic to main to avoid regression
    Reverting code changes as simply removing order: -1 caused regression for #4726
    and did not fully align with Tailwind v4.

--- packages-presets/preset-wind4/src/variants/children.ts
+++ packages-presets/preset-wind4/src/variants/children.ts
@@ -1,8 +1,8 @@
 import type { Variant } from '@unocss/core'
 import type { Theme } from '../theme'
-import { variantMatcher } from '@unocss/rule-utils'
+import { variantMatcher } from '../utils'
 
 export const variantChildren: Variant<Theme>[] = [
-  variantMatcher('*', input => ({ selector: `${input.selector} > *` })),
-  variantMatcher('**', input => ({ selector: `${input.selector} *` })),
+  variantMatcher('*', input => ({ selector: `${input.selector} > *` }), { order: -1 }),
+  variantMatcher('**', input => ({ selector: `${input.selector} *` }), { order: -1 }),
 ]
