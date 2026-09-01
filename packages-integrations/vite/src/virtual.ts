199c1ba4f248ea4bd97a016361fbe3f19b5445bd
Chris <hizyyv@gmail.com>
2026-09-01 18:23:05 +0800

    fix(vite): encode virtual IDs in global HMR updates (#5298)

--- packages-integrations/vite/src/virtual.ts
+++ packages-integrations/vite/src/virtual.ts
@@ -1,7 +1,15 @@
 const VITE_VIRTUAL_ID_PREFIX = '\0'
+const VITE_CLIENT_ID_PREFIX = '/@id/'
+const VITE_NULL_BYTE_PLACEHOLDER = '__x00__'
 
 export function toViteVirtualId(id: string) {
   return id.startsWith(VITE_VIRTUAL_ID_PREFIX)
     ? id
     : `${VITE_VIRTUAL_ID_PREFIX}${id}`
+}
+
+export function toViteClientPath(id: string) {
+  return id.startsWith(VITE_VIRTUAL_ID_PREFIX)
+    ? `${VITE_CLIENT_ID_PREFIX}${id.replace(VITE_VIRTUAL_ID_PREFIX, VITE_NULL_BYTE_PLACEHOLDER)}`
+    : id
 }
